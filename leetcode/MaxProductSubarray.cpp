e2b70486f9e6df9fdacf87ccd023f2cf7fdc3fb6
Thomas <txchen@gmail.com>
2014-10-16 18:23:44 -1000

    add new questions, fix issues in old questions

---
+++ leetcode/MaxProductSubarray.cpp
@@ -0,0 +1,39 @@
+// 10/16/2014
+
+// Note: the array is separately by several segments by 0.
+// Get the max from each segment, then get the global max from local max.
+// You need a second round from back to front, for there may be neg products (e.g., -1, 3, 5).
+// This works.
+class Solution {
+public:
+    int maxProduct(int A[], int n) {
+        int mp = A[0], p = A[0];
+        for (int i = 1; i < n; ++ i) {
+            if (p == 0) p = 1;
+            p *= A[i];
+            mp = max(mp, p);
+        }
+        
+        mp = max(mp, A[n-1]);
+        p = A[n-1];
+        for (int i = n-2; i >= 0; -- i) {
+            if (p == 0) p = 1;
+            p *= A[i];
+            mp = max(mp, p);
+        }
+        
+        return mp;
+    }
+};
+
+
+/*
+
+Maximum Product Subarray
+
+Find the contiguous subarray within an array (containing at least one number) which has the largest product.
+
+For example, given the array [2,3,-2,4],
+the contiguous subarray [2,3] has the largest product = 6. 
+
+*/
--- leetcode/ValidBinarySearchTree.cpp
+++ leetcode/ValidBinarySearchTree.cpp
@@ -21,10 +21,33 @@ public:
         return isValid(root, INT_MIN, INT_MAX);
     }
     
+    // This has an issue if a node's value is INT_MIN or INT_MAX.
     bool isValid(TreeNode * n, int minv, int maxv) {
         if (n == NULL) return true;
         return (n->val > minv && n->val < maxv) &&
                isValid(n->left, minv, n->val) &&
                isValid(n->right, n->val, maxv);
+    }
+
+    // Method 2. This fixes the issue when a node's value equals INT_MIN or INT_MAX.
+    // Call with: bst(root, NULL, INT_MIN, INT_MAX).
+    // NOTE: Seems Method 2 is wrong? {3,1,5,0,2,4,6,#,#,#,3} should be false, but returns true.
+    // So Method 2 is NOT correct. 10-16-2014.
+    bool bst(TreeNode * n, TreeNode * parent, int L, int R) {
+        if (n == NULL) return true;
+
+        return (n->val >= L && n->val <= R &&
+                (parent == NULL ? true : n->val != parent->val) &&
+                bst(n->left, n, L, n->val) &&
+                bst(n->right, n, n->val, R));
+    }
+
+    // Method 3. This works! Use long long type to overcome overflow issue.
+    void IsBST2(TreeNode * root) { bst2(root, INT_MIN - 11l, INT_MAX + 1ll); }
+
+    bool bst2(TreeNode * root, long  long MIN, long long MAX){
+        if (root == NULL) return true;
+        return root->val < MAX && root->val > MIN &&
+               bst2(root->left, MIN, root->val) && bst2(root->right, root->val, MAX);
     }
 };
