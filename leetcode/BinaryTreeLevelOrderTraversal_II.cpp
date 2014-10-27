6377a893563f3433410a42d1c84ca68d2639451e
Tom Chen <txchen@gmail.com>
2014-10-27 00:37:38 -1000

    Update BinaryTreeLevelOrderTraversal_II.cpp

--- leetcode/BinaryTreeLevelOrderTraversal_II.cpp
+++ leetcode/BinaryTreeLevelOrderTraversal_II.cpp
@@ -123,3 +123,36 @@ public:
         return vs;
     }
 };
+
+
+// This also works.
+class Solution3 {
+public:
+    vector<vector<int> > levelOrderBottom(TreeNode *root) {
+        vector<vector<int> > ans;
+        if (! root) return ans;
+        
+        queue<TreeNode *> q;
+        q.push(root);
+        int level_ct = 1;
+        
+        while (level_ct > 0) {
+            int ct = 0;
+            vector<int> R;
+            
+            for (; level_ct > 0; -- level_ct) {
+                TreeNode * t = q.front();
+                q.pop();
+                R.push_back(t->val);
+                
+                if (t->left) { q.push(t->left); ++ ct; }
+                if (t->right) { q.push(t->right); ++ ct; }
+            }
+            
+            level_ct = ct;
+            ans.insert(ans.begin(), R);
+        }
+        
+        return ans;
+    }
+};
