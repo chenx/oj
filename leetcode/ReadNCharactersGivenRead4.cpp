0c3d2ae42ecc1f87d4f19a7ac9807c8d3f8641f3
Tom Chen <txchen@gmail.com>
2014-11-24 01:03:52 -1000

    Update ReadNCharactersGivenRead4.cpp

--- leetcode/ReadNCharactersGivenRead4.cpp
+++ leetcode/ReadNCharactersGivenRead4.cpp
@@ -4,5 +4,6 @@
  * Ref:
  * [1] http://www.mitbbs.com/article_t/JobHunting/32836155.html
  * [2] http://blog.csdn.net/a83610312/article/details/12872437
+ * [3] http://www.mitbbs.com/article_t/JobHunting/32810997.html
  */
  
