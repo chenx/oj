9ff8f2887032e954903c3c03a72e81da2992d461
Tom Chen <txchen@gmail.com>
2014-11-24 01:02:53 -1000

    Update ReadNCharactersGivenRead4.cpp

--- leetcode/ReadNCharactersGivenRead4.cpp
+++ leetcode/ReadNCharactersGivenRead4.cpp
@@ -1,4 +1,8 @@
 /**
  * Read N Characters Given Read4.
+ * 
+ * Ref:
+ * [1] http://www.mitbbs.com/article_t/JobHunting/32836155.html
+ * [2] http://blog.csdn.net/a83610312/article/details/12872437
  */
  
