99aa72ba21b9a2ace8ce8af561941783d7d40010
Tom Chen <txchen@gmail.com>
2015-01-18 22:51:08 -1000

    Update PascalsTriangle_II.cpp

--- leetcode/PascalsTriangle_II.cpp
+++ leetcode/PascalsTriangle_II.cpp
@@ -103,8 +103,30 @@ public:
         
         return v;
     }
-}    
-    
+}  
+
+// This works too.
+// Note the use of function fill() on vector.
+// ex:
+// row 0: 1
+// row 1: 1 1
+// row 2: 1 2 1   <== only need to start processing from this row.
+// row 3: 1 3 3 1
+class Solution6 {
+public:
+    vector<int> getRow(int rowIndex) {
+        vector<int> row(rowIndex + 1);
+        fill(row.begin(), row.end(), 1);
+        
+        for (int i = 2; i <= rowIndex; ++ i) {
+            for (int j = i - 1; j > 0; -- j) {
+                row[j] = row[j] + row[j - 1];
+            }
+        } 
+        
+        return row;
+    }
+}; 
 
 /*
 Problem:
