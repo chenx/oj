52f0716a50019ae5e210909e9eb3cba5eebf85be
Tom Chen <txchen@gmail.com>
2015-01-18 23:03:50 -1000

    Update PascalsTriangle.cpp

--- leetcode/PascalsTriangle.cpp
+++ leetcode/PascalsTriangle.cpp
@@ -5,7 +5,31 @@
 // @Last modified: 12/22/2012
 //
 
-class Solution {
+// This works too.
+class Solution3 {
+public:
+    vector<vector<int> > generate(int numRows) {
+        vector<vector<int> > rows;
+        if (numRows == 0) return rows;
+
+        vector<int> row1(1);
+        row1[0] = 1;
+        rows.push_back(row1);
+        
+        for (int i = 1; i < numRows; ++ i) {
+            vector<int> v = rows[i - 1]; // copy the previous row.
+            v.push_back(1);              // add last 1.
+            for (int j = i-1; j > 0; -- j) v[j] += v[j - 1]; // update elements.
+            
+            rows.push_back(v);           // add new row.
+        }
+        
+        return rows;
+    }
+};
+
+// This works too.
+class Solution2 {
 public:
     vector<vector<int> > generate(int numRows) {
         vector<vector<int> > vs;
