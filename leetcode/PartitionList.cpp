a3092ad217299fa1685e715f7257c72664b9b55f
Tom Chen <txchen@gmail.com>
2015-01-24 09:28:59 -1000

    Update PartitionList.cpp

--- leetcode/PartitionList.cpp
+++ leetcode/PartitionList.cpp
@@ -5,6 +5,42 @@
 // @Last modified: 1/5/2012
 //
 
+// This works too. Slightly different.
+class Solution2 {
+public:
+    ListNode *partition(ListNode *head, int x) {
+        // split the original list into 2, based on x. Then combine.
+        if (head == NULL || head->next == NULL) return head;
+        
+        ListNode * h1 = NULL, * t1 = NULL, 
+                 * h2 = NULL, * t2 = NULL; // don't forget to intialize to NULL.
+        while (head != NULL) {
+            if (head->val < x) { addNodeToList(h1, t1, head); }
+            else { addNodeToList(h2, t2, head); }
+        }
+        
+        if (t1 == NULL) return h2; // don't forget t1 could be NULL.
+        else {
+            t1->next = h2;
+            return h1;
+        }
+    }
+    
+    // h and t can be ptr to ptr or reference of ptr.
+    // v can be both, or just a ptr.
+    void addNodeToList(ListNode * &h, ListNode * &t, ListNode *& v) {
+        if (NULL == h) {
+            h = t = v;
+        }
+        else {
+            t->next = v;
+            t = t->next;
+        }
+        v = v->next;
+        t->next = NULL; // This is necessary.
+    }
+};
+
 /**
  * Definition for singly-linked list.
  * struct ListNode {
