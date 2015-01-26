a5294e721b0bc3ae0007ef09ce92d922b98c3047
Tom Chen <txchen@gmail.com>
2015-01-25 19:09:41 -1000

    Update PalindromePartitioning.cpp

--- leetcode/PalindromePartitioning.cpp
+++ leetcode/PalindromePartitioning.cpp
@@ -11,6 +11,49 @@
 // @Last modified: 3/2/2013
 //
 
+// This works too.
+// Different from Solution only in index position.
+// X.C. 1/25/2015
+class Solution2 {
+public:
+    vector<vector<string> > partition(string s) {
+        int n = s.length();
+        vector<vector<vector<string> > > ans(n);
+        
+        for (int i = 1; i <= n; ++ i) {
+            string a = s.substr(0, i);
+            if (isPalindrome(a)) { 
+                vector<string> u;
+                u.push_back(a);
+                ans[i-1].push_back(u); 
+            }
+            
+            for (int j = 1; j < i; ++ j) {
+                string b = s.substr(j, i - j); // check each substring starts at j, ends at i.
+                vector<vector<string> > v = ans[j-1];
+                if (v.size() > 0 && isPalindrome(b)) {
+                    for (int k = 0; k < v.size(); ++ k) {
+                        vector<string> u = v[k];
+                        u.push_back(b);
+                        ans[i-1].push_back(u);
+                    }
+                }
+            }
+        }
+        
+        return ans[n-1];
+    }
+    
+    bool isPalindrome(string s) {
+        int L = 0, R = s.length() - 1;
+        while (L < R) {
+            if (s[L] != s[R]) return false;
+            ++ L, -- R;
+        }
+        return true;
+    }
+};
+
 class Solution {
 public:
     vector<vector<string>> partition(string s) {
