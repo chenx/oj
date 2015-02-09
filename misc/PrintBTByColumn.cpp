77c410ca9e5a1a21cb1f52dbb7a3283beb52ccd2
Tom Chen <txchen@gmail.com>
2015-02-09 13:26:17 -1000

    Update PrintBTByColumn.cpp

--- misc/PrintBTByColumn.cpp
+++ misc/PrintBTByColumn.cpp
@@ -34,6 +34,8 @@ struct Node {
 };
 
 // This works.
+// However, using pre-order traversal, top-down order may not be guaranteed. 
+// E.g., if 5 has a right child in test2().
 void printT(Node * n, map<int, vector<int> > & m, int col, int &mi, int &ma) {
     if (n == NULL) return;
 
@@ -85,6 +87,35 @@ void test2() {
     printT(root, m, col, mi, ma);
     output(m, mi, ma);
 }
+
+////////////////////
+// This works. 
+// From: http://www.mitbbs.com/mitbbs_article_t.php?board=JobHunting&gid=32883277&start=32883353&pno=-1
+void printBT_BFS(TreeNode *root, map<int, vector<int> > &hash, int &mincol, int &maxcol) {
+    if(root==NULL) return;
+    queue<pair<TreeNode *, int> > q; //store the node pointer and the column
+    q.push(pair<TreeNode *, int>(root, 0));
+    while(!q.empty()) {
+        pair<TreeNode *, int> item = q.front();
+        q.pop();
+        TreeNode *node = item.first;
+        int col = item.second;
+        hash[col].push_back(node->val);
+        mincol = min(mincol, col);
+        maxcol = max(maxcol, col);
+        if(node->left!=NULL) q.push(pair<TreeNode *, int>(node->left, col-1));
+        if(node->right!=NULL) q.push(pair<TreeNode *, int>(node->right, col+1));
+    }
+}
+void print(map<int, vector<int> > &hash, int mincol, int maxcol) {
+    for(int i=mincol;i<=maxcol;i++) {
+        for(int j=0;j<hash[i].size();j++) {
+            cout<<hash[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+}
+////////////////////
 
 int main() {
     test1();
