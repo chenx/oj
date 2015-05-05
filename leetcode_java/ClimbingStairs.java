21ab767f03a0b21d933f314cf822d327dd9b9e93
Tom Chen <txchen@gmail.com>
2015-05-04 23:44:47 -1000

    Create ClimbingStairs.java

---
+++ leetcode_java/ClimbingStairs.java
@@ -0,0 +1,14 @@
+public class Solution {
+    public int climbStairs(int n) {
+        if (n <= 2) return n;
+        
+        int a = 1, b = 2;
+        for (int i = 3; i <= n; ++ i) {
+            int tmp = b;
+            b = a + b;
+            a = tmp;
+        }
+        
+        return b;
+    }
+}
