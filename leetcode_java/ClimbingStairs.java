a56b849bdf8f69df8c9476bc89568bf95b6a475b
Tom Chen <txchen@gmail.com>
2015-09-21 12:44:02 -1000

    Update ClimbingStairs.java

--- leetcode_java/ClimbingStairs.java
+++ leetcode_java/ClimbingStairs.java
@@ -1,3 +1,10 @@
+/**
+ * Climbing Stairs
+ *
+ * You are climbing a stair case. It takes n steps to reach to the top.
+ *
+ * Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top? 
+ */
 public class Solution {
     public int climbStairs(int n) {
         if (n <= 2) return n;
@@ -10,5 +17,21 @@ public class Solution {
         }
         
         return b;
+    }
+}
+
+// This works too. But above may be better.
+public class Solution2 {
+    public int climbStairs(int n) {
+        if (n <= 2) return n;
+        
+        int a = 1, b = 2, x = 0;
+        for (int i = 3; i <= n; ++ i) {
+            x = a + b;
+            a = b;
+            b = x;
+        }
+        
+        return x;
     }
 }
