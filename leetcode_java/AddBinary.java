f3e93a489b5233ff347d254af75defa5d0164207
Tom Chen <txchen@gmail.com>
2015-09-21 11:52:58 -1000

    Update AddBinary.java

--- leetcode_java/AddBinary.java
+++ leetcode_java/AddBinary.java
@@ -6,6 +6,46 @@
  * Return "100". 
  */
  
+// This works too. Is better.
+public class Solution {
+    private int carry;
+
+    public String addBinary(String a, String b) {
+        StringBuffer s = new StringBuffer("");
+        
+        int ia = a.length() - 1, ib = b.length() - 1;
+        
+        for (; ia >= 0 && ib >= 0; -- ia, -- ib) {
+            s.append("" + add(a.charAt(ia) - '0' + b.charAt(ib) - '0' + carry));
+        }
+        for (; ia >= 0; -- ia) {
+            s.append("" + add(a.charAt(ia) - '0' + carry));
+        }
+        for (; ib >= 0; -- ib) {
+            s.append("" + add(b.charAt(ib) - '0' + carry));
+        }
+        
+        if (carry > 0) {
+            s.append("" + carry);
+        }
+        
+        s.reverse();
+        return s.toString();
+    }
+    
+    private int add(int sum) {
+        if (sum > 1) {
+            sum -= 2;
+            carry = 1;
+        }
+        else {
+            carry = 0;
+        }
+
+        return sum;
+    }
+}
+
 // This works.
 public class Solution {
     private int sum;
