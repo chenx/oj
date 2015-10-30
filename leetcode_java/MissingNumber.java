9d23f188888f3e249eb5adadfd9b1f0bd343922c
Tom Chen <txchen@gmail.com>
2015-10-29 17:22:47 -1000

    Create MissingNumber.java

---
+++ leetcode_java/MissingNumber.java
@@ -0,0 +1,26 @@
+public class Solution {
+    public int missingNumber(int[] nums) {
+        int len = nums.length;
+        
+        int x = 0;
+        for (int i = 0; i < len; ++ i) {
+            x ^= (i+1);
+            x ^= nums[i];
+        }
+        
+        return x;
+    }
+}
+
+/**
+Missing Number
+Difficulty: Medium
+
+Given an array containing n distinct numbers taken from 0, 1, 2, ..., n, find the one that is missing from the array.
+
+For example,
+Given nums = [0, 1, 3] return 2.
+
+Note:
+Your algorithm should run in linear runtime complexity. Could you implement it using only constant extra space complexity? 
+ */
