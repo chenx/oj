88d67e0f9f42541e3109c2f12f14090059ed148e
Tom Chen <txchen@gmail.com>
2015-10-29 18:23:19 -1000

    Create DeleteNodeInALinkedList.cpp

---
+++ leetcode/DeleteNodeInALinkedList.cpp
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    void deleteNode(ListNode* node) {
+        if (! node || ! node->next) return;
+        
+        ListNode * nextNode = node->next;
+        node->val = nextNode->val;
+        node->next = nextNode->next;
+        delete nextNode;
+    }
+};
+
+
+/**
+Delete Node in a Linked List
+Difficulty: Easy
+
+Write a function to delete a node (except the tail) in a singly linked list, given only access to that node.
+
+Supposed the linked list is 1 -> 2 -> 3 -> 4 and you are given the third node with value 3, 
+the linked list should become 1 -> 2 -> 4 after calling your function. 
+ */
