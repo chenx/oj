3ef88615560310dcfc2efc040307c4e3d68c88dd
Tom Chen <txchen@gmail.com>
2015-11-01 00:46:04 -1000

    Update PowerOfTwo.java

--- leetcode_java/PowerOfTwo.java
+++ leetcode_java/PowerOfTwo.java
@@ -1,7 +1,13 @@
 public class Solution {
+    // This works too.
     public boolean isPowerOfTwo(int n) {
         if (n < 0) return false;
         return (n != 0) && ((n & (n-1)) == 0);
+    }
+    
+    // This seems better.
+    public boolean isPowerOfTwo(int n) {
+        return (n > 0) && (n & n-1) == 0;
     }
 }
 
