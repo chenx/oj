0037d19e3955270ad0fc4ed8ebf92636d43799a9
Tom Chen <txchen@gmail.com>
2015-11-01 20:59:51 -1000

    Create KthLargestElementInArray.cpp

---
+++ leetcode/KthLargestElementInArray.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        int len = nums.size();
+        findKth(nums, 0, len - 1, len - k);
+        return nums[len - k];
+    }
+    
+    void findKth(vector<int> & nums, int left, int right, int k) {
+        if (left >= right) return;
+        
+        swap(nums[left], nums[left + (right - left) / 2]);
+        int pivot = nums[left];
+        int L = left + 1, R = right;
+        
+        while (L <= R) {
+            while (L <= R && nums[L] < pivot) ++ L;
+            while (L <= R && nums[R] > pivot) -- R;
+            if (L < R) {
+                swap(nums[L], nums[R]);
+                ++ L; -- R;
+            }
+            else {
+                ++ L;
+            }
+        }
+        swap(nums[left], nums[R]);
+        
+        if (R < k) findKth(nums, R+1, right, k);
+        if (R > k) findKth(nums, left, R-1, k);
+    }
+    
+};
+
+
+/**
+Kth Largest Element in an Array
+Difficulty: Medium
+
+Find the kth largest element in an unsorted array. Note that it is the 
+kth largest element in the sorted order, not the kth distinct element.
+
+For example,
+Given [3,2,1,5,6,4] and k = 2, return 5.
+
+Note:
+You may assume k is always valid, 1 ≤ k ≤ array's length.
+ */
