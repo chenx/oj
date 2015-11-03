e8c6f7219ecaa06f162c66f97c87862122c90bd5
Tom Chen <txchen@gmail.com>
2015-11-03 00:04:45 -1000

    Update GasStation.cpp

--- leetcode/GasStation.cpp
+++ leetcode/GasStation.cpp
@@ -1,3 +1,23 @@
+// O(N). This also works. Seems the best.
+class Solution {
+public:
+    int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
+        int len = gas.size(), sum = 0, total = 0, index = 0;
+        for (int i = 0; i < len; ++ i) {
+            if (sum <= 0) {
+                index = i;
+                sum = 0;
+            }
+            
+            int gain = gas[i] - cost[i];
+            sum += gain;
+            total += gain;
+        }
+        
+        return total < 0 ? -1 : index;
+    }
+};
+
 // 
 // O(N). Also works.
 // The idea is: this can be converted to the maximum subarray sum problem: 
@@ -150,3 +170,19 @@ public:
         return -1;
     }
 };
+
+
+/**
+Gas Station
+Difficulty: Medium
+
+There are N gas stations along a circular route, where 
+the amount of gas at station i is gas[i].
+
+You have a car with an unlimited gas tank and it costs cost[i] of gas 
+to travel from station i to its next station (i+1). You begin the 
+journey with an empty tank at one of the gas stations.
+
+Return the starting gas station's index if you can travel around 
+the circuit once, otherwise return -1. 
+ */
