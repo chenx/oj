5061856705303bb8a232376c8941692ed44c88b3
Tom Chen <txchen@gmail.com>
2015-11-02 22:13:28 -1000

    Update BitwiseANDofNumbersRange.java

--- leetcode_java/BitwiseANDofNumbersRange.java
+++ leetcode_java/BitwiseANDofNumbersRange.java
@@ -1,3 +1,35 @@
+// This works too. Best so far.
+// Idea is: It's can be reduced to find the same prefix of the numbers in this range. 
+// From: https://leetcode.com/discuss/53646/simple-and-easy-to-understand-java-solution
+public class Solution {
+    public int rangeBitwiseAnd(int m, int n) {
+        int diffBits = 0;
+        while (m != n) {
+            m >>= 1;
+            n >>= 1;
+            diffBits ++;
+        }
+        return n << diffBits;
+    }
+}
+
+// This works too.
+// From: https://leetcode.com/discuss/56801/my-simple-recursive-solution-c-68ms
+// If we only consider the last bit of some successive numbers, 
+// the last bit of result of bitwise AND must be 0. So I recursive 
+// the problem into judge the last bit of range numbers, m and n. 
+// If m == n, return m; else if m != n, put a 0 after the result 
+// of rangeBitwiseAnd(m >> 1, n >> 1);
+public class Solution3 {
+    public int rangeBitwiseAnd(int m, int n) {
+        if (m == n){
+            return m ;
+        }
+        int res = rangeBitwiseAnd(m >> 1, n >> 1);
+        return res << 1;
+    }
+}
+
 // This works. 
 // From: https://leetcode.com/discuss/57971/c-64ms-2-simple-solutions 
 public class Solution {
