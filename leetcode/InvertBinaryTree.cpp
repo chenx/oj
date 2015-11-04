1bea809064269dc04f1c4c25ef42d1d1ad5c8044
Tom Chen <txchen@gmail.com>
2015-11-03 18:39:03 -1000

    Update InvertBinaryTree.cpp

--- leetcode/InvertBinaryTree.cpp
+++ leetcode/InvertBinaryTree.cpp
@@ -35,4 +35,10 @@ to
   7     2
  / \   / \
 9   6 3   1
+
+Trivia:
+This problem was inspired by this original tweet by Max Howell:
+
+    Google: 90% of our engineers use the software you wrote (Homebrew), 
+    but you can’t invert a binary tree on a whiteboard so fuck off.
  */
