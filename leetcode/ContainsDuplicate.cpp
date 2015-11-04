8cbe136b2c479e0c48e0d84bb50d9cf630146082
Tom Chen <txchen@gmail.com>
2015-11-03 18:46:06 -1000

    Update ContainsDuplicate.cpp

--- leetcode/ContainsDuplicate.cpp
+++ leetcode/ContainsDuplicate.cpp
@@ -1,3 +1,16 @@
+// This works too.
+class Solution2 {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_set<int> s;
+        for (int i = 0, len = nums.size(); i < len; ++ i) {
+            if (s.find(nums[i]) != s.end()) return true;
+            s.insert(nums[i]);
+        }
+        return false;
+    }
+};
+
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
