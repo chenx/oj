c79c4bba1c38ea05d178d44c4a1ddc2e9558d67d
Tom Chen <txchen@gmail.com>
2015-11-03 18:38:08 -1000

    Create InvertBinaryTree.java

---
+++ leetcode_java/InvertBinaryTree.java
@@ -0,0 +1,45 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) return null;
+        TreeNode tmp = invertTree(root.left);
+        root.left = invertTree(root.right);
+        root.right = tmp;
+        return root;
+    }
+}
+
+/**
+Invert Binary Tree
+Difficulty: Easy
+
+Invert a binary tree.
+
+     4
+   /   \
+  2     7
+ / \   / \
+1   3 6   9
+
+to
+
+     4
+   /   \
+  7     2
+ / \   / \
+9   6 3   1
+
+Trivia:
+This problem was inspired by this original tweet by Max Howell:
+
+    Google: 90% of our engineers use the software you wrote (Homebrew), 
+    but you can’t invert a binary tree on a whiteboard so fuck off.
+ */
