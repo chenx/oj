e42b2051b9f7579dd324eabcb139f5e2dc925010
Tom Chen <txchen@gmail.com>
2015-11-05 00:21:29 -1000

    Create RectangleArea.cpp

---
+++ leetcode/RectangleArea.cpp
@@ -0,0 +1,43 @@
+// See: https://leetcode.com/discuss/66048/5ms-java-in-a-readable
+class Solution {
+public:
+    int computeArea(int A, int B, int C, int D, int E, int F, int G, int H) {
+        int area1 = (C-A) * (D-B);
+        int area2 = (G-E) * (H-F);
+        int overlap = overlapArea(A, B, C, D, E, F, G, H);
+        return area1 + area2 - overlap;
+    }
+
+private:    
+    int overlapArea(int A, int B, int C, int D, int E, int F, int G, int H) {
+        int bottomX = max(A, E);
+        int bottomY = max(B, F);
+        int topX = min(C, G);
+        int topY = min(D, H);
+        
+        int area = 0;
+        if (topX > bottomX && topY > bottomY) {
+            area = (topX - bottomX) * (topY - bottomY);
+        }
+        return area;
+    }
+};
+
+
+/**
+Rectangle Area
+Difficulty: Easy
+
+Find the total area covered by two rectilinear rectangles in a 2D plane.
+
+Each rectangle is defined by its bottom left corner and top right corner as shown in the figure.
+
+(A, B) is for x, y coordinates of left bottom corner of the first rectangle.
+(C, D) is for x, y coordinates of upper right corner of the first rectangle.
+
+(E, F) is for x, y coordinates of left bottom corner of the second rectangle.
+(G, I) is for x, y coordinates of upper right corner of the second rectangle.
+
+Assume that the total area is never beyond the maximum possible value of int.
+
+ */
