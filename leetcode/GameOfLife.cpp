d6b60cbff18af6336e9f032020b172b69deb2f97
Tom Chen <txchen@gmail.com>
2015-11-05 17:04:01 -1000

    Update GameOfLife.cpp

--- leetcode/GameOfLife.cpp
+++ leetcode/GameOfLife.cpp
@@ -26,9 +26,9 @@ struct Coord {
 public:
     int x;
     int y;
-    Coord(int x, int y) {
-        this->x = x;
-        this->y = y;
+    Coord(int x, int y) : x(x), y(y) {
+        /*this->x = x;
+        this->y = y;*/
     }
 };
 
