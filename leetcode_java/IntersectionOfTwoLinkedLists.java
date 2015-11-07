404ffcbe0cb2ba6fb76d0f3ad94b02b17681f0e6
Tom Chen <txchen@gmail.com>
2015-11-07 11:54:39 -1000

    Create IntersectionOfTwoLinkedLists.java

---
+++ leetcode_java/IntersectionOfTwoLinkedLists.java
@@ -0,0 +1,57 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        int lenA = 0, lenB = 0;
+        for (ListNode n = headA; n != null; n = n.next) ++ lenA;
+        for (ListNode n = headB; n != null; n = n.next) ++ lenB;
+        
+        if (lenA > lenB) {
+            for (int ct = lenA - lenB; ct > 0; -- ct) headA = headA.next;
+        }
+        else {
+            for (int ct = lenB - lenA; ct > 0; -- ct) headB = headB.next;
+        }
+        
+        while (headA != headB) {
+            headA = headA.next;
+            headB = headB.next;
+        }
+        
+        return headA;        
+    }
+}
+
+/**
+Intersection of Two Linked Lists
+Difficulty: Easy
+
+Write a program to find the node at which the intersection of two singly linked lists begins.
+
+For example, the following two linked lists:
+
+A:          a1 → a2
+                   ↘
+                     c1 → c2 → c3
+                   ↗            
+B:     b1 → b2 → b3
+
+begin to intersect at node c1.
+
+Notes:
+
+    If the two linked lists have no intersection at all, return null.
+    The linked lists must retain their original structure after the function returns.
+    You may assume there are no cycles anywhere in the entire linked structure.
+    Your code should preferably run in O(n) time and use only O(1) memory.
+
+ */
