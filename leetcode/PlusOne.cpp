20c79e55c22870715740b80ad05319406fe14619
Tom Chen <txchen@gmail.com>
2015-11-07 11:46:21 -1000

    Update PlusOne.cpp

--- leetcode/PlusOne.cpp
+++ leetcode/PlusOne.cpp
@@ -29,3 +29,25 @@ public:
         return digits;        
     }
 };
+
+// This works too.
+class Solution2 {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        int len = digits.size(), carry = 1;
+        
+        for (int i = len - 1; i >= 0; -- i) {
+            digits[i] += carry;
+            if (digits[i] >= 10) {
+                digits[i] -= 10;
+                carry = 1;
+            }
+            else {
+                return digits;
+            }
+        }
+        
+        digits.insert(digits.begin(), 1);
+        return digits;
+    }
+};
