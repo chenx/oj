62a74967a91f3d544260fb748abc63b66daf6d94
Tom Chen <txchen@gmail.com>
2015-11-10 11:18:40 -1000

    Update Combinations.java

--- leetcode_java/Combinations.java
+++ leetcode_java/Combinations.java
@@ -26,6 +26,30 @@ public class Solution {
     }
 }
 
+// This works too. Iterative version.
+// From: https://leetcode.com/discuss/67405/java-my-iterative-13ms-and-recursive-7ms-solutions
+public class Solution {
+    public List<List<Integer>> combine(int n, int k) {
+        List<List<Integer>> list = new ArrayList<List<Integer>>();
+        List<Integer> kind = new ArrayList<Integer>();
+        int i = 1;
+        while(i<=n || kind.size()!=0){
+            if(kind.size()==k){
+                list.add(new ArrayList(kind));
+            }
+            if(i>n||kind.size()==k){
+                i = kind.get(kind.size()-1)+1;
+                kind.remove(kind.size()-1);
+            }
+            else{
+                kind.add(i);
+                i++;
+            }
+        }
+        return list;
+    }
+}
+
 /**
 Combinations
 Difficulty: Medium
