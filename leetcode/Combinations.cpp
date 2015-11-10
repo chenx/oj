2fa45e8694e8c9b64ec3a82f560699f7dc7423b1
Tom Chen <txchen@gmail.com>
2015-11-10 11:20:40 -1000

    Update Combinations.cpp

--- leetcode/Combinations.cpp
+++ leetcode/Combinations.cpp
@@ -8,6 +8,26 @@
 #include <vector>
 using namespace std;
 
+// This works too. Iterative version.
+class Solution2 {
+public:
+    vector<vector<int>> combine(int n, int k) {
+        vector<vector<int>> result;
+        int i = 0;
+        vector<int> p(k, 0);
+        while (i >= 0) {
+            p[i]++;
+            if (p[i] > n) --i;
+            else if (i == k - 1) result.push_back(p);
+            else {
+                ++i;
+                p[i] = p[i - 1];
+            }
+        }
+        return result;        
+    }
+};
+
 class Solution {
 public:
     vector<vector<int> > combine(int n, int k) {
