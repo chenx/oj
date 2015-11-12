c7beb3da79ca54517e95831fae7c9c58243f271d
Tom Chen <txchen@gmail.com>
2015-11-11 17:01:31 -1000

    Update UniquePaths.cpp

--- leetcode/UniquePaths.cpp
+++ leetcode/UniquePaths.cpp
@@ -45,14 +45,23 @@ public:
     }
 };
 
-/*
-Problem:
+/**
+Unique Paths
+Difficulty: Medium
 
-A robot is located at the top-left corner of a m x n grid (marked 'Start' in the diagram below).
+A robot is located at the top-left corner of a m x n grid 
+(marked 'Start' in the diagram below).
 
-The robot can only move either down or right at any point in time. The robot is trying to reach the bottom-right corner of the grid (marked 'Finish' in the diagram below).
+The robot can only move either down or right at any point in time. 
+The robot is trying to reach the bottom-right corner of the grid 
+(marked 'Finish' in the diagram below).
 
 How many possible unique paths are there?
+
+
+Above is a 3 x 7 grid. How many possible unique paths are there?
+
+Note: m and n will be at most 100.
  */
 
 // This is the case of can only go by the upper right half of the square.
