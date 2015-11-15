fd0d46a81a97ec155f72c58a50c4749acd1cf207
Tom Chen <txchen@gmail.com>
2015-11-15 01:04:26 -1000

    Update RemoveLinkedListElements.java

--- leetcode_java/RemoveLinkedListElements.java
+++ leetcode_java/RemoveLinkedListElements.java
@@ -28,12 +28,35 @@ public class Solution {
     }
 }
 
+// This works too.
+public class Solution2 {
+    public ListNode removeElements(ListNode head, int val) {
+        if (head == null) return null;
+        
+        ListNode h = head, prev = null;
+        
+        while (h != null) {
+            if (h.val == val) {
+                if (prev == null) h = head = h.next;
+                else h = prev.next = h.next;
+            }
+            else {
+                prev = h;
+                h = h.next;
+            }
+        }
+        
+        return head;
+    }
+}
+
 /**
-Remove Linked List Elements.
+Remove Linked List Elements
+Difficulty: Easy
 
 Remove all elements from a linked list of integers that have value val.
 
 Example
 Given: 1 --> 2 --> 6 --> 3 --> 4 --> 5 --> 6, val = 6
 Return: 1 --> 2 --> 3 --> 4 --> 5 
-*/
+ */
