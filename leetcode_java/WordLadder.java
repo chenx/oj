e338bc214fab330c616243036ff04441884bd3fb
Tom Chen <txchen@gmail.com>
2015-11-15 23:48:31 -1000

    Update WordLadder.java

--- leetcode_java/WordLadder.java
+++ leetcode_java/WordLadder.java
@@ -1,3 +1,49 @@
+// This works. 
+// Optimized from Solution, and passes all tests.
+// Note the use of toCharArray(), and let j be char type to avoid cast.
+public class Solution2 {
+    public int ladderLength(String beginWord, String endWord, Set<String> wordList) {
+        if (beginWord.equals(endWord)) return 1;
+        Queue<String> q = new LinkedList<String>(); // <string, distance>
+        HashMap<String, Integer> used = new HashMap<String, Integer>();
+        int dist = 0;
+        
+        q.add(beginWord);
+        used.put(beginWord, 1);
+        
+        while (! q.isEmpty()) {
+            String s = q.peek();
+            char[] sa = s.toCharArray(); // <--
+            int d = used.get(s); 
+            q.remove();
+            
+            for (int i = 0, len = s.length(); i < len; ++ i) {
+                char c = sa[i];
+                for (char j = 'a'; j < 'z'; ++ j) { // <--
+                    if (c == j) continue;
+
+                    sa[i] = j;
+                    String t = String.valueOf(sa); // <--
+
+                    if (t.equals(endWord)) {
+                        if (dist == 0 || dist > 1 + d) dist = 1 + d;
+                        //return dist; // this works too, but use the above for safe.
+                    }
+                    else {
+                        if (wordList.contains(t) && ! used.containsKey(t)) {
+                            q.add(t);
+                            used.put(t, 1 + d);
+                        }
+                    }
+                } // end of for 
+                sa[i] = c;
+            } // end of for
+        } // end of while
+        
+        return dist;
+    }
+}
+
 // This should work. But time out for large test.
 public class Solution {
     public int ladderLength(String beginWord, String endWord, Set<String> wordList) {
