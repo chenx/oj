3ef61b5e382bc1228476036901a2f10d18ae7750
Tom Chen <txchen@gmail.com>
2015-11-15 23:39:42 -1000

    Create WordLadder.java

---
+++ leetcode_java/WordLadder.java
@@ -0,0 +1,75 @@
+// This should work. But time out for large test.
+public class Solution {
+    public int ladderLength(String beginWord, String endWord, Set<String> wordList) {
+        if (beginWord.equals(endWord)) return 1;
+        Queue<String> q = new LinkedList<String>(); // <string, distance>
+        HashMap<String, Integer> used = new HashMap<String, Integer>();
+        int dist = 0;
+        
+        q.add(beginWord);
+        used.put(beginWord, 1);
+        
+        while (! q.isEmpty()) {
+            String s = q.peek();
+            int d = used.get(s); 
+            q.remove();
+            
+            for (int i = 0, len = s.length(); i < len; ++ i) {
+                char c = s.charAt(i);
+                for (int j = 0; j < 26; ++ j) {
+                    if (c == (char) ('a' + j)) continue;
+
+                    //s.setCharAt(i, (char) ('a' + j));
+                    s = s.substring(0, i) + ( (char) ('a' + j) ) + s.substring(i + 1);
+
+                    if (s.equals(endWord)) {
+                        if (dist == 0 || dist > 1 + d) {
+                            dist = 1 + d;
+                            return dist;
+                        }
+                    }
+                    else {
+                        if (wordList.contains(s) && ! used.containsKey(s)) {
+                            q.add(s);
+                            used.put(s, 1 + d);
+                        }
+                    }
+                } // end of for 
+                //s.setCharAt(i, c); // recover s[i].
+                s = s.substring(0, i) + c + s.substring(i + 1);
+            } // end of for
+        } // end of while
+        
+        return dist;        
+    }
+}
+
+
+/**
+Word Ladder
+Difficulty: Medium
+
+Given two words (beginWord and endWord), and a dictionary's word list, 
+find the length of shortest transformation sequence from beginWord to 
+endWord, such that:
+
+    Only one letter can be changed at a time
+    Each intermediate word must exist in the word list
+
+For example,
+
+Given:
+beginWord = "hit"
+endWord = "cog"
+wordList = ["hot","dot","dog","lot","log"]
+
+As one shortest transformation is "hit" -> "hot" -> "dot" -> "dog" -> "cog",
+return its length 5.
+
+Note:
+
+    Return 0 if there is no such transformation sequence.
+    All words have the same length.
+    All words contain only lowercase alphabetic characters.
+
+ */
