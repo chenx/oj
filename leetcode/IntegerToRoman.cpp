c651f3bf8012d971d77e538eb4ebe5eb9fbdd2bc
Tom Chen <txchen@gmail.com>
2015-11-29 17:12:23 -1000

    Update IntegerToRoman.cpp

--- leetcode/IntegerToRoman.cpp
+++ leetcode/IntegerToRoman.cpp
@@ -9,9 +9,9 @@
 class Solution3 {
 public:
     string intToRoman(int num) {
-        string s;
-        string roman[] = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
-        int val[] = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        static string roman[] = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+        static int val[] = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        string s = "";
         
         for (int i = 0, len = sizeof(val) / sizeof(int); i < len; ++ i) {
             eval(num, s, val[i], roman[i]);
