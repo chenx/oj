b2c73589d2d49c405611f09f6e81b326e5916360
Tom Chen <txchen@gmail.com>
2016-01-20 23:37:51 -1000

    Update EditDistance.cpp

--- leetcode/EditDistance.cpp
+++ leetcode/EditDistance.cpp
@@ -20,6 +20,29 @@
 #include <string>
 using namespace std;
 
+// This works too.
+class Solution4 {
+public:
+    int minDistance(string word1, string word2) {
+        int len1 = word1.length(), len2 = word2.length();
+        vector<vector<int>> v(len1+1, vector<int>(len2+1, 0));
+        
+        for (int j = 0; j <= len2; ++ j) v[0][j] = j;
+        
+        for (int i = 1; i <= len1; ++ i) {
+            v[i][0] = i;
+            for (int j = 1; j <= len2; ++ j) {
+                int add = 1 + v[i-1][j];
+                int del = 1 + v[i][j-1];
+                int rep = v[i-1][j-1] + (word1[i-1] == word2[j-1] ? 0 : 1); 
+                v[i][j] = min(rep, min(add, del));
+            }
+        }
+        
+        return v[len1][len2];
+    }
+};
+
 class Solution {
 public:
     int minDistance(string word1, string word2) {
@@ -123,3 +146,18 @@ int main() {
     
     return 0;
 }
+
+
+/**
+Edit Distance
+Difficulty: Hard
+
+Given two words word1 and word2, find the minimum number of steps required 
+to convert word1 to word2. (each operation is counted as 1 step.)
+
+You have the following 3 operations permitted on a word:
+
+a) Insert a character
+b) Delete a character
+c) Replace a character
+ */
