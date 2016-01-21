f6cee6526ec99763fec7d3bbcd3f104dcd801c72
Tom Chen <txchen@gmail.com>
2016-01-20 15:31:25 -1000

    Update BinaryTreeLevelOrderTraversal.cpp

--- leetcode/BinaryTreeLevelOrderTraversal.cpp
+++ leetcode/BinaryTreeLevelOrderTraversal.cpp
@@ -20,6 +20,37 @@
  http://www.cplusplus.com/reference/queue/queue/
  */
  
+// Better solutions.
+class Solution2 {
+public:
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        vector<vector<int>> ans;
+        if (! root) return ans;
+        
+        queue<TreeNode *> q;
+        q.push(root);
+        int ct = 1;
+        
+        while (! q.empty()) {
+            vector<int> v;
+            for (; ct > 0; -- ct) {
+                TreeNode * n = q.front();
+                q.pop();
+                v.push_back(n->val);
+                
+                if (n->left != NULL) q.push(n->left);
+                if (n->right != NULL) q.push(n->right);
+            }
+            
+            ans.push_back(v);
+            ct = q.size();
+        }
+        
+        return ans;
+    }
+};
+
+
 class Solution {
 public:
     vector<vector<int> > levelOrder(TreeNode *root) {
