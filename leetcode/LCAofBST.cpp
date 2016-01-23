6d3a20c1435cdda4329e7461fb7cf5fb16a33844
Tom Chen <txchen@gmail.com>
2016-01-22 23:20:28 -1000

    Update LCAofBST.cpp

--- leetcode/LCAofBST.cpp
+++ leetcode/LCAofBST.cpp
@@ -7,6 +7,20 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
+ 
+// This works too. Assume root, p, q all not null.
+class Solution2 {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if (p->val > q->val) return lowestCommonAncestor(root, q, p);
+        
+        if (root->val < p->val) return lowestCommonAncestor(root->right, p, q);
+        if (root->val > q->val) return lowestCommonAncestor(root->left, p, q);
+        return root;
+    }
+};
+
+
 class Solution {
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
