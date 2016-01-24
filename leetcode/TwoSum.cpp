4604027f9d04783e549c6c718812ee9a7ec4b488
Tom Chen <txchen@gmail.com>
2016-01-23 20:49:12 -1000

    Update TwoSum.cpp

--- leetcode/TwoSum.cpp
+++ leetcode/TwoSum.cpp
@@ -51,3 +51,61 @@ public:
         return ans;
     }
 };
+
+// Both functions in this class work. 1/23/2016
+class Solution3 {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> v;
+        
+        unordered_map<int, int> m; // (value, index)
+        for (int i = 0; i < nums.size(); ++ i) {
+            if (m.find(nums[i]) != m.end()) {
+                v.push_back(m[nums[i]]);
+                v.push_back(i+1);
+                break;
+            }
+            else {
+                m[target - nums[i]] = i + 1;
+            }
+        }
+        
+        return v;
+    }
+    
+    vector<int> twoSum2(vector<int>& nums, int target) {
+        vector<int> v;
+        
+        unordered_map<int, int> m; // (value, index)
+        for (int i = 0; i < nums.size(); ++ i) {
+            if (m.find(target - nums[i]) != m.end()) {
+                v.push_back(m[target - nums[i]]);
+                v.push_back(i+1);
+                break;
+            }
+            else {
+                m[nums[i]] = i + 1;
+            }
+        }
+        
+        return v;
+    }
+};
+
+
+/**
+Two Sum
+Difficulty: Medium
+
+Given an array of integers, find two numbers such that they add up 
+to a specific target number.
+
+The function twoSum should return indices of the two numbers such that 
+they add up to the target, where index1 must be less than index2. Please 
+note that your returned answers (both index1 and index2) are not zero-based.
+
+You may assume that each input would have exactly one solution.
+
+Input: numbers={2, 7, 11, 15}, target=9
+Output: index1=1, index2=2 
+ */
