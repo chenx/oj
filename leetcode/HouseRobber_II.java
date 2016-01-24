d34ac005c868102ed3942b034cbaedd72f533973
Tom Chen <txchen@gmail.com>
2016-01-23 20:34:08 -1000

    Update HouseRobber_II.java

--- leetcode/HouseRobber_II.java
+++ leetcode/HouseRobber_II.java
@@ -1,3 +1,29 @@
+// This works too. 1/23/2016.
+class Solution2 {
+public:
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        if (n == 0) return 0;
+        if (n == 1) return nums[0];
+        
+        vector<int> v1(n);
+        v1[0] = 0;
+        v1[1] = nums[0];
+        for (int i = 2; i <= n-1; ++ i) {
+            v1[i] = max(v1[i-1], v1[i-2] + nums[i-1]);
+        }
+        
+        vector<int> v2(n);
+        v2[0] = 0;
+        v2[1] = nums[1];
+        for (int i = 2; i <= n-1; ++ i) {
+            v2[i] = max(v2[i-1], v2[i-2] + nums[i]);
+        }
+        
+        return max(v1[n-1], v2[n-1]);
+    }
+};
+
 class Solution {
 public:
     int rob(vector<int>& nums) {
