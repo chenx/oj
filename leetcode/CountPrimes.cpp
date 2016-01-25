c6b391e96a092ccea95525f4e5bb5c0714af4b73
Tom Chen <txchen@gmail.com>
2016-01-24 22:32:41 -1000

    Update CountPrimes.cpp

--- leetcode/CountPrimes.cpp
+++ leetcode/CountPrimes.cpp
@@ -51,6 +51,29 @@ public:
     }
 };
 
+// This works too. Use the commented out version also good, which equates Solution2.
+class Solution3 {
+public:
+    int countPrimes(int n) {
+        if (n <= 2) return 0;
+        
+        vector<bool> v(n, true);
+
+        //for (int i = 2; i*i < n; ++ i) {
+        for (int i = 2; i < n; ++ i) {
+            if (v[i] == false) continue;
+            //for (int j = i*i; j < n; j += i) v[j] = false;
+            for (int j = 2*i; j < n; j += i) v[j] = false;
+        }
+
+        int ct = 0;        
+        for (int i = 2; i < n; ++ i) {
+            if (v[i] == true) ++ ct;
+        }
+        
+        return ct;
+    }
+};
 
 /**
 Count Primes
