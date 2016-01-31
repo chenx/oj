618541162e6eecebf37385819a3fca76bef41f36
Tom Chen <txchen@gmail.com>
2016-01-30 23:49:12 -1000

    Update BinaryTreePaths.cpp

--- leetcode/BinaryTreePaths.cpp
+++ leetcode/BinaryTreePaths.cpp
@@ -7,6 +7,34 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
+
+// This works too.
+class Solution2 {
+public:
+    vector<string> binaryTreePaths(TreeNode* root) {
+        vector<string> ans;
+        if (root == NULL) return ans;
+        getPath(ans, "", root);
+        return ans;
+    }
+    
+    void getPath(vector<string> &ans, string p, TreeNode *n) {
+        if (! n) return;
+        
+        if (p == "") p = to_string(n->val);
+        else p += "->" + to_string(n->val);
+        
+        if (n->left == NULL && n->right == NULL) {
+            ans.push_back(p);
+            return;
+        }
+        
+        getPath(ans, p, n->left);
+        getPath(ans, p, n->right);
+    }
+};
+
+
 class Solution {
 public:
     vector<string> binaryTreePaths(TreeNode* root) {
