c47c2ad8019da41966c80216121a2a8296bb8104
Tom Chen <txchen@gmail.com>
2016-01-30 23:24:42 -1000

    Create BestTimeToBuyAndSellStock_WithCooldown.cpp

---
+++ leetcode/BestTimeToBuyAndSellStock_WithCooldown.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        if(n <= 1) return 0;
+        vector<int> dp(n + 1);
+        int maxv = - prices[0];
+        for (int i = 1; i < n; i++) {
+            dp[i + 1] = max(dp[i], maxv + prices[i]);
+            maxv = max(dp[i - 1] - prices[i], maxv);
+        }
+    
+        return dp[n];
+    }
+};
+
+/**
+Best Time to Buy and Sell Stock with Cooldown
+Difficulty: Medium
+
+Say you have an array for which the ith element is the price of 
+a given stock on day i.
+
+Design an algorithm to find the maximum profit. You may complete 
+as many transactions as you like (ie, buy one and sell one share 
+of the stock multiple times) with the following restrictions:
+
+    You may not engage in multiple transactions at the same time 
+    (ie, you must sell the stock before you buy again).
+    
+    After you sell your stock, you cannot buy stock on next day. 
+    (ie, cooldown 1 day)
+
+Example:
+
+prices = [1, 2, 3, 0, 2]
+maxProfit = 3
+transactions = [buy, sell, cooldown, buy, sell]
+
+ */
