3f704e2a8eeaba3d3eea38423613d8a548830f44
Tom Chen <txchen@gmail.com>
2016-02-04 19:08:58 -1000

    Update LetterCombinationsOfAPhoneNumber.cpp

--- leetcode/LetterCombinationsOfAPhoneNumber.cpp
+++ leetcode/LetterCombinationsOfAPhoneNumber.cpp
@@ -5,33 +5,60 @@
 // @Last modified: 1/15/2013
 //
 
-class Solution {
+// Works. Clean. 2/4/2016.
+class Solution4 {
 public:
     vector<string> letterCombinations(string digits) {
-        vector<string> v;
-        if (digits == "") {
-            //v.push_back("");
-            return v;
-        }
-        
-        string mapping[] = {"0", "1", "abc", "def", "ghi",
-                        "jkl", "mno", "pqrs", "tuv", "wxyz"};
-        
-        string s = "";
-        comb(v, digits, s, 0, mapping);
-        return v;
+        vector<string> ans;
+        if (digits == "") return ans;
+        getComb(ans, digits, 0, "");
+        return ans;
     }
     
-    void comb(vector<string> &v, string &digits, 
-              string s, int pos, string mapping[]) {
+    void getComb(vector<string> &ans, string &digits, int pos, string s) {
         if (pos == digits.length()) {
-            v.push_back(s);
+            ans.push_back(s);
             return;
         }
         
-        string m = mapping[digits[pos] - '0'];
-        for (int i = 0; i < m.size(); i ++) {
-            comb(v, digits, s + m[i], pos + 1, mapping);
+        // This handles 0 and 1 consistently without other chars.
+        static string m[] = {"0", "1", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+        string chars = m[digits[pos] - '0'];
+        for (int i = 0; i < chars.length(); ++ i) {
+            getComb(ans, digits, pos + 1, s + chars[i]);
+        }
+    }
+};
+
+// This works too. But not so good since 0/1 are handled differently. 11/16/2015
+class Solution3 {
+public:
+    vector<string> letterCombinations(string digits) {
+        vector<string> ans;
+        if (digits == "") return ans;
+        
+        string s = "";
+        getComb(ans, digits, 0, s);
+        return ans;
+    }
+    
+    void getComb(vector<string> &ans, string &digits, int pos, string s) {
+        if (pos == digits.length()) {
+            ans.push_back(s);
+            return;
+        }
+        
+        static string m[] = {"abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+        
+        char c = digits[pos];
+        if (c >= '2' && c <= '9') {
+            string v = m[c - '2'];
+            for (int i = 0, len = v.length(); i < len; ++ i) {
+                getComb(ans, digits, pos + 1, s + v[i]);
+            }
+        } 
+        else {
+            getComb(ans, digits, pos + 1, s + digits[pos]);
         }
     }
 };
@@ -69,35 +96,49 @@ public:
 };
 
 
-// This works too. 11/16/2015
-class Solution3 {
+class Solution {
 public:
     vector<string> letterCombinations(string digits) {
-        vector<string> ans;
-        if (digits == "") return ans;
+        vector<string> v;
+        if (digits == "") return v;
+
+        string mapping[] = {"0", "1", "abc", "def", "ghi",
+                        "jkl", "mno", "pqrs", "tuv", "wxyz"};
         
         string s = "";
-        getComb(ans, digits, 0, s);
-        return ans;
+        comb(v, digits, s, 0, mapping);
+        return v;
     }
     
-    void getComb(vector<string> &ans, string &digits, int pos, string s) {
+    void comb(vector<string> &v, string &digits, 
+              string s, int pos, string mapping[]) {
         if (pos == digits.length()) {
-            ans.push_back(s);
+            v.push_back(s);
             return;
         }
         
-        static string m[] = {"abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
-        
-        char c = digits[pos];
-        if (c >= '2' && c <= '9') {
-            string v = m[c - '2'];
-            for (int i = 0, len = v.length(); i < len; ++ i) {
-                getComb(ans, digits, pos + 1, s + v[i]);
-            }
-        } 
-        else {
-            getComb(ans, digits, pos + 1, s + digits[pos]);
+        string m = mapping[digits[pos] - '0'];
+        for (int i = 0; i < m.size(); i ++) {
+            comb(v, digits, s + m[i], pos + 1, mapping);
         }
     }
 };
+
+
+/**
+Letter Combinations of a Phone Number
+Difficulty: Medium
+
+Given a digit string, return all possible letter combinations 
+that the number could represent.
+
+A mapping of digit to letters (just like on the telephone buttons) 
+is given below.
+
+Input:Digit string "23"
+Output: ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"].
+
+Note:
+Although the above answer is in lexicographical order, 
+your answer could be in any order you want. 
+ */
