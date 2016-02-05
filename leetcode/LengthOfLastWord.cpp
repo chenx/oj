0d17b7525bb6a8786a774cb48efaf03b5375e578
Tom Chen <txchen@gmail.com>
2016-02-04 18:55:47 -1000

    Update LengthOfLastWord.cpp

--- leetcode/LengthOfLastWord.cpp
+++ leetcode/LengthOfLastWord.cpp
@@ -5,25 +5,61 @@
 // @Last modified: 12/19/2012
 //
 
-class Solution {
+// Works. The best!
+class Solution5 {
+public:
+    int lengthOfLastWord(string s) {
+        int q = s.length() - 1;
+        while (q >= 0 && isspace(s[q])) -- q;
+        
+        if (q < 0) return 0; // all space.
+        
+        int p = q - 1;
+        while (p >= 0 && ! isspace(s[p])) -- p;
+        
+        return q - p;
+    }
+};
+
+
+// This works too.
+// Modified from Java version. May be the best, as no pointer is used.
+class Solution4 {
+public:
+    int lengthOfLastWord(string s) {
+        int p = s.length() - 1;
+        
+        for (; p >= 0 && s[p] == ' '; -- p) {}
+        if (p < 0) return 0; // all spaces.
+        
+        // now s[p] is last non-space char.
+        int q = p;
+        for (; q >= 0 && s[q] != ' '; -- q) {}
+        ++ q; // always increment q, no matter q < 0 or s[q] is space.
+        
+        return p - q + 1;        
+    }
+};
+
+
+//
+// This works too, and is shorter!
+//
+class Solution3 {
 public:
     int lengthOfLastWord(const char *s) {
-        if (s == NULL) return 0;
+        if (s == NULL || *s == '\0') return 0;
         
-        char * p = (char *) s, * q;
-
-        for (; *p == ' ' ; ++ p) {} // skip start spaces.
-        if (*p == 0) return 0;
+        const char * head = s;
         
-        q = p; // q is first non-space char.
-        for (++ p; *p; ++ p) {
-            if (*(p - 1) == ' ' && *p != ' ') q = p;
-        }
-        -- p; // p at the end.
+        while (*s) ++ s;
+        -- s;
         
-        for(; *p == ' '; -- p) ; // skip end spaces.
+        while (*s == ' ') -- s;
+        const char * end = s; // end of last word.
+        while (*s != ' ' && s >= head) -- s; // go to start of last word.
         
-        return p - q + 1;
+        return end - s;
     }
 };
 
@@ -57,42 +93,41 @@ public:
 };
 
 
-//
-// This works too, and is shorter!
-//
-class Solution3 {
+class Solution {
 public:
     int lengthOfLastWord(const char *s) {
-        if (s == NULL || *s == '\0') return 0;
+        if (s == NULL) return 0;
         
-        const char * head = s;
+        char * p = (char *) s, * q;
+
+        for (; *p == ' ' ; ++ p) {} // skip start spaces.
+        if (*p == 0) return 0;
         
-        while (*s) ++ s;
-        -- s;
+        q = p; // q is first non-space char.
+        for (++ p; *p; ++ p) {
+            if (*(p - 1) == ' ' && *p != ' ') q = p;
+        }
+        -- p; // p at the end.
         
-        while (*s == ' ') -- s;
-        const char * end = s; // end of last word.
-        while (*s != ' ' && s >= head) -- s; // go to start of last word.
+        for(; *p == ' '; -- p) ; // skip end spaces.
         
-        return end - s;
+        return p - q + 1;
     }
 };
 
-// This works too.
-// Modified from Java version. May be the best, as no pointer is used.
-class Solution4 {
-public:
-    int lengthOfLastWord(string s) {
-        int p = s.length() - 1;
-        
-        for (; p >= 0 && s[p] == ' '; -- p) {}
-        if (p < 0) return 0; // all spaces.
-        
-        // now s[p] is last non-space char.
-        int q = p;
-        for (; q >= 0 && s[q] != ' '; -- q) {}
-        ++ q; // always increment q, no matter q < 0 or s[q] is space.
-        
-        return p - q + 1;        
-    }
-};
+
+/**
+Length of Last Word
+Difficulty: Easy
+
+Given a string s consists of upper/lower-case alphabets and empty space 
+characters ' ', return the length of last word in the string.
+
+If the last word does not exist, return 0.
+
+Note: A word is defined as a character sequence consists of non-space characters only.
+
+For example,
+Given s = "Hello World",
+return 5. 
+ */
