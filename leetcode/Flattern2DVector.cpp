0474a02d3c632289250574dbc7af609adc89b73f
Tom Chen <txchen@gmail.com>
2016-02-08 18:47:10 -1000

    Update Flattern2DVector.cpp

--- leetcode/Flattern2DVector.cpp
+++ leetcode/Flattern2DVector.cpp
@@ -2,78 +2,75 @@
 // Also see: https://leetcode.com/discuss/questions/oj/flatten-2d-vector
 
 // This works too. Simpler.
- 1 class Vector2D {
- 2 public:
- 3     Vector2D(vector<vector<int>>& vec2d) {
- 4         int row = vec2d.size();
- 5         for (int r = 0; r < row; r++) {
- 6             int col = vec2d[r].size();
- 7             for (int c = 0; c < col; c++)
- 8                 data.push_back(vec2d[r][c]);
- 9         }
-10         idx = 0;
-11     }
-12 
-13     int next() {
-14         return data[idx++];
-15     }
-16 
-17     bool hasNext() {
-18         return idx < data.size();
-19     }
-20 private:
-21     vector<int> data;
-22     int idx;
-23 };
-24 
-25 /**
-26  * Your Vector2D object will be instantiated and called as such:
-27  * Vector2D i(vec2d);
-28  * while (i.hasNext()) cout << i.next();
-29  */
+class Vector2D2 {
+public:
+    Vector2D(vector<vector<int>>& vec2d) {
+        int row = vec2d.size();
+        for (int r = 0; r < row; r++) {
+            int col = vec2d[r].size();
+            for (int c = 0; c < col; c++)
+                data.push_back(vec2d[r][c]);
+        }
+        idx = 0;
+    }
+ 
+    int next() {
+        return data[idx++];
+    }
+ 
+    bool hasNext() {
+        return idx < data.size();
+    }
+private:
+    vector<int> data;
+    int idx;
+};
+ 
+ 
 
-// Works. 
- 1 class Vector2D {
- 2 public:
- 3     Vector2D(vector<vector<int>>& vec2d) {
- 4         data = vec2d;
- 5         r = c = 0;
- 6         row = vec2d.size();
- 7         col = (row == 0 ? -1 : data[r].size());
- 8         skipEmptyVector();
- 9     }
-10 
-11     int next() {
-12         int elem = data[r][c];
-13         if (c == col - 1) {
-14             r++;
-15             c = 0;
-16             col = data[r].size();
-17         }
-18         else c++;
-19         skipEmptyVector();
-20         return elem;
-21     }
-22 
-23     bool hasNext() {
-24         return col != -1 && (r < row && c < col);
-25     }
-26 private:
-27     vector<vector<int>> data;
-28     int row, col, r, c;
-29     void skipEmptyVector(void) {
-30         while (!col) {
-31             r++;
-32             col = data[r].size();
-33         }
-34     }
-35 };
-36 
-37 /**
-38  * Your Vector2D object will be instantiated and called as such:
-39  * Vector2D i(vec2d);
-40  * while (i.hasNext()) cout << i.next();
-41  */
+// Works. Tested.
+class Vector2D {
+public:
+    Vector2D(vector<vector<int>>& vec2d) {
+        data = vec2d;
+        r = c = 0;
+        row = vec2d.size();
+        col = (row == 0 ? -1 : data[r].size());
+        skipEmptyVector();
+    }
+ 
+    int next() {
+         int elem = data[r][c];
+         if (c == col - 1) {
+             r++;
+             c = 0;
+             col = data[r].size();
+         }
+         else c++;
+         skipEmptyVector();
+         return elem;
+    }
+
+    bool hasNext() {
+        return col != -1 && (r < row && c < col);
+    }
+    
+private:
+    vector<vector<int>> data;
+    int row, col, r, c;
+    void skipEmptyVector(void) {
+        while (!col) {
+            r++;
+            col = data[r].size();
+        }
+    }
+};
+
+/**
+ * Your Vector2D object will be instantiated and called as such:
+ * Vector2D i(vec2d);
+ * while (i.hasNext()) cout << i.next();
+ */
 
 /**
 Flatten 2D Vector
