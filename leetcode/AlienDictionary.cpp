d67dd26d4ea81f2c6a9cc9786705bdce5fbab305
Tom Chen <txchen@gmail.com>
2016-02-09 18:09:15 -1000

    Update AlienDictionary.cpp

--- leetcode/AlienDictionary.cpp
+++ leetcode/AlienDictionary.cpp
@@ -6,24 +6,25 @@ public:
         if (words.size() == 0) return "";
         if (words.size() == 1) return words[0];
         
-        bool e[26]={false};
-        int in[26]={0};
+        bool e[26] = {false}; //    vector<bool> e(26, false);
+        int in[26] = {0}; //    vector<int> in(26, 0);
         vector<vector<int>> E(26);
         int count=0;
 
         // find which letters exist, and count number of different letters.
-        for(int i=0;i<words.size();i++){
-            for(int j=0;j<words[i].size();j++){
+        for(int i=0;i<words.size();i++) {
+            for(int j=0;j<words[i].size();j++) {
                 if (e[words[i][j] - 'a'] == false) {
-                    e[words[i][j]-'a'] = true;
-                    count++;
+                    e[words[i][j] - 'a'] = true;
+                    count ++;
                 }
             }
         }
 
-        for (int i = 1; i < words.size(); ++ i){
-            for (int j=0, len = min(words[i-1].size(), words[i].size()); j < len; ++ j){
-                if (words[i-1][j] != words[i][j]){
+        // create edges between letters, and get in-degree for each letter.
+        for (int i = 1; i < words.size(); ++ i) {
+            for (int j=0, len = min(words[i-1].size(), words[i].size()); j < len; ++ j) {
+                if (words[i-1][j] != words[i][j]) {
                     E[words[i-1][j]-'a'].push_back(words[i][j]-'a'); // add edge
                     in[words[i][j]-'a']++; // increment in-degree
                     break;
@@ -32,22 +33,20 @@ public:
         }
 
         string R;
-        while(count--){
-            bool found=false;
-            for(int i=0;i<26;i++){
-                if(e[i]){
-                    if(in[i]==0){
-                        R.push_back('a'+i);
-                        e[i]=false;
-                        for(int j=0;j<E[i].size();j++){
-                            in[E[i][j]]--;
-                        }
-                        found=true;
-                        break;
+        while (count--) { // in each cycle, add those letters whose in-degree is 0 to result.
+            bool found = false;
+            for (int i=0;i<26;i++) {
+                if (e[i] && in[i]==0) {
+                    R += 'a' + i; //R.push_back('a'+i);
+                    e[i]=false;
+                    for(int j=0;j<E[i].size();j++) {
+                        in[E[i][j]]--;
                     }
+                    found=true;
+                    break;
                 }
             }
-            if(!found) return "";
+            if(!found) return ""; // conflict/cycle found. give up.
         }
         
         return R;
