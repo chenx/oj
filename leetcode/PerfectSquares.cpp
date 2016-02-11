889ab23403a96d1e839313d116a4ea6c497a8f22
Tom Chen <txchen@gmail.com>
2016-02-10 23:25:57 -1000

    Update PerfectSquares.cpp

--- leetcode/PerfectSquares.cpp
+++ leetcode/PerfectSquares.cpp
@@ -1,34 +1,36 @@
+// Works too.
 class Solution {
 public:
-    int numSquares(int n) {
+    int numSquares3(int n) {
         if (n <= 0) return 0;
-
-        vector<int> coins;
+        vector<int> cand;
         for (int i = 1; i * i <= n; ++ i) {
-            coins.push_back(i * i);
+            cand.push_back(i * i);
         }
         
-        vector<int> ans(n+1); // default to 0.
+        vector<int> v(1+n, 0);
         for (int i = 1; i <= n; ++ i) {
-            ans[i] = i;
-            for (int j = 0, lenj = coins.size(); j < lenj; ++ j) {
-                if (i == coins[j]) {
-                    ans[i] = 1;
+            for (int j = 0; j < cand.size(); ++ j) {
+                if (i < cand[j]) break;
+                else if (i == cand[j]) {
+                    v[i] = 1;
                     break;
                 }
-                else if (i > coins[j]) {
-                    if (ans[i - coins[j]] > 0) {
-                        ans[i] = min(ans[i], 1 + ans[i - coins[j]]);
+                else {
+                    if (v[i - cand[j]] > 0) {
+                        if (v[i] == 0 || v[i] > 1 + v[i - cand[j]]) {
+                            v[i] = 1 + v[i - cand[j]];
+                        }
                     }
                 }
             }
         }
         
-        return ans[n];
+        return v[n];
     }
 };
 
-// This works too. But not as good as Solution above.
+// This works too. But not as good as Solution below.
 class Solution2 {
 public:
     int numSquares(int n) {
@@ -51,6 +53,36 @@ public:
                     if (ans[i - coins[j]] > 0) {
                         if (ans[i] == 0) ans[i] = 1 + ans[i - coins[j]];
                         else ans[i] = min(ans[i], 1 + ans[i - coins[j]]);
+                    }
+                }
+            }
+        }
+        
+        return ans[n];
+    }
+};
+
+class Solution {
+public:
+    int numSquares(int n) {
+        if (n <= 0) return 0;
+
+        vector<int> coins;
+        for (int i = 1; i * i <= n; ++ i) {
+            coins.push_back(i * i);
+        }
+        
+        vector<int> ans(n+1); // default to 0.
+        for (int i = 1; i <= n; ++ i) {
+            ans[i] = i;
+            for (int j = 0, lenj = coins.size(); j < lenj; ++ j) {
+                if (i == coins[j]) {
+                    ans[i] = 1;
+                    break;
+                }
+                else if (i > coins[j]) {
+                    if (ans[i - coins[j]] > 0) {
+                        ans[i] = min(ans[i], 1 + ans[i - coins[j]]);
                     }
                 }
             }
