2efaea112affe2e66d6770fde34cb210c1c34c86
Tom Chen <txchen@gmail.com>
2016-02-11 18:31:58 -1000

    Create FactorCombinations.cpp

---
+++ leetcode/FactorCombinations.cpp
@@ -0,0 +1,91 @@
+// Works. Tested. Simplified from Solution.
+class Solution2 {
+public:
+    vector<vector<int>> getFactors(int n) {
+        vector<vector<int> > ans;
+        for (int i = 2; i * i <= n; i++) {
+            if (n % i != 0) {
+                continue;
+            }
+            ans.push_back(vector<int> {i, n / i});
+            auto tmp = getFactors(n / i); // tmp: vector<vector<int>>
+            for (auto j : tmp) {          // j: vector<int>
+                if (i <= j.front()) {
+                    j.insert(j.begin(), i);
+                    ans.push_back(j);
+                }
+            }
+        }
+        return ans;
+    }
+};
+
+// Works. Tested. From: https://leetcode.com/discuss/66903/share-my-short-c-solution
+class Solution {
+public:
+    vector<vector<int>> getFactors(int n) {
+        vector<vector<int> > ans;
+        for (int i = 2; i * i <= n; i++) {
+            if (n % i != 0) {
+                continue;
+            }
+            ans.push_back(vector<int> {i, n / i});
+            auto tmp = getFactors(n / i); // vector<vector<int>>
+            for (auto j : tmp) {          // j: vector<int>
+                j.push_back(i);
+                sort(j.begin(), j.end());
+                if (find(ans.begin(), ans.end(), j) == ans.end()) {
+                    ans.push_back(j);
+                }
+            }
+        }
+        return ans;
+    }
+};
+
+/**
+Factor Combinations
+Difficulty: Medium
+
+Numbers can be regarded as product of its factors. For example,
+
+8 = 2 x 2 x 2;
+  = 2 x 4.
+Write a function that takes an integer n and return all possible 
+combinations of its factors.
+
+Note: 
+Each combination's factors must be sorted ascending, for example: 
+The factors of 2 and 6 is [2, 6], not [6, 2].
+
+You may assume that n is always positive.
+Factors should be greater than 1 and less than n.
+
+Examples: 
+input: 1
+output: 
+[]
+
+input: 37
+output: 
+[]
+
+input: 12
+output:
+[
+  [2, 6],
+  [2, 2, 3],
+  [3, 4]
+]
+
+input: 32
+output:
+[
+  [2, 16],
+  [2, 2, 8],
+  [2, 2, 2, 4],
+  [2, 2, 2, 2, 2],
+  [2, 4, 4],
+  [4, 8]
+]
+ */
