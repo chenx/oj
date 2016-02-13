dd33eee34dac883a2ad63c5f0d003c4b2375992c
Tom Chen <txchen@gmail.com>
2016-02-12 21:22:47 -1000

    Update ReadNCharactersGivenRead4.cpp

--- leetcode/ReadNCharactersGivenRead4.cpp
+++ leetcode/ReadNCharactersGivenRead4.cpp
@@ -24,3 +24,17 @@ int read(char * buf, int n) {
     }
     return read_ct;
 }
+
+
+/**
+Read N Characters Given Read4 My Submissions Question
+Difficulty: Easy
+
+The API: int read4(char *buf) reads 4 characters at a time from a file.
+
+The return value is the actual number of characters read. For example, 
+it returns 3 if there is only 3 characters left in the file.
+
+By using the read4 API, implement the function int read(char *buf, int n) 
+that reads n characters from the file.
+ */
