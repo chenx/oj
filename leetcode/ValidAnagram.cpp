0fb272d0e2f82a1f653d51957943763bd2b950a9
Tom Chen <txchen@gmail.com>
2016-02-21 21:56:56 -1000

    Update ValidAnagram.cpp

--- leetcode/ValidAnagram.cpp
+++ leetcode/ValidAnagram.cpp
@@ -27,3 +27,17 @@ public:
     }    
 };
 
+
+/**
+Valid Anagram
+Difficulty: Easy
+
+Given two strings s and t, write a function to determine if t is an anagram of s.
+
+For example,
+s = "anagram", t = "nagaram", return true.
+s = "rat", t = "car", return false.
+
+Note:
+You may assume the string contains only lowercase alphabets.
+ */
