8a3c611324dacb218ba9af468d62b509c70601ed
Tom Chen <txchen@gmail.com>
2016-03-03 16:32:20 -1000

    Update GenerateParentheses.cpp

--- leetcode/GenerateParentheses.cpp
+++ leetcode/GenerateParentheses.cpp
@@ -5,6 +5,53 @@
 // @Last modified: 1/15/2013
 //
 
+// Works too. Tested. Most clean.
+class Solution3 {
+public:
+    vector<string> generateParenthesis(int n) {
+        vector<string> ans;
+        helper(ans, "", n, 0, 0);
+        return ans;
+    }
+    
+    void helper(vector<string> &ans, string s, int n, int L, int R) {
+        if (L == n && R == n) {
+            ans.push_back(s);
+            return;
+        }
+        
+        if (L > R) helper(ans, s + ")", n, L, R + 1);
+        if (L < n) helper(ans, s + "(", n, L + 1, R);
+    }
+};
+
+// This works too.
+class Solution2 {
+public:
+    vector<string> generateParenthesis(int n) {
+        vector<string> ans;
+        string s;
+        
+        generate(ans, s, 0, 0, n);
+        
+        return ans;
+    }
+    
+    void generate(vector<string> &ans, string s, int left, int right, int n) {
+        if (left == n && right == n) {
+            ans.push_back(s);
+            return;
+        }
+        
+        if (left > right) {
+            generate(ans, s + ")", left, right + 1, n);
+        }
+        if (left < n) {
+            generate(ans, s + "(", left + 1, right, n);
+        }
+    }
+};
+
 /*
    Notes:
    1) stack s is passed by value, i.e., copied. Can't use just 1.
@@ -47,30 +94,3 @@ public:
     }
 };
 
-
-// This works too.
-class Solution2 {
-public:
-    vector<string> generateParenthesis(int n) {
-        vector<string> ans;
-        string s;
-        
-        generate(ans, s, 0, 0, n);
-        
-        return ans;
-    }
-    
-    void generate(vector<string> &ans, string s, int left, int right, int n) {
-        if (left == n && right == n) {
-            ans.push_back(s);
-            return;
-        }
-        
-        if (left > right) {
-            generate(ans, s + ")", left, right + 1, n);
-        }
-        if (left < n) {
-            generate(ans, s + "(", left + 1, right, n);
-        }
-    }
-};
