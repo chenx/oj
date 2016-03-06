7b37c6cf6dca89e41367aa74f792f6b8c54b4d2c
Tom Chen <txchen@gmail.com>
2016-03-06 00:15:01 -1000

    Update UniqueBinarySearchTrees_II.cpp

--- leetcode/UniqueBinarySearchTrees_II.cpp
+++ leetcode/UniqueBinarySearchTrees_II.cpp
@@ -5,6 +5,40 @@
 // @Last modified: 12/24/2012
 //
 
+// Works too. Tested. 
+// Modified from Solution2 with more clean code.
+class Solution3 {
+public:
+    vector<TreeNode*> generateTrees(int n) {
+        if (n == 0) return vector<TreeNode *>();
+        return gen(1, n);
+    }
+    
+    vector<TreeNode*> gen(int L, int R) {
+        if (L > R) return vector<TreeNode*>(1, NULL);
+        if (L == R) return vector<TreeNode*>(1, new TreeNode(L));
+        
+        vector<TreeNode *> ans;
+        
+        for (int i = L; i <= R; ++ i) {
+            vector<TreeNode*> left = gen(L, i-1);
+            vector<TreeNode*> right = gen(i+1, R);
+            
+            for (int j = 0; j < left.size(); ++ j) {
+                for (int k = 0; k < right.size(); ++ k) {
+                    TreeNode * root = new TreeNode(i);
+                    root->left = left[j];
+                    root->right = right[k];
+                    ans.push_back(root);
+                }
+            }
+        }
+        
+        return ans;
+    }
+};
+
+
 /**
  * Definition for binary tree
  * struct TreeNode {
@@ -17,7 +51,7 @@
  * Recursive procedure.
  * Note that trees at the same level will share some of their children.
  */
-class Solution {
+class Solution2 {
 public:
     vector<TreeNode *> generateTrees(int n) {
         if (n == 0) { // This branch is needed by new version of leetcode.
@@ -57,7 +91,7 @@ public:
 
 // This generates the same tree set, but ordering is different.
 // So this can't pass the test. 11/4/2014
-class Solution2 {
+class Solution {
 public:
     vector<TreeNode *> generateTrees2(int n) {
         vector<vector<TreeNode *> > ans;
