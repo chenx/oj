f726ab121d7ecbc8b5e13e4f6136e32572e74978
Tom Chen <txchen@gmail.com>
2016-03-06 12:50:49 -1000

    Update MissingRanges.cpp

--- leetcode/MissingRanges.cpp
+++ leetcode/MissingRanges.cpp
@@ -1,8 +1,32 @@
+// Should work. Not tested.
+class Solution2 {
+public:
+    vector<string> findMissingRanges(vector<int>& A, int start, int end) {
+        vector<string> ans;
+        int prev = start - 1;
+        for (int i = 0; i < A.size(); ++ i) {
+            if (A[i] - prev > 1) {
+                ans.push_back(getRange(prev + 1, A[i] - 1));
+            }
+            prev = A[i];
+        }
+        if (prev < end) ans.push_back(prev + 1, end);
+        
+        return ans;
+    }
+    
+    string getRange(int start, int end) {
+        if (start == end) return to_string(start);
+        else return to_string(start) + "->" + to_string(end);
+    }
+};
+
 // This works. Tested.
 // From: http://www.danielbit.com/blog/puzzle/leetcode/leetcode-missing-ranges
 class Solution {
 public:
     vector<string> findMissingRanges(vector<int>& A, int start, int end) {
+        vector<string> ranges;
         int n = A.size();
         int prev = start - 1, cur;
         for (int i = 0; i <= n; ++ i) {
