0dd39e22036e9168591139220f070212c79d2084
Tom Chen <txchen@gmail.com>
2016-03-06 17:53:57 -1000

    Update MinimumDepthOfBinaryTree.cpp

--- leetcode/MinimumDepthOfBinaryTree.cpp
+++ leetcode/MinimumDepthOfBinaryTree.cpp
@@ -15,6 +15,24 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
+
+// Works too. Tested. Although Solution is preferred.
+class Solution2 {
+public:
+    int minDepth(TreeNode* root) {
+        if (root == NULL) return 0;
+        
+        int L = root->left == NULL ? INT_MAX : minDepth(root->left),
+            R = root->right == NULL ? INT_MAX : minDepth(root->right);
+        
+        int d = min(L, R);
+        if (d == INT_MAX) d = 0;
+        
+        return 1 + d;
+    }
+};
+
+// Works. Preferred answer.
 class Solution {
 public:
     int minDepth(TreeNode *root) {
@@ -26,7 +44,15 @@ public:
         if (root->left != NULL && root->right == NULL) return 1 + minDepth(root->left);
         return 1 + min(minDepth(root->left), minDepth(root->right));
     }
-    
-    int min(int a, int b) { return (a > b) ? b : a; }
 };
 
+
+/**
+Minimum Depth of Binary Tree
+Difficulty: Easy
+
+Given a binary tree, find its minimum depth.
+
+The minimum depth is the number of nodes along the shortest path 
+from the root node down to the nearest leaf node.
+ */
