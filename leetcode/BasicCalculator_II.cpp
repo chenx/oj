3dbfe8090bcf3750288eb1f2772ae289e69fdf36
Tom Chen <txchen@gmail.com>
2016-03-07 12:09:29 -1000

    Update BasicCalculator_II.cpp

--- leetcode/BasicCalculator_II.cpp
+++ leetcode/BasicCalculator_II.cpp
@@ -1,3 +1,86 @@
+// Works too. Best so far. Recursive descent.
+// Note ignoreSpace() is called only in num().
+class Solution2 {
+public:
+    int calculate(string s) {
+        const char * p = s.c_str();
+        return E(p);
+    }
+    
+    // E := F | F +- F
+    // F := T | T */ T
+    // T := num | (E)
+    int E(const char *& p) {
+        int n = F(p);
+        while (*p == '+' || *p == '-') {
+            char op = *p;
+            ++ p;
+            int m = F(p);
+            if (op == '+') n += m;
+            else n -= m;
+        }
+        return n;
+    }
+    
+    int F(const char *& p) {
+        int n = T(p);
+        while (*p == '*' || *p == '/') {
+            char op = *p;
+            ++ p;
+            int m = T(p);
+            if (op == '*') n *= m;
+            else {
+                if (m == 0) throw exception(); // divide by 0.
+                n /= m;
+            }
+        }
+        return n;
+    }
+
+    int T(const char *& p) {
+        if (isNum(p)) {
+            return num(p);
+        }
+        else {
+            expect(p, '(');
+            int n = E(p);
+            expect(p, ')');
+            return n;
+        }
+    }
+
+    int num(const char *& p) {
+        ignoreSpace(p);
+
+        int v = 0;
+        while (isDigit(*p)) {
+            v = v * 10 + *p - '0';
+            ++ p;
+        }
+        ignoreSpace(p);
+
+        return v;
+    }
+    
+    void expect(const char * p, char c) {
+        if (*p == c) return;
+        throw exception();
+    }
+    
+    bool isNum(const char * p) {
+        while (isspace(*p)) ++ p;
+        return isdigit(*p);
+    }
+    
+    bool isDigit(char c) {
+        return c >= '0' && c <= '9';
+    }
+    
+    void ignoreSpace(const char *& p) {
+        while (isspace(*p)) ++ p;
+    }
+};
+
 
 class Solution {
 public:
