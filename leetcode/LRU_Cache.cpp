d085846cee553e454a7f32f90f5ae92b863dc80c
Tom Chen <txchen@gmail.com>
2016-03-09 22:20:08 -1000

    Update LRU_Cache.cpp

--- leetcode/LRU_Cache.cpp
+++ leetcode/LRU_Cache.cpp
@@ -9,6 +9,126 @@ When the cache reached its capacity, it should invalidate the least recently
 used item before inserting a new item. 
  */
  
+// version 2. Works too. Tested.
+
+struct Node {
+    int key;
+    int val;
+    struct Node * prev;
+    struct Node * next;
+    Node(int k, int v) : key(k), val(v), prev(NULL), next(NULL) {}
+};
+
+class DLL {
+private:
+    Node * head;
+    Node * tail;
+    int _size;
+public:
+    DLL() : head(NULL), tail(NULL), _size(0) {}
+    
+    int size() {
+        return _size;
+    }
+    bool empty() {
+        return _size == 0;
+    }
+    
+    Node * front() {
+        return head;
+    }
+    
+    void pop_front() {
+        if (empty()) return;
+        remove(head);
+    }
+    
+    Node * push_back(int key, int val) {
+        Node * n = new Node(key, val);
+        
+        if (head == NULL) {
+            head = tail = n;
+        }
+        else {
+            n->next = tail;
+            tail->prev = n;
+            tail = n;
+        }
+        
+        _size ++;
+        
+        return n;
+    }
+    
+    void remove(Node * n) {
+        if (! n || _size == 0) return;
+        
+        if (_size == 1) {
+            if (n != head) return; // possible if n is an irrelevant node.
+            head = tail = NULL;
+        }
+        else {
+            if (n == head) {
+                head = head->prev;
+                head->next = NULL;
+            }
+            else if (n == tail) {
+                tail = tail->next;
+                tail->prev = NULL;
+            }
+            else {
+                n->prev->next = n->next;
+                n->next->prev = n->prev;
+            }
+        }
+        
+        delete n;
+        -- _size;
+    }
+};
+
+class LRUCache{
+private:
+    int _capacity;
+    DLL dll;
+    unordered_map<int, Node *> hash;
+    
+public:
+    LRUCache(int capacity) {
+        if (capacity < 0) capacity = 0; // or throw exception.
+        _capacity = capacity;
+    }
+    
+    int get(int key) {
+        if (hash.find(key) == hash.end()) return -1;
+        
+        Node * n = hash[key];
+        int val = n->val;
+        dll.remove(n);
+
+        hash[key] = dll.push_back(key, val);
+
+        return val;
+    }
+    
+    void set(int key, int value) {
+        if (hash.find(key) != hash.end()) {
+            dll.remove(hash[key]);
+        }
+        
+        hash[key] = dll.push_back(key, value);
+        
+        if (dll.size() > _capacity) {
+            hash.erase(dll.front()->key);
+            dll.pop_front();
+        }
+    }
+};
+
+
+//////////////////////////
+
+// Version 1. Works. Tested.
  
 #include <iostream>
 #include <map>
