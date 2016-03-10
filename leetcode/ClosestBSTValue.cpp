514e40b2ba9fb124bdfe502335ee651c1ea0feb5
Tom Chen <txchen@gmail.com>
2016-03-10 13:21:02 -1000

    Update ClosestBSTValue.cpp

--- leetcode/ClosestBSTValue.cpp
+++ leetcode/ClosestBSTValue.cpp
@@ -1,4 +1,30 @@
-// Should work. Not tested.
+// Should work. Not tested. O(logn).
+class Solution3 {
+public:
+    int closestValue(TreeNode* root, double target) {
+        if (root == NULL) return 0;
+        
+        double mdiff = abs(root->val - target);
+        int val = root->val;
+        helper(root, target, mdiff, val);
+        return val;
+    }
+    
+    void helper(TreeNode * root, double target, double &mdiff, int &val) {
+        if (root == NULL) return;
+        
+        double diff = abs(root->val - target);
+        if (diff < mdiff) {
+            mdiff = diff;
+            val = root->val;
+        }
+        
+        if (target < root->val) helper(root->left, target, mdiff, val);
+        else if (target > root->val) helper(root->right, target, mdiff, val);
+    }
+};
+
+// Should work. Not tested. O(n).
 class Solution2 {
 public:
     int closestValue(TreeNode* root, double target) {
@@ -19,7 +45,7 @@ public:
     }
 };
 
-// Works. Tested.
+// Works. Tested. O(n).
 class Solution {
 public:
     int closestValue(TreeNode* root, double target) {
