1cc13e3640334637a09697248812cff519988e10
Tom Chen <txchen@gmail.com>
2016-03-18 01:20:29 -1000

    Update MaxProductSubarray.cpp

--- leetcode/MaxProductSubarray.cpp
+++ leetcode/MaxProductSubarray.cpp
@@ -1,5 +1,29 @@
-// 10/16/2014
+// Works too. Tested.
+class Solution2 {
+public:
+    int maxProduct(vector<int>& nums) {
+        int n = nums.size();
+        if (n == 0) return 0;
+        
+        int maxp = INT_MIN, p = 1;
+        for (int i = 0; i < n; ++ i) {
+            if (p == 0) p = 1;
+            p *= nums[i];
+            maxp = max(maxp, p);
+        }
+        
+        p = 1;
+        for (int i = n-1; i >= 0; -- i) {
+            if (p == 0) p = 1;
+            p *= nums[i];
+            maxp = max(maxp, p);
+        }
+        
+        return maxp;
+    }
+};
 
+// 10/16/2014
 // Note: the array is separately by several segments by 0.
 // Get the max from each segment, then get the global max from local max.
 // You need a second round from back to front, for there may be neg products (e.g., -1, 3, 5).
