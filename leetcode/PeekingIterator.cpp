b5ceefa6a3e7162023603aa04cf4e3383f94dca6
Tom Chen <txchen@gmail.com>
2016-03-21 20:04:27 -1000

    Update PeekingIterator.cpp

--- leetcode/PeekingIterator.cpp
+++ leetcode/PeekingIterator.cpp
@@ -1,7 +1,42 @@
+// Solution 2. Works. Tested.
+class PeekingIterator : public Iterator {
+public:
+    PeekingIterator(const vector<int>& nums) : Iterator(nums) {
+	// Initialize any member here.
+	// **DO NOT** save a copy of nums and manipulate it directly.
+	// You should only use the Iterator interface methods.
+        _hasNext = Iterator::hasNext();
+        if (_hasNext) _data = Iterator::next();
+    }
+
+    // Returns the next element in the iteration without advancing the iterator.
+    int peek() {
+        return _data;
+    }
+
+    // hasNext() and next() should behave the same as in the Iterator interface.
+    // Override them if needed.
+    int next() {
+	int data = _data;
+	    
+	_hasNext = Iterator::hasNext();
+	if (_hasNext) _data = Iterator::next();
+	    
+        return data;
+    }
+
+    bool hasNext() const {
+       return _hasNext;
+    }
+private:
+    bool _hasNext;
+    int _data;
+};
+
 // Below is the interface for Iterator, which is already defined for you.
 // **DO NOT** modify the interface for Iterator.
 class Iterator {
-    struct Data;
+        struct Data;
 	Data* data;
 public:
 	Iterator(const vector<int>& nums);
