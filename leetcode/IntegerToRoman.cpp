2fadef940ae528407466a0bec606d74a983d480e
Tom Chen <txchen@gmail.com>
2016-03-25 15:31:36 -1000

    Update IntegerToRoman.cpp

--- leetcode/IntegerToRoman.cpp
+++ leetcode/IntegerToRoman.cpp
@@ -85,3 +85,13 @@ public:
         return s;
     }
 };
+
+
+/**
+Integer to Roman
+Difficulty: Medium
+
+Given an integer, convert it to a roman numeral.
+
+Input is guaranteed to be within the range from 1 to 3999.
+ */
