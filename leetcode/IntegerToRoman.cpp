27fddfc2baf1bb22f69b16ae23ff16ec4f3e5486
Tom Chen <txchen@gmail.com>
2016-03-25 15:36:53 -1000

    Update IntegerToRoman.cpp

--- leetcode/IntegerToRoman.cpp
+++ leetcode/IntegerToRoman.cpp
@@ -1,5 +1,23 @@
 //
-// http://www.leetcode.com/onlinejudge#
+// Symbol 	Value
+// I 	    1
+// V 	    5
+// X 	    10
+// L 	    50
+// C 	    100
+// D    	500
+// M    	1,000
+// 
+// Subtractive notation is often used as follows:
+//    I placed before V or X indicates one less, so four is IV (one less than five) 
+//        and nine is IX (one less than ten)
+//    X placed before L or C indicates ten less, so forty is XL (ten less than fifty) 
+//        and ninety is XC (ten less than a hundred)
+//    C placed before D or M indicates a hundred less, so four hundred is CD (a 
+//        hundred less than five hundred) and nine hundred is CM (a hundred less than a thousand)[5]
+//
+// See: https://en.wikipedia.org/wiki/Roman_numerals
+//
 // @Author: Xin Chen
 // @Created on: 12/24/2012
 // @Last modified: 12/24/2012
