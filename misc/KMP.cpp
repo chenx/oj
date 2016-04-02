2fa057f950f4a9916b9c36a11c82364af95c1977
Tom Chen <txchen@gmail.com>
2016-04-02 00:39:47 -1000

    Update KMP.cpp

--- misc/KMP.cpp
+++ misc/KMP.cpp
@@ -67,9 +67,71 @@ int kmpMatch(string T, string P) {
     return -1;
 }
 
+
+// this failure function is from:
+// https://github.com/chenx/oj/blob/master/leetcode/ShortestPalindrome.cpp
+// next size is s.length() + 1. 
+// In comparison, in getFailureFunc() next size is s.length().
+void getFailureFunc2(string s, vector<int> & next) {
+    int len = -1, i = 0;
+    next[0] = -1;
+
+    while (i < s.length()) {
+        if (len == -1 || s[len] == s[i]) {
+            ++ i;
+            ++ len;
+            next[i] = len;
+        }
+        else {
+            len = next[len];
+        }
+    }
+}
+
+int kmpMatch2(string T, string P) {
+    int n = T.length(), m = P.length();
+    if (n < m) return -1; // text is shorter than pattern.
+    if (m == 0) return 0; // pattern is empty string.
+
+    vector<int> F(m + 1, 0);
+    getFailureFunc2(P, F); //return -1;
+    int i = 0, j = 0;
+
+    while (i < n) {
+        if (T[i] == P[j]) {
+            if (j == m-1) {
+                return i -m + 1;
+            }
+            ++ i, ++ j;
+        }
+        else if (j > 0) {
+            j = F[j]; // F[j - 1];
+        }
+        else { // j == 0
+             ++ i;
+        }
+    }
+    return -1;
+}
+
+
 int main() {
     string T = "abacaabacabdd", P = "abacab";
     int k = kmpMatch(T, P);
     cout << "k = " << k << endl;
+
+    k = kmpMatch2(T, P);
+    cout << "kmp2: T: " << T << ", P: " << P << ". Match pos k = " << k << endl;
+
     return 0;
 }
+
+
+/**
+output:
+
+vec size = 6
+0 0 1 0 1 2 
+kmp1: T: abacaabacabdd, P: abacab. Match pos k = 5
+kmp2: T: abacaabacabdd, P: abacab. Match pos k = 5
+ */
