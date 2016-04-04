dcb9fbe19712b588fb464d65cc021cace7127bf2
Tom Chen <txchen@gmail.com>
2016-04-04 12:12:02 -1000

    Update ReadNCharactersGivenRead4.cpp

--- leetcode/ReadNCharactersGivenRead4.cpp
+++ leetcode/ReadNCharactersGivenRead4.cpp
@@ -1,3 +1,29 @@
+// Should work. Not tested.
+// Compared to Solution, this has less corner cases to consider.
+class Solution2 {
+public:
+    int readn(int n, char *buffer) { // max n bytes?
+        const int SIZE = 4096;
+        int a = n / SIZE, b = n % SIZE, ct = 0, read_ct = 0;
+        char * buf4k = new char[4096];
+
+        for (int i = 0; i < a; ++ i) {
+            ct = read4k(buf4k);
+            memcpy(buffer + read_ct, buf4k, ct);
+            read_ct += ct;
+            if (ct < SIZE) return read_ct;
+        }
+        
+        if (b > 0) {
+            ct = read4k(buf4k);
+            memcpy(buffer + read_ct, buf4k, ct);
+            read_ct += ct;
+        }
+        
+        return read_ct;
+    }
+};
+
 /**
  * Read N Characters Given Read4.
  * 
@@ -8,23 +34,25 @@
  */
 
 // Work. Tested.
-int read(char * buf, int n) {
-    int read_ct = 0;
-    char * buf4 = new char[4]; // or (char *) malloc(sizeof(char) * 4
-    bool eof = false;
-    while (! eof && read_ct < n) {
-        int ct = read4(buf4);
-        if (ct < 4) eof = true;
-        
-        ct = min(ct, n - read_ct);
-        if (ct > 0) {
-            memcpy(buf + read_ct, buf4, ct);
-            read_ct += ct;
+class Solution {
+public:
+    int read(char * buf, int n) {
+        int read_ct = 0;
+        char * buf4 = new char[4]; // or (char *) malloc(sizeof(char) * 4
+        bool eof = false;
+        while (! eof && read_ct < n) {
+            int ct = read4(buf4);
+            if (ct < 4) eof = true;
+            
+            ct = min(ct, n - read_ct);
+            if (ct > 0) {
+                memcpy(buf + read_ct, buf4, ct);
+                read_ct += ct;
+            }
         }
+        return read_ct;
     }
-    return read_ct;
-}
-
+};
 
 /**
 Read N Characters Given Read4 My Submissions Question
