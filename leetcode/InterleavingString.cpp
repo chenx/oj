7e465c9b0ef867224c7181999c745564ba160ce1
Tom Chen <txchen@gmail.com>
2016-04-05 13:07:05 -1000

    Update InterleavingString.cpp

--- leetcode/InterleavingString.cpp
+++ leetcode/InterleavingString.cpp
@@ -5,6 +5,38 @@
 // @Last modified: 1/8/2012
 //
 
+// Works. Maybe most clean of DP solution so far.
+class Solution8 {
+public:
+    bool isInterleave(string s1, string s2, string s3) {
+        int n1 = s1.length(), n2 = s2.length(), n3 = s3.length();
+        if (n1 + n2 != n3) return false;
+        if (n1 == 0) return s2 == s3;
+        if (n2 == 0) return s1 == s3;
+        
+        vector<vector<bool> > v(n1+1, vector<bool>(n2+1, false));
+        
+        for (int j = 0; j < n2; ++ j) {
+            if (s2[j] == s3[j]) v[0][j+1] = true;
+            else break;
+        }
+        for (int i = 0; i < n1; ++ i) {
+            if (s1[i] == s3[i]) v[i+1][0] = true;
+            else break;
+        }
+        
+        for (int i = 1; i <= n1; ++ i) {
+            for (int j = 1; j <= n2; ++ j) {
+                v[i][j] = (v[i-1][j] && s1[i-1] == s3[i-1+j]) ||
+                          (v[i][j-1] && s2[j-1] == s3[i+j-1]);
+            }
+        }
+        
+        return v[n1][n2];
+    }
+};
+
+
 #include <iostream>
 #include <string>
 #include <stack>
