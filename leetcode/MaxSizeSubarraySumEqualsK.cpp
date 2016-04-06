c56fd95cf75bd98cab08f7064af10e714b040630
Tom Chen <txchen@gmail.com>
2016-04-05 22:30:03 -1000

    Update MaxSizeSubarraySumEqualsK.cpp

--- leetcode/MaxSizeSubarraySumEqualsK.cpp
+++ leetcode/MaxSizeSubarraySumEqualsK.cpp
@@ -10,7 +10,7 @@ class Solution {
 public:
     int maxSubArrayLen(vector<int>& nums, int k) {
         unordered_map<int, int> m;
-        m[0] = -1;
+        m[0] = -1;  // for the case m[sum] = k.
         int sum = 0, maxLen = 0;
         for (int i = 0; i < nums.size(); i++) {
             sum += nums[i];
