4122c230cc2325c8598d97dad4f38f7d4efebc9f
Tom Chen <txchen@gmail.com>
2016-04-09 21:06:08 -1000

    Update ClosestBSTValue.cpp

--- leetcode/ClosestBSTValue.cpp
+++ leetcode/ClosestBSTValue.cpp
@@ -25,7 +25,7 @@ public:
 };
 
 
-// Should work. Not tested. O(n).
+// Should work. Tested locally, not in lc. O(n).
 class Solution2 {
 public:
     int closestValue(TreeNode* root, double target) {
@@ -43,6 +43,29 @@ public:
         }
         
         return val;
+    }
+    
+    vector<int> postorderTraversal(TreeNode *root) {
+        // IMPORTANT: Please reset any member data you declared, as
+        // the same Solution instance will be reused for each test case.
+        vector<int> ans;
+        if (! root) return ans;
+        
+        stack<TreeNode *> s;
+        s.push(root);
+        
+        while(! s.empty()) {
+            TreeNode * n = s.top();
+            s.pop();
+            
+            ans.push_back(n->val);
+            
+            if (n->left) s.push(n->left);
+            if (n->right) s.push(n->right);
+        }
+        
+        reverse(ans.begin(), ans.end());
+        return ans;
     }
 };
 
