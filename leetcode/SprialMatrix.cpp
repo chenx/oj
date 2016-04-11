4fc9cca9a87ffc9944e9dee0b07240cd96fb250e
Tom Chen <txchen@gmail.com>
2016-04-10 18:42:17 -1000

    Update SprialMatrix.cpp

--- leetcode/SprialMatrix.cpp
+++ leetcode/SprialMatrix.cpp
@@ -4,63 +4,38 @@
 // @Created on: 12/18/2012
 // @Last modified: 4/6/2012
 //
-class Solution {
-public:
-    vector<int> spiralOrder(vector<vector<int> > &matrix) {
-        vector<int> v;
-        if (matrix.size() == 0 || matrix[0].size() == 0) return v;
-        int x = 0, X = matrix.size() - 1, 
-            y = 0, Y = matrix[0].size() - 1, i, j;
-        while (x <= X && y <= Y) {
-            // print top row.
-            for (i = x, j = y; j <= Y; j ++) v.push_back(matrix[i][j]);
-            // print right edge.
-            for (i = x + 1, j = Y; i <= X; i ++) v.push_back(matrix[i][j]);
-            // print bottom row.
-            for (i = X, j = Y - 1; j >= y && i > x; j --) v.push_back(matrix[i][j]);
-            // print left edge.
-            for (i = X - 1, j = y; i > x && j < Y; i --) v.push_back(matrix[i][j]);
-    
-            x ++, X --, y ++, Y --;
-        }
-        return v;
-    }
-        
-};
 
 
 //
-// This works too and is more clear. 
+// Works. Best so far.  More clear. 
 // Difference from the first solution is take x == X and y == Y conditions out. 
 //
-class Solution2 {
+class Solution3 {
 public:
     vector<int> spiralOrder(vector<vector<int> > &matrix) {
-        vector<int> v;
-        if (matrix.size() == 0 || matrix[0].size() == 0) return v;
-        
-        int m = matrix.size();
-        int n = matrix[0].size();
+        vector<int> ans;
+        if (matrix.size() == 0 || matrix[0].size() == 0) return ans;
 
-        int x = 0, y = 0, X = m-1, Y = n-1, i, j;
+        int x = 0, y = 0, X = matrix.size() - 1, Y = matrix[0].size() - 1;
         
-        while (x <= X && y <= Y) {
-            for (i = x, j = y; j <= Y; ++ j) v.push_back(matrix[i][j]);
-            for (i = x+1, j = Y; i <= X; ++ i) v.push_back(matrix[i][j]);
+        while(x <= X && y <= Y) {
+            for (int i = x, j = y; j <= Y; ++ j) ans.push_back(matrix[i][j]);
+            for (int i = x+1, j = Y; i <= X; ++ i) ans.push_back(matrix[i][j]);
             if (x == X) break;
-            for (i = X, j = Y-1; j >= y; -- j) v.push_back(matrix[i][j]);
+            for (int i = X, j = Y-1; j >= y; -- j) ans.push_back(matrix[i][j]);
             if (y == Y) break;
-            for (i = X-1, j = y; i > x ; -- i)  v.push_back(matrix[i][j]);
+            for (int i = X-1, j = y; i > x; -- i) ans.push_back(matrix[i][j]);
             
-            ++ x, -- X, ++ y, -- Y;
+            ++ x, ++ y, -- X, -- Y;
         }
         
-        return v;
+        return ans;
     }
 };
 
+
 // This works too.
-class Solution3 {
+class Solution2 {
 public:
     vector<int> spiralOrder(vector<vector<int>>& matrix) {
         vector<int> v;
@@ -81,6 +56,30 @@ public:
             ++ x; -- X; ++ y; -- Y;
         }
         
+        return v;
+    }
+};
+
+
+class Solution {
+public:
+    vector<int> spiralOrder(vector<vector<int> > &matrix) {
+        vector<int> v;
+        if (matrix.size() == 0 || matrix[0].size() == 0) return v;
+        int x = 0, X = matrix.size() - 1, 
+            y = 0, Y = matrix[0].size() - 1, i, j;
+        while (x <= X && y <= Y) {
+            // print top row.
+            for (i = x, j = y; j <= Y; j ++) v.push_back(matrix[i][j]);
+            // print right edge.
+            for (i = x + 1, j = Y; i <= X; i ++) v.push_back(matrix[i][j]);
+            // print bottom row.
+            for (i = X, j = Y - 1; j >= y && i > x; j --) v.push_back(matrix[i][j]);
+            // print left edge.
+            for (i = X - 1, j = y; i > x && j < Y; i --) v.push_back(matrix[i][j]);
+    
+            x ++, X --, y ++, Y --;
+        }
         return v;
     }
 };
