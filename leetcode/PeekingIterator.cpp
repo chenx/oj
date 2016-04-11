077129ec6b3892e060de538bbd07a297f3bfb92f
Tom Chen <txchen@gmail.com>
2016-04-10 23:03:46 -1000

    Update PeekingIterator.cpp

--- leetcode/PeekingIterator.cpp
+++ leetcode/PeekingIterator.cpp
@@ -1,6 +1,43 @@
+// Works. Single out a getNext() method.
+class PeekingIterator3 : public Iterator {
+public:
+	PeekingIterator(const vector<int>& nums) : Iterator(nums) {
+	    // Initialize any member here.
+	    // **DO NOT** save a copy of nums and manipulate it directly.
+	    // You should only use the Iterator interface methods.
+        getNext();
+	}
+
+    // Returns the next element in the iteration without advancing the iterator.
+	int peek() {
+        return _data;
+	}
+
+	// hasNext() and next() should behave the same as in the Iterator interface.
+	// Override them if needed.
+	int next() {
+	    int val = _data;
+        getNext();
+	    return val;
+	}
+
+	bool hasNext() const {
+       return _hasNext;
+	}
+private:
+    bool _hasNext;
+    int  _data;
+    
+    void getNext() {
+        _hasNext = Iterator::hasNext();
+	    if (_hasNext) _data = Iterator::next();
+    }
+};
+
+
 // Solution 2. Works. Tested.
 // Slightly better function next().
-class PeekingIterator : public Iterator {
+class PeekingIterator2 : public Iterator {
 public:
     PeekingIterator(const vector<int>& nums) : Iterator(nums) {
 	// Initialize any member here.
