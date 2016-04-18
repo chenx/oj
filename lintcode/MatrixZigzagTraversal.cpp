89f61e86779d9424bd8efc41fbce7edf60670e67
Tom Chen <txchen@gmail.com>
2016-04-17 17:19:46 -1000

    Create MatrixZigzagTraversal.cpp

---
+++ lintcode/MatrixZigzagTraversal.cpp
@@ -0,0 +1,25 @@
+// See:
+// [1] UVA Problem 264 – Count on Cantor Solution
+//     https://quickgrid.wordpress.com/tag/zig-zag-2d-array-traversal/
+// [2] http://cboard.cprogramming.com/c-programming/124402-zigzag-traverse-matrix.html
+
+
+/**
+Matrix Zigzag Traversal
+
+Given a matrix of m x n elements (m rows, n columns), 
+return all elements of the matrix in ZigZag-order.
+
+Example
+
+Given a matrix:
+
+[
+  [1, 2,  3,  4],
+  [5, 6,  7,  8],
+  [9,10, 11, 12]
+]
+
+return [1, 2, 5, 9, 6, 3, 4, 7, 10, 11, 8, 12]
+
+ */
