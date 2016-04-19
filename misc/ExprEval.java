80d34f55847bb1fb1bcef0f99691f47db2eab61c
Tom Chen <txchen@gmail.com>
2016-04-19 11:31:26 -1000

    Update ExprEval.java

--- misc/ExprEval.java
+++ misc/ExprEval.java
@@ -14,13 +14,18 @@ public class ExprEval implements Evaluator {
 
     public Double evaluate(Node root) throws Exception {
         if (root == null) return 0.0;
+        callStack.push(root);
 
+        Double val;
         if (root.isOperator()) {
-            return calc(root.getOperator(), evaluate(root.left), evaluate(root.right));
+            val = calc(root.getOperator(), evaluate(root.left), evaluate(root.right));
         }
         else {
-            return root.getValue();
+            val = root.getValue();
         }
+
+        callStack.pop();
+        return val;
     }
 
     Double calc(char operator, double a, double b) throws Exception {
@@ -29,14 +34,31 @@ public class ExprEval implements Evaluator {
         else if (operator == '*') return a * b;
         else if (operator == '/') {
             //if (b == 0) throw new Exception("divide by zero");
-            if (Math.abs(b) < 0.0000001) throw new Exception("divide by zero");
+            if (Math.abs(b) < 0.0000001) exit("divide by zero");
             else return a / b;
         }
-        else {
-            throw new Exception("unknown operator");
-        }
+        else exit("unknown operator: " + operator);
+        
+        return 0.0;
     }
 
+    private void exit(String msg) {
+        dumpCallStack();
+        throw new Exception(msg);
+    }
+    // call stack, to display where error happens.
+    private Stack<Node> callStack;
+    private void dumpCallStack() {
+        System.out.println("call stack:");
+        while (! callStack.empty()) {
+           Node n = callStack.peek();
+           if (n.isOperator()) System.out.println( n.op );
+           else System.out.println( n.val );
+           callStack.pop();
+        }
+    }
+    public ExprEval() { callStack = new Stack<Node>(); }
+    
     void test1() {
         Node root = new Node('+');
         root.left = new Node('/');
@@ -44,8 +66,10 @@ public class ExprEval implements Evaluator {
         root.left.right = new Node(3);
         root.right = new Node('*');
         root.right.left = new Node(2);
-        root.right.right = new Node(3);
-
+        root.right.right = new Node('/');
+        root.right.right.left = new Node(3);
+        root.right.right.right = new Node(1);
+        
         try {
             double v = evaluate(root);
             double a = 10;
