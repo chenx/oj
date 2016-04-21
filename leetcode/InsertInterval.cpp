ebd5902770744c4fd04ab98b75eb1e361466a214
Tom Chen <txchen@gmail.com>
2016-04-21 11:03:11 -0700

    Update InsertInterval.cpp

--- leetcode/InsertInterval.cpp
+++ leetcode/InsertInterval.cpp
@@ -14,6 +14,69 @@
  *     Interval(int s, int e) : start(s), end(e) {}
  * };
  */
+ 
+/**
+ * Definition of Interval:
+ * class Interval {
+ * public:
+ *     int start, end;
+ *     Interval(int start, int end) {
+ *         this->start = start;
+ *         this->end = end;
+ *     }
+ * }
+ */
+
+// Works. Assume input intervals list is sorted with no overlapping.
+// http://www.lintcode.com/en/problem/insert-interval/
+class Solution2 {
+public:
+    /**
+     * Insert newInterval into intervals.
+     * @param intervals: Sorted interval list.
+     * @param newInterval: new interval.
+     * @return: A new interval list.
+     */
+    vector<Interval> insert(vector<Interval> &intervals, Interval newInterval) {
+        bool inserted = false;
+        for (int i = 0; i < intervals.size(); ++ i) {
+            if (newInterval.start <= intervals[i].start) {
+                intervals.insert(intervals.begin() + i, newInterval);
+                inserted = true;
+                break;
+            }
+        }
+        if (! inserted) intervals.push_back(newInterval);
+        
+        return merge(intervals);
+    }
+    
+    vector<Interval> merge(vector<Interval> &intervals) {
+        if (intervals.size() <= 1) return intervals;
+
+        vector<Interval> ans;
+        int n = intervals.size();
+        //sort(intervals.begin(), intervals.end(), comp);
+        
+        for (int i = 1; i < n; ++ i) {
+            Interval & a = intervals[i-1], & b = intervals[i];
+            if (a.end < b.start) ans.push_back(a);
+            else if (a.end <= b.end) b.start = a.start;
+            else {
+                b.start = a.start;
+                b.end = a.end;
+            }
+        }
+        ans.push_back(intervals[n-1]);
+        
+        return ans;
+    }
+    
+    static bool comp(const Interval & a, const Interval & b) {
+        return a.start < b.start;
+    }
+};
+
 
 bool comp(const Interval a, const Interval b) {
     return a.start < b.start;
