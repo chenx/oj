674fc922f71df8e2c6772f890c028c66eaeae916
Tom Chen <txchen@gmail.com>
2016-04-21 18:35:44 -0700

    Update BT_PostorderTraversal.cpp

--- leetcode/BT_PostorderTraversal.cpp
+++ leetcode/BT_PostorderTraversal.cpp
@@ -7,7 +7,7 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
-class Solution {
+class Solution2 {
 public:
     vector<int> postorderTraversal(TreeNode *root) {
         // IMPORTANT: Please reset any member data you declared, as
@@ -29,6 +29,39 @@ public:
         }
         
         reverse(ans.begin(), ans.end());
+        return ans;
+    }
+};
+
+
+class Solution {
+public:
+    vector<int> postorderTraversal(TreeNode* root) {
+        vector<int> ans;
+        
+        stack<TreeNode * > s;
+        TreeNode * n = root, * visited = root;
+        
+        while (n != NULL || ! s.empty()) {
+            while (n != NULL) {
+                s.push(n);
+                n = n->left;
+            }
+            
+            n = s.top();
+
+            if (n->right == NULL || n->right == visited) {
+                s.pop(); // do pop here!
+                ans.push_back(n->val); // and here!
+
+                visited = n;
+                n = NULL;
+            }
+            else {
+                n = n->right;
+            }
+        }
+        
         return ans;
     }
 };
