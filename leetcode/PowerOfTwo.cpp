8e74ebfd288a541fc009d0df56d568066959fdb6
Tom Chen <txchen@gmail.com>
2016-05-14 23:13:47 -1000

    Update PowerOfTwo.cpp

--- leetcode/PowerOfTwo.cpp
+++ leetcode/PowerOfTwo.cpp
@@ -1,3 +1,34 @@
+// Works too! This is good.
+class Solution4 {
+    const static int m = 1 << 31;
+public:
+    bool isPowerOfTwo(int n) {
+        return n > 0 && m % n == 0;
+    }
+};
+
+// Works too.
+class Solution3 {
+    const int m;
+public:
+    Solution() : m (1 << 31) { }
+    
+    bool isPowerOfTwo(int n) {
+        return n > 0 && m % n == 0;
+    }
+};
+
+// Works too.
+class Solution2 {
+    int m;
+public:
+    Solution() { m = 1 << 31; }
+    
+    bool isPowerOfTwo(int n) {
+        return n > 0 && m % n == 0;
+    }
+};
+
 //
 // Note: (n > 0) && (n & n-1 == 0) is incorrect, since it'll calculate n-1 == 0 first.
 //
