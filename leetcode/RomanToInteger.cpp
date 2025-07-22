bebfde693463b944949f1a1f0b9a11c8dc92d695
Tom Chen <txchen@gmail.com>
2025-07-21 23:50:26 -0700

    Update RomanToInteger.cpp

--- leetcode/RomanToInteger.cpp
+++ leetcode/RomanToInteger.cpp
@@ -9,6 +9,7 @@
 class Solution4 {
 public:
     int romanToInt(string s) {
+        // Hashmap is slow. Use function getV is faster.
         unordered_map<char, int> m = {
             {'I', 1}, {'V', 5}, {'X', 10}, {'L', 50}, 
             {'C', 100}, {'D', 500}, {'M', 1000},
