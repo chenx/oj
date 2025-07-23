aa6039888384adcc4f41d4a1dd0212d2d139df56
Tom Chen <txchen@gmail.com>
2025-07-23 01:07:11 -0700

    Update SearchInsertPosition.cpp

--- leetcode/SearchInsertPosition.cpp
+++ leetcode/SearchInsertPosition.cpp
@@ -10,15 +10,12 @@ public:
    int searchInsert(vector<int>& nums, int target) {
        int L = 0, R = nums.size() - 1;
 
-
        while (L <= R) {
            int mid = L + (R - L)/2;
            if (nums[mid] == target) return mid;
            else if (nums[mid] < target) ++ L;
            else -- R;
        }
-
-
        return L;
    }
 };
