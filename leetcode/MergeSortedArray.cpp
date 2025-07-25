a129778aa008ab095483d7061ef6785cedfb96e8
Tom Chen <txchen@gmail.com>
2025-07-24 22:38:12 -0700

    Update MergeSortedArray.cpp

--- leetcode/MergeSortedArray.cpp
+++ leetcode/MergeSortedArray.cpp
@@ -5,6 +5,25 @@
 // @Last modified: 12/18/2012
 //
 
+// Works too.
+class Solution2 {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int i = nums1.size() - 1;
+        for (-- m, -- n; m >= 0 && n >= 0; ) {
+            if (nums1[m] > nums2[n]) {
+                nums1[i --] = nums1[m --];
+            } else {
+                nums1[i --] = nums2[n --];
+            }
+        }
+
+        while (n >= 0) {
+            nums1[i --] = nums2[n --];
+        }
+    }
+};
+
 class Solution {
 public:
     void merge(int A[], int m, int B[], int n) {
@@ -16,7 +35,7 @@ public:
             else A[k --] = B[n --];
         }
         
-        //while (n >= 0) {} // do nothing
+        //while (m >= 0) {} // do nothing
         while (n >= 0) { A[k --] = B[n --]; }
     }
 };
