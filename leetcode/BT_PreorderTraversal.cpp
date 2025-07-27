847a223e12648a6ab386218a603d8f4bd086a8c4
Tom Chen <txchen@gmail.com>
2025-07-27 00:38:11 -0700

    Update BT_PreorderTraversal.cpp

--- leetcode/BT_PreorderTraversal.cpp
+++ leetcode/BT_PreorderTraversal.cpp
@@ -7,7 +7,7 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
-class Solution {
+class Solution2 {
 public:
     vector<int> preorderTraversal(TreeNode *root) {
         // IMPORTANT: Please reset any member data you declared, as
@@ -29,4 +29,20 @@ public:
         
         return ans;
     }
+};
+
+class Solution {
+Public:
+   vector<int> preorderTraversal2(TreeNode* root) {
+       vector<int> result;
+       pre(root, result);
+       return result;
+   }
+
+   void pre(TreeNode* root, vector<int>& result) {
+       if (! root) return;
+       result.push_back(root->val);
+       pre(root->left, result);
+       pre(root->right, result);
+   }
 };
