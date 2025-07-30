4f475a8676d1c7117757e84e5e52652f0e8bcc77
Tom Chen <txchen@gmail.com>
2025-07-29 23:28:30 -0700

    Update MeetingRooms.cpp

--- leetcode/MeetingRooms.cpp
+++ leetcode/MeetingRooms.cpp
@@ -8,6 +8,25 @@
  * };
  */
 
+// Works.
+class Solution3 {
+public:
+    bool canAttendMeetings(vector<vector<int>>& intervals) {
+        if (intervals.empty()) return true;
+        sort(intervals.begin(), intervals.end());
+        // sort(intervals.begin(), intervals.end(), comp);
+
+        for (int i = 0; i < intervals.size() - 1; i ++) {
+            if (intervals[i][1] > intervals[i+1][0]) return false;
+        }
+        return true;
+    }
+
+    static bool comp(const vector<int>& a, const vector<int>& b) {
+        return a[0] < b[0];
+    }
+};
+
 // Works too.
 class Solution2 {
 public:
