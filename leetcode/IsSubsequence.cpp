3afa500ba448aa0157d624e30c3442b93f76c71b
Tom Chen <txchen@gmail.com>
2025-08-02 22:49:08 -0700

    Create IsSubsequence.cpp

---
+++ leetcode/IsSubsequence.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        if (s == "") return true;
+
+        int j = 0;
+        for (int i = 0; i < t.length(); i ++) {
+            if (t[i] == s[j]) {
+                j ++;
+                if (j == s.length()) return true;
+            }
+        }
+        return false;
+    }
+};
+
+/*
+Given two strings s and t, return true if s is a subsequence of t, or false otherwise.
+
+A subsequence of a string is a new string that is formed from the original string by 
+deleting some (can be none) of the characters without disturbing the relative positions 
+of the remaining characters. (i.e., "ace" is a subsequence of "abcde" while "aec" is not).
+
+Example 1:
+Input: s = "abc", t = "ahbgdc"
+Output: true
+
+Example 2:
+Input: s = "axc", t = "ahbgdc"
+Output: false
+*/
