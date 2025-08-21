2238446039656d9f869504fd670b1af6ce05d2f1
Tom Chen <txchen@gmail.com>
2025-08-20 23:01:34 -0700

    Update ConvertSortedListToBinarySearchTree.cpp

--- leetcode/ConvertSortedListToBinarySearchTree.cpp
+++ leetcode/ConvertSortedListToBinarySearchTree.cpp
@@ -23,6 +23,30 @@
  * };
  */
 
+// Works.
+class Solution6 {
+public:
+    TreeNode* sortedListToBST(ListNode* head) {
+        if (!head) return NULL;
+        if (!head->next) return new TreeNode(head->val);
+
+        ListNode *fast = head, *slow = head, *prev = NULL;
+
+        while(fast && fast->next) {
+            prev = slow;
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+
+        TreeNode* root = new TreeNode(slow->val);
+        prev->next = NULL;
+        
+        root->left = sortedListToBST(head);
+        root->right = sortedListToBST(slow->next);
+        return root;
+    }
+};
+
 // Works. Best answer so far. No prev pointer, no dummy head.
 class Solution5 {
 public:
