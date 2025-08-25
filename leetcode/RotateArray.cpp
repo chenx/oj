4628ef9d8a0dc0aae0c1b9c419b8f4c27ab498d2
Tom Chen <txchen@gmail.com>
2025-08-25 15:57:42 -0700

    Update RotateArray.cpp

--- leetcode/RotateArray.cpp
+++ leetcode/RotateArray.cpp
@@ -1,3 +1,24 @@
+// Works.
+class Solution4 {
+public:
+   void rotate(vector<int>& nums, int k) {
+       int n = nums.size();
+       k = k % n;
+       if (k == 0) return;
+
+       reverse(nums, 0, n-1);
+       reverse(nums, 0, k-1);
+       reverse(nums, k, n-1);
+   }
+
+
+   void reverse(vector<int>& nums, int i, int j) {
+       while (i < j) {
+           swap(nums[i ++], nums[j --]);
+       }
+   }
+};
+
 class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
