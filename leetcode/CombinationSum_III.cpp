27d1719a80706a1433d81067cff77a18ecae483e
Tom Chen <txchen@gmail.com>
2025-09-03 16:35:30 -0700

    Update CombinationSum_III.cpp

--- leetcode/CombinationSum_III.cpp
+++ leetcode/CombinationSum_III.cpp
@@ -1,5 +1,39 @@
+// Works. From https://leetcode.com/problems/combination-sum-iii/editorial/
+class Solution4 {
+public:
+    void backtrack(int remain, int k, vector<int>& comb, int next_start,
+                   vector<vector<int>>& results) {
+        if (remain == 0 && comb.size() == k) {
+            // Note: it's important to make a deep copy here by using push_back,
+            // which copies 'comb' to 'results'. This prevents modifications to
+            // 'comb' in other branches of backtracking from affecting the
+            // stored combinations.
+            results.push_back(comb);
+            return;
+        } else if (remain < 0 || comb.size() == k) {
+            // Exceed the scope, no need to explore further.
+            return;
+        }
+
+        // Iterate through the reduced list of candidates.
+        for (int i = next_start; i < 9; ++i) {
+            comb.push_back(i + 1);
+            this->backtrack(remain - i - 1, k, comb, i + 1, results);
+            comb.pop_back();
+        }
+    }
+
+    vector<vector<int>> combinationSum3(int k, int n) {
+        vector<vector<int>> results;
+        vector<int> comb;
+
+        this->backtrack(n, k, comb, 0, results);
+        return results;
+    }
+};
+
 // Works. Same as CombinationSum.cpp with minor changes 1), 2) and 3) as below.
-class Solution {
+class Solution3 {
 public:
     vector<vector<int>> combinationSum3(int k, int n) {
         vector<int> candidates;
@@ -42,7 +76,7 @@ public:
 
 
 // Works. Tested. Cleaned from Solution.
-class Solution {
+class Solution2 {
 public:
     vector<vector<int>> combinationSum3(int k, int n) {
         vector<vector<vector<int>>> ans(n+1);
