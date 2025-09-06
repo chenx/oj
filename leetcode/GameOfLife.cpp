616fc2af991959537dd1002f11d2b3abdc5de9e2
Tom Chen <txchen@gmail.com>
2025-09-06 16:53:03 -0700

    Update GameOfLife.cpp

--- leetcode/GameOfLife.cpp
+++ leetcode/GameOfLife.cpp
@@ -1,3 +1,45 @@
+// Works. Adapted from Solution3.
+class Solution4 {
+    vector<vector<int>> dirs = {
+        {-1, -1}, {0, -1}, {1, -1},
+        {-1,  0},          {1,  0},
+        {-1,  1}, {0,  1}, {1,  1}
+    };
+
+    int getSum(vector<vector<int>>& board, int m, int n, int i, int j) {
+        int sum = 0;
+        for (int k = 0; k < dirs.size(); ++ k) {
+            int x = i + dirs[k][0], y = j + dirs[k][1];
+            if (x >= 0 && x < m && y >= 0 && y < n) {
+                sum += board[x][y] & 1;
+            }
+        }
+        return sum;
+    }
+public:
+    void gameOfLife(vector<vector<int>>& board) {
+        if (board.size() == 0 || board[0].size() == 0) return;
+        int m = board.size(), n = board[0].size();
+
+        for (int i = 0; i < m; ++ i) {
+            for (int j = 0; j < n; ++ j) {
+                int sum = getSum(board, m, n, i, j);
+                if (board[i][j] && 1) {
+                    if (sum == 2 || sum == 3) board[i][j] |= 2;
+                } else {
+                    if (sum == 3) board[i][j] |= 2;
+                }
+            }
+        }
+
+        for (int i = 0; i < m; ++ i) {
+            for (int j = 0; j < n; ++ j) {
+                board[i][j] >>= 1;
+            }
+        }
+    }
+};
+
 // Works. Tested. O(1) space. Best solution. Use high bit to store new life value.
 class Solution3 {
 public:
