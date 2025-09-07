b8b87138ae47559610f1ea6e7fec4059ae7c73aa
Tom Chen <txchen@gmail.com>
2025-09-06 18:32:03 -0700

    Update SmallestRectangleEnclosingBlackPixels.cpp

--- leetcode/SmallestRectangleEnclosingBlackPixels.cpp
+++ leetcode/SmallestRectangleEnclosingBlackPixels.cpp
@@ -1,39 +1,37 @@
 // Works. 
 // O(mlog(n) + nlog(m)) time.
 // From: https://leetcode.com/problems/smallest-rectangle-enclosing-black-pixels/editorial/
+// queue: front(), push(), pop();
+// deque: front(), push_back(), pop_front(), push_front(), pop_back()
 class Solution3 {
 public:
     int minArea(vector<vector<char>>& image, int x, int y) {
+        if (image.size() == 0 || image[0].size() == 0) return 0;
         int m = image.size(), n = image[0].size();
-        int left = searchColumns(image, 0, y, 0, m, true);
-        int right = searchColumns(image, y + 1, n, 0, m, false);
-        int top = searchRows(image, 0, x, left, right, true);
-        int bottom = searchRows(image, x + 1, m, left, right, false);
-        return (right - left) * (bottom - top);
-    }
+        int minx = INT_MAX, maxx = INT_MIN, miny = INT_MAX, maxy = INT_MIN;
+        vector<int> dirs = {-1, 0, 1, 0, -1};
 
-    int searchColumns(vector<vector<char>>& image, int i, int j, int top, int bottom, bool whiteToBlack) {
-        while (i != j) {
-            int k = top, mid = (i + j) / 2;
-            while (k < bottom && image[k][mid] == '0') ++k;
-            if (k < bottom == whiteToBlack) // k < bottom means the column mid has black pixel
-                j = mid; //search the boundary in the smaller half
-            else
-                i = mid + 1; //search the boundary in the greater half
-        }
-        return i;
-    }
+        deque<vector<int>> q;
+        q.push_back({x, y});
 
-    int searchRows(vector<vector<char>>& image, int i, int j, int left, int right, bool whiteToBlack) {
-        while (i != j) {
-            int k = left, mid = (i + j) / 2;
-            while (k < right && image[mid][k] == '0') ++k;
-            if (k < right == whiteToBlack) // k < right means the row mid has black pixel
-                j = mid;
-            else
-                i = mid + 1;
+        while (! q.empty()) {
+            vector<int> p = q.front();
+            q.pop_front();
+            image[p[0]][p[1]] = '.';
+
+            minx = min(minx, p[0]);
+            maxx = max(maxx, p[0]);
+            miny = min(miny, p[1]);
+            maxy = max(maxy, p[1]);
+
+            for (int k = 0; k < 4; ++ k) {
+                int x = p[0] + dirs[k], y = p[1] + dirs[k+1];
+                if (x >= 0 && x < m && y >= 0 && y < n && image[x][y] == '1') {
+                    q.push_back({x, y});
+                }
+            }
         }
-        return i;
+        return (maxx - minx + 1) * (maxy - miny + 1);
     }
 };
 
