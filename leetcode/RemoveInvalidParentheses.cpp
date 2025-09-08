c15f46aa25e257d14e4224cc50c54f90e177c241
Tom Chen <txchen@gmail.com>
2025-09-07 22:29:58 -0700

    Update RemoveInvalidParentheses.cpp

--- leetcode/RemoveInvalidParentheses.cpp
+++ leetcode/RemoveInvalidParentheses.cpp
@@ -1,3 +1,70 @@
+// Works. From https://leetcode.com/problems/remove-invalid-parentheses/editorial/
+class Solution3 {
+    set<string> validExpressions;
+
+    void recurse(string& s, int index, int leftCount, int rightCount, int leftRem, int rightRem,
+      string& expression) {
+        // If we reached the end of the string, just check if the resulting expression is
+        // valid or not and also if we have removed the total number of left and right
+        // parentheses that we should have removed.
+        if (index == s.length()) {
+            if (leftRem == 0 && rightRem == 0) {
+                validExpressions.insert(expression);
+            }
+            return;
+        } 
+        
+        char character = s[index];
+        int length = expression.length();
+
+        // The discard case. Note that here we have our pruning condition.
+        // We don't recurse if the remaining count for that parenthesis is == 0.
+        if ((character == '(' && leftRem > 0) || (character == ')' && rightRem > 0)) {
+            recurse(s, index + 1, leftCount, rightCount,
+                    leftRem - (character == '(' ? 1 : 0),
+                    rightRem - (character == ')' ? 1 : 0),
+                    expression);
+        }
+        expression += character;
+
+        // Simply recurse one step further if the current character is not a parenthesis.
+        if (character != '(' && character != ')') {
+            recurse(s, index + 1, leftCount, rightCount, leftRem, rightRem, expression);
+        } else if (character == '(') {
+            // Consider an opening bracket.
+            recurse(s, index + 1, leftCount + 1, rightCount, leftRem, rightRem, expression);
+        } else if (rightCount < leftCount) {
+            // Consider a closing bracket.
+            recurse(s, index + 1, leftCount, rightCount + 1, leftRem, rightRem, expression);
+        }
+
+        // Delete for backtracking.
+        expression = expression.substr(0, expression.length() - 1);
+    }
+public:
+    vector<string> removeInvalidParentheses(string s) {
+        int left = 0, right = 0;
+        // First, we find out the number of misplaced left and right parentheses.
+        for (int i = 0; i < s.length(); i++) {
+            // Simply record the left one.
+            if (s[i] == '(') {
+                left ++;
+            } else if (s[i] == ')') {
+                // If we don't have a matching left, then this is a misplaced right, record it.
+                right = left == 0 ? right + 1 : right;
+
+                // Decrement count of left parentheses because we have found a right
+                // which CAN be a matching one for a left.
+                left = left > 0 ? left - 1 : left;
+            }
+        }
+
+        string p;
+        recurse(s, 0, 0, 0, left, right, p);
+        return vector<string>(validExpressions.begin(), validExpressions.end());
+    }
+};
+
 // Another solution:
 // https://leetcode.com/discuss/81478/easy-short-concise-and-fast-java-dfs-3-ms-solution
 
