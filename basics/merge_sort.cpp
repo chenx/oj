07f0fd5a26b34aa00d5974c9326267d9f2273b7c
Tom Chen <txchen@gmail.com>
2025-09-09 15:34:40 -0700

    Update merge_sort.cpp

--- basics/merge_sort.cpp
+++ basics/merge_sort.cpp
@@ -1,3 +1,5 @@
+// Merge 2 arrays.
+
 void mergesort(vector<int>& nums) {
   msort(nums, 0, nums.size() - 1);
 }
@@ -20,3 +22,46 @@ void msort(vector<int>& nums, int left, int right) {
   while (i <= M) copy[k ++] = nums[i ++];
   while (j <= n) copy[k ++] = nums[j ++];
 }
+
+
+// Merge 2 lists
+class Solution {
+public:
+   ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+       if (!list1) return list2;
+       if (!list2) return list1;
+
+
+       ListNode *h = NULL, *t = NULL;
+       while (list1 && list2) {
+           if (list1->val < list2->val) {
+               getNextNode(list1, list2, h, t);
+           } else {
+               getNextNode(list2, list1, h, t);
+           }
+       }
+
+
+       if (list1) { t->next = list1; }
+       if (list2) { t->next = list2; }
+
+
+       return h;
+   }
+
+
+   void getNextNode(ListNode*& list1, ListNode*& list2,
+                    ListNode*& h, ListNode*& t) {
+       if (h == NULL) {
+           h = t = list1;
+           list1 = list1->next;
+           t->next = NULL;
+       } else {
+           t->next = list1;
+           list1 = list1->next;
+           t = t->next;
+           t->next = NULL;
+       }
+   }
+};
+
