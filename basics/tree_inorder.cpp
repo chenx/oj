1d9111d16c9ff0d1799e4354b2afe689a0089ffd
Tom Chen <txchen@gmail.com>
2025-09-08 22:50:31 -0700

    Update tree_inorder.cpp

--- basics/tree_inorder.cpp
+++ basics/tree_inorder.cpp
@@ -1,1 +1,30 @@
-    if (q->left) q.push(q->left);
+// Recursive
+void inorder(TreeNode* root, vector<int>& ans) {
+  if (!root) return;
+
+  preorder(root->left);
+  ans.push_back(root->val);
+  preorder(root->right);
+}
+
+// Iterative
+void inorder(TreeNode* root, vector<int>& ans) {
+  if (!root) return;
+
+  stack<TreeNode*> s;
+  TreeNode* n = root;
+
+  while (true) {
+    while(n) {
+        s.push(n);
+        n = n->left;
+    }
+
+    if (s.empty()) break;
+      
+    n = s.top();
+    s.pop();
+    ans.push_back(n->val);
+    n = n->right;
+  }
+}
