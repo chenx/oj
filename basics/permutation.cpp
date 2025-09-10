56862e70b3ffb1a25c0f8c2d79b36d82d7698d45
Tom Chen <txchen@gmail.com>
2025-09-09 19:39:33 -0700

    Update permutation.cpp

--- basics/permutation.cpp
+++ basics/permutation.cpp
@@ -1,5 +1,21 @@
-void next_perm(vector<int>& v) {
-  if (v.size() <= 1) return;
+class Solution {
+public:
+   void nextPermutation(vector<int>& nums) {
+       int n = nums.size();
 
-  
-}
+
+       int i, j;
+       // Find first i, nums[i] < nums[i+1]
+       for (i = n-2; i >= 0 && nums[i] >= nums[i+1]; i --) ;
+       if (i == -1) {
+           reverse(nums.begin(), nums.end());
+           return;
+       }
+
+
+       // Find first item from right, nums[j] > nums[i]
+       for (j = n-1; nums[j] <= nums[i]; --j) ;
+       swap(nums[i], nums[j]);
+       reverse(nums.begin() + i + 1, nums.end());
+   }
+};
