e0f4604c33ecc2c3d7c262dc6450e500691291bc
Tom Chen <txchen@gmail.com>
2025-10-03 14:54:43 -0700

    Create TrappingRainWater.java

---
+++ leetcode_java/TrappingRainWater.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int trap(int[] height) {
+        int len = height.length, water = 0;
+        
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < len; ++ i) {
+            if (stack.isEmpty() || height[i] <= height[stack.peek()]) {
+                stack.push(i);
+            } else {
+                int bottom = height[stack.peek()];
+                stack.pop();
+                while (!stack.isEmpty() && height[stack.peek()] <= height[i]) {
+                    int width = i - stack.peek() - 1;
+                    water += (height[stack.peek()] - bottom) * width;
+                    
+                    bottom = height[stack.peek()];
+                    stack.pop();
+                }
+                if (!stack.isEmpty()) {
+                    int width = i - stack.peek() - 1;
+                    water += (height[i] - bottom) * width;
+                }
+                stack.push(i);
+            }
+        }
+        return water;
+    }
+}
+
+/**
+Trapping Rain Water
+
+Given n non-negative integers representing an elevation map where the width of each bar is 1, 
+compute how much water it can trap after raining.
+ */
