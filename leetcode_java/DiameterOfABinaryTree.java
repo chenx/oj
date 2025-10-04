157a568cd76263b9dc4b799442be5c57c05a94f3
Tom Chen <txchen@gmail.com>
2025-10-03 17:10:40 -0700

    Update DiameterOfABinaryTree.java

--- leetcode_java/DiameterOfABinaryTree.java
+++ leetcode_java/DiameterOfABinaryTree.java
@@ -1,3 +1,22 @@
+class Solution2 {
+    private int diameter;
+    public int diameterOfBinaryTree(TreeNode root) {
+        diameter = 0;
+        getLongestPath(root);
+        return diameter;
+    }
+
+    private int getLongestPath(TreeNode root) {
+        if (root == null) return 0;
+
+        int L = getLongestPath(root.left);
+        int R = getLongestPath(root.right);
+
+        diameter = Math.max(diameter, L + R);
+        return Math.max(L, R) + 1;
+     }
+}
+
 class Solution {
     private int diameter;
     public int diameterOfBinaryTree(TreeNode root) {
@@ -12,7 +31,7 @@ class Solution {
         int L = getLongestPath(root.left);
         int R = getLongestPath(root.right);
 
-        diameter = Math.max(diameter, L + R + 2);
+        diameter = Math.max(diameter, L + R + 2);  // +2 to cancel out -1 of null nodes at the 2 ends.
         return Math.max(L, R) + 1;
      }
 }
