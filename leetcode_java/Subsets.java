4f0e94d01f02eafba25899936ca9c6f6fb27df2a
Tom Chen <txchen@gmail.com>
2025-10-03 21:16:50 -0700

    Update Subsets.java

--- leetcode_java/Subsets.java
+++ leetcode_java/Subsets.java
@@ -1,3 +1,20 @@
+class Solution2 {
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<>();
+        
+        ans.add(new ArrayList<Integer>()); // empty list
+        
+        for (int i = 0; i < nums.length; ++ i) {
+            for (int j = 0, len = ans.size(); j < len; ++ j) {
+                List<Integer> copy = new ArrayList<>(ans.get(j));
+                copy.add(nums[i]);
+                ans.add(copy);
+            }
+        }
+        return ans;
+    }
+}
+
 public class Solution {
     public List<List<Integer>> subsets(int[] nums) {
         List<List<Integer>> ans = new ArrayList<List<Integer>>();
