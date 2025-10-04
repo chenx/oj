207ceec85caedb7e415cca36dfd6f27c2aecb1fb
Tom Chen <txchen@gmail.com>
2025-10-03 23:44:29 -0700

    Update LongestPalindromicSubstring.java

--- leetcode_java/LongestPalindromicSubstring.java
+++ leetcode_java/LongestPalindromicSubstring.java
@@ -1,3 +1,32 @@
+class Solution2 {
+    int maxLen = 0, start = 0;
+    
+    public String longestPalindrome(String s) {
+        int len = s.length();
+        
+        for (int i = 0; i < len; ++ i) {
+            getVal(s, i, i);
+            if (i < len-1 && s.charAt(i) == s.charAt(i+1)) {
+                getVal(s, i, i+1);
+            }
+        }
+        return s.substring(start, start + maxLen); // substring(start, end)
+    }
+    
+    void getVal(String s, int L, int R) {
+        while (L > 0 && R < s.length() - 1) {
+            if (s.charAt(L-1) != s.charAt(R+1)) break;
+            -- L;
+            ++ R;
+        }
+        int len = R - L + 1;
+        if (len > maxLen) {
+            maxLen = len;
+            start = L;
+        }
+    }
+}
+
 public class Solution {
     public String longestPalindrome(String s) {
         int n = s.length(), v;
