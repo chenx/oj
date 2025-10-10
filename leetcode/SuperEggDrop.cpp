41e93c5d369307716a65a010af7d3b088801e109
Tom Chen <txchen@gmail.com>
2025-10-09 23:22:36 -0700

    Update SuperEggDrop.cpp

--- leetcode/SuperEggDrop.cpp
+++ leetcode/SuperEggDrop.cpp
@@ -1,7 +1,7 @@
 // Dynamic Programming with Binary Search
 // Time Complexity: O(KNlogN).
 // Space Complexity: O(KN).
-class Solution {
+class Solution2 {
 public:
     int superEggDrop(int K, int N) {
         return dp(K, N);
@@ -11,10 +11,8 @@ public:
     int dp(int K, int N) {
         if (!memo.contains(N * 100 + K)) {
             int ans;
-            if (N == 0)
-                ans = 0;
-            else if (K == 1)
-                ans = N;
+            if (N == 0) ans = 0;
+            else if (K == 1) ans = N;
             else {
                 int lo = 1, hi = N;
                 while (lo + 1 < hi) {
@@ -22,17 +20,13 @@ public:
                     int t1 = dp(K-1, x-1);
                     int t2 = dp(K, N-x);
 
-                    if (t1 < t2)
-                        lo = x;
-                    else if (t1 > t2)
-                        hi = x;
-                    else
-                        lo = hi = x;
+                    if (t1 < t2) lo = x;
+                    else if (t1 > t2) hi = x;
+                    else lo = hi = x;
                 }
                 ans = 1 + min(max(dp(K-1, lo-1), dp(K, N-lo)),
                               max(dp(K-1, hi-1), dp(K, N-hi)));
             }
-
             memo[N * 100 + K] = ans;
         }
 
