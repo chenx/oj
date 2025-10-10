5aed4cc1d605a2b298fa7a2ddb0854766d882f89
Tom Chen <txchen@gmail.com>
2025-10-09 23:19:25 -0700

    Update SuperEggDrop.cpp

--- leetcode/SuperEggDrop.cpp
+++ leetcode/SuperEggDrop.cpp
@@ -1,4 +1,48 @@
+// Dynamic Programming with Binary Search
+// Time Complexity: O(KNlogN).
+// Space Complexity: O(KN).
+class Solution {
+public:
+    int superEggDrop(int K, int N) {
+        return dp(K, N);
+    }
+
+    map<int, int> memo;
+    int dp(int K, int N) {
+        if (!memo.contains(N * 100 + K)) {
+            int ans;
+            if (N == 0)
+                ans = 0;
+            else if (K == 1)
+                ans = N;
+            else {
+                int lo = 1, hi = N;
+                while (lo + 1 < hi) {
+                    int x = (lo + hi) / 2;
+                    int t1 = dp(K-1, x-1);
+                    int t2 = dp(K, N-x);
+
+                    if (t1 < t2)
+                        lo = x;
+                    else if (t1 > t2)
+                        hi = x;
+                    else
+                        lo = hi = x;
+                }
+                ans = 1 + min(max(dp(K-1, lo-1), dp(K, N-lo)),
+                              max(dp(K-1, hi-1), dp(K, N-hi)));
+            }
+
+            memo[N * 100 + K] = ans;
+        }
+
+        return memo[N * 100 + K];
+    }
+};
+
 // From https://leetcode.com/problems/super-egg-drop/editorial/
+// Time Complexity: O(KlogN)
+// Space Complexity: O(1)
 class Solution {
 public:
     int superEggDrop(int K, int N) {
