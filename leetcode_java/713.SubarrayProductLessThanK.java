5ee3f7e81e702272d37f8f26fafd448c27c8a777
Tom Chen <txchen@gmail.com>
2025-10-10 23:17:42 -0700

    Create 713.SubarrayProductLessThanK.java

---
+++ leetcode_java/713.SubarrayProductLessThanK.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int numSubarrayProductLessThanK(int[] nums, int k) {
+        if (k <= 1) return 0;
+        int product = 1, count = 0;
+
+        for (int i = 0, left = 0; i < nums.length; ++ i) {
+            product *= nums[i];
+            while (product >= k) {
+                product /= nums[left];
+                ++ left;
+            }
+            count += i - left + 1;
+        }
+        return count;
+    }
+}
+
+/**
+713. Subarray Product Less Than K
+
+Given an array of integers nums and an integer k, return the number of contiguous subarrays 
+where the product of all the elements in the subarray is strictly less than k.
+ */
