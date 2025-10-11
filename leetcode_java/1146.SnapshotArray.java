f7837510356bfa9c1a16a001805b523af4e64fe2
Tom Chen <txchen@gmail.com>
2025-10-10 23:52:55 -0700

    Create 1146.SnapshotArray.java

---
+++ leetcode_java/1146.SnapshotArray.java
@@ -0,0 +1,38 @@
+// See https://leetcode.com/problems/snapshot-array/editorial/
+// Time complexity: O(nlogn+k)
+// Space complexity: O(n+k)
+class SnapshotArray {
+    int snapId = 0;
+    TreeMap<Integer, Integer>[] historyRecords;
+    
+    public SnapshotArray(int length) {
+        historyRecords = new TreeMap[length];
+        for (int i = 0; i < length; i++) {
+            historyRecords[i] = new TreeMap<Integer, Integer>();
+            historyRecords[i].put(0, 0);
+        }
+    }
+
+    public void set(int index, int val) {
+        historyRecords[index].put(snapId, val);
+    }
+
+    public int snap() {
+        return snapId++;
+    }
+
+    public int get(int index, int snapId) {
+        return historyRecords[index].floorEntry(snapId).getValue();
+    }
+}
+
+/**
+1146. Snapshot Array
+
+Implement a SnapshotArray that supports the following interface:
+
+    SnapshotArray(int length) initializes an array-like data structure with the given length. Initially, each element equals 0.
+    void set(index, val) sets the element at the given index to be equal to val.
+    int snap() takes a snapshot of the array and returns the snap_id: the total number of times we called snap() minus 1.
+    int get(index, snap_id) returns the value at the given index, at the time we took the snapshot with the given snap_id
+ */
