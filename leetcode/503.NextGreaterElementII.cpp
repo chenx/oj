7bf10602244e2b2f09eb38bdc2072c22a452bf12
Tom Chen <txchen@gmail.com>
2025-10-14 20:37:52 -0700

    Create 503.NextGreaterElementII.cpp

---
+++ leetcode/503.NextGreaterElementII.cpp
@@ -0,0 +1,29 @@
+// From: https://leetcode.com/problems/next-greater-element-ii/editorial
+// Time: O(n), Space: O(n)
+class Solution {
+public:
+    vector<int> nextGreaterElements(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> res(n, 0);
+        stack<int> stack;
+        for (int i = 2 * n - 1; i >= 0; --i) {
+            while (!stack.empty() && nums[stack.top()] <= nums[i % n]) {
+                stack.pop();
+            }
+            res[i % n] = stack.empty() ? -1 : nums[stack.top()];
+            stack.push(i % n);
+        }
+        return res;
+    }
+};
+
+/**
+503. Next Greater Element II
+
+Given a circular integer array nums (i.e., the next element of nums[nums.length - 1] is nums[0]), 
+return the next greater number for every element in nums.
+
+The next greater number of a number x is the first greater number to its traversing-order next in 
+the array, which means you could search circularly to find its next greater number. If it doesn't 
+exist, return -1 for this number.
+ */
