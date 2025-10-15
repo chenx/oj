862629b03cd6c8e408bf9b77211a9b9403b16710
Tom Chen <txchen@gmail.com>
2025-10-14 20:43:49 -0700

    Update 503.NextGreaterElementII.cpp

--- leetcode/503.NextGreaterElementII.cpp
+++ leetcode/503.NextGreaterElementII.cpp
@@ -1,3 +1,24 @@
+// Same as Solution, but use 2 cycles to avoid confusion of i % n.
+class Solution2 {
+public:
+    vector<int> nextGreaterElements(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> res(n, 0);
+        stack<int> stack;
+
+        for (int k = 0; k < 2; ++ k) {
+            for (int i = n - 1; i >= 0; --i) {
+                while (!stack.empty() && nums[stack.top()] <= nums[i]) {
+                    stack.pop();
+                }
+                res[i] = stack.empty() ? -1 : nums[stack.top()];
+                stack.push(i);
+            }
+        }
+        return res;
+    }
+};
+
 // From: https://leetcode.com/problems/next-greater-element-ii/editorial
 // Time: O(n), Space: O(n)
 class Solution {
