73f281e5f42f861e16c76c7f7931b7acb24a5aaf
Tom Chen <txchen@gmail.com>
2025-10-20 17:34:12 -0700

    Implement merge intervals function in JavaScript

---
+++ leetcode_javascript/MergeIntervals.js
@@ -0,0 +1,35 @@
+/**
+ * @param {number[][]} intervals
+ * @return {number[][]}
+ */
+var merge = function(intervals) {
+    const ans = [];
+
+    intervals.sort((a, b) => { return a[0] - b[0]; });
+
+    const n = intervals.length;
+    for (let i = 1; i < n; ++ i) {
+        const a = intervals[i-1], b = intervals[i];
+        if (a[1] < b[0]) {
+            ans.push(a);
+        } else if (a[1] <= b[1]) {
+            b[0] = a[0];
+        } else {
+            b[0] = a[0], b[1] = a[1];
+        }
+    }
+    ans.push(intervals[n-1]);
+
+    return ans;
+};
+
+/**
+--- 
+    --
+
+----
+  --
+
+----
+ -
+ */
