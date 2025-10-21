26c663d4ba22a66db6d8dbe28f31cc7e5b7d9a72
Tom Chen <txchen@gmail.com>
2025-10-20 19:11:27 -0700

    Update IntersectionOfTwoArrays.js

--- leetcode_javascript/IntersectionOfTwoArrays.js
+++ leetcode_javascript/IntersectionOfTwoArrays.js
@@ -1,3 +1,18 @@
+// Version 2.
+var intersection = function(nums1, nums2) {
+    const set1 = new Set();
+    nums1.forEach(n => set1.add(n));
+
+    const set2 = new Set();
+    nums2.forEach(n => set2.add(n));
+
+    const ret = [];
+    for (const n of set1) {
+        if (set2.has(n)) ret.push(n);
+    }
+    return ret;
+};
+
 /**
  * @param {number[]} nums1
  * @param {number[]} nums2
