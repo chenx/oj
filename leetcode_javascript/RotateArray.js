7cce9a0dd0868ad30f163d5655e0b3f3675a2cfb
Tom Chen <txchen@gmail.com>
2025-10-20 18:49:19 -0700

    Implement array rotation function in RotateArray.js

---
+++ leetcode_javascript/RotateArray.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var rotate = function(nums, k) {
+    const n = nums.length;
+    k %= n;
+
+    reverse(nums, 0, n-1);
+    reverse(nums, 0, k-1);
+    reverse(nums, k, n-1);
+};
+
+function reverse(nums, i, j) {
+    for (; i < j; ++ i, -- j) {
+        const tmp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = tmp;
+    }
+}
