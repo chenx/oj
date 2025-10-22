9dda79ca7ad9f883cb4578476d2554b8e9e40353
Tom Chen <txchen@gmail.com>
2025-10-21 22:14:19 -0700

    Update RotateArray.js

--- leetcode_javascript/RotateArray.js
+++ leetcode_javascript/RotateArray.js
@@ -1,3 +1,22 @@
+// Works too.
+var rotate2 = function(nums, k) {
+    const n = nums.length;
+    k %= n;
+
+    reverse(nums, 0, n-1);
+    reverse(nums, 0, k-1);
+    reverse(nums, k, n-1);
+
+    function reverse(nums, i, j) {
+        for (; i < j; ++ i, -- j) {
+            const tmp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = tmp;
+        }
+    }
+};
+
+
 /**
  * @param {number[]} nums
  * @param {number} k
