dbd9b6bbe1b338e2465c08df96a9beb6aa5c5447
Tom Chen <txchen@gmail.com>
2025-10-21 23:30:00 -0700

    Update 694.NumberOfDistinctIslands.js

--- leetcode_javascript/694.NumberOfDistinctIslands.js
+++ leetcode_javascript/694.NumberOfDistinctIslands.js
@@ -2,6 +2,47 @@
  * @param {number[][]} grid
  * @return {number}
  */
+var numDistinctIslands2 = function(grid) {
+    if (grid.length === 0 || grid[0].length === 0) return;
+    const m = grid.length, n = grid[0].length;
+    const directions = [-1, 0, 1, 0, -1];
+    const actions = ['U', 'L', 'D', 'R'];
+    let path = '';
+
+    const dfs = (i, j) => {
+        grid[i][j] = 2; // so we don't visit a visited cell.
+
+        for (let k = 0; k < 4; ++ k) {
+            let x = i + directions[k], y = j + directions[k+1];
+            if (x >= 0 && x < m && y >= 0 && y < n) {
+                if (grid[x][y] === 1) {
+                    path += actions[k];
+                    dfs(x, y);
+                }
+            }
+        }
+
+        path += '.';
+    }
+
+    const islands = new Set();
+    for (let i = 0; i < m; ++ i) {
+        for (let j = 0; j < n; ++ j) {
+            if (grid[i][j] == 1) {
+                path = '';
+                dfs(i, j);
+                islands.add(path);
+            }
+        }
+    }
+
+    return islands.size;
+};
+
+/**
+ * @param {number[][]} grid
+ * @return {number}
+ */
 var numDistinctIslands = function(grid) {
     if (grid.length === 0 || grid[0].length === 0) return;
     const m = grid.length, n = grid[0].length;
