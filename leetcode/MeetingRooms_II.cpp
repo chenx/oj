0073c84fc879bf809eccb5fe60b09fed098c6291
Tom Chen <txchen@gmail.com>
2025-10-21 21:02:39 -0700

    Update MeetingRooms_II.cpp

--- leetcode/MeetingRooms_II.cpp
+++ leetcode/MeetingRooms_II.cpp
@@ -1,3 +1,21 @@
+class Solution3 {
+public:
+    int minMeetingRooms(vector<vector<int>>& intervals) {
+        sort(intervals.begin(), intervals.end(), comp);
+
+        priority_queue<int, vector<int>, greater<int>> pq;
+        for (int i = 0; i < intervals.size(); ++ i) {
+            if (!pq.empty() && intervals[i][0] >= pq.top()) pq.pop();
+            pq.push(intervals[i][1]);
+        }
+        return pq.size();
+    }
+
+    static bool comp(vector<int>& a, vector<int>& b) {
+        return a[0] < b[0];
+    }
+};
+
 /**
  * Definition for an interval.
  * struct Interval {
