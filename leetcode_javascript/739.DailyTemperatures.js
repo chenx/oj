b14e565aa56c967405b7806183335ddfd3b4c9e7
Tom Chen <txchen@gmail.com>
2025-10-22 01:28:38 -0700

    Create 739.DailyTemperatures.js

---
+++ leetcode_javascript/739.DailyTemperatures.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} temperatures
+ * @return {number[]}
+ */
+var dailyTemperatures = function(temperatures) {
+    const n = temperatures.length;
+    const ans = new Array(n).fill(0);
+    let hottest = 0;
+
+    for (let i = n-1; i >= 0; -- i) {
+        const t = temperatures[i];
+        if (t >= hottest) {
+            hottest = t;
+            continue;
+        }
+
+        let day = 1;
+        while ( temperatures[i + day] <= t ) {
+            day += ans[i + day];
+        }
+        ans[i] = day;
+    }
+    return ans;
+};
