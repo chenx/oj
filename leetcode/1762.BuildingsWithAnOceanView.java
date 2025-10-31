303448f33ae57e45433ba23b98a666a832b7d909
Tom Chen <txchen@gmail.com>
2025-10-30 22:08:34 -0700

    Create 1762.BuildingsWithAnOceanView.java

---
+++ leetcode/1762.BuildingsWithAnOceanView.java
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    vector<int> findBuildings(vector<int>& heights) {
+        // use a stack, go from right to left
+        // if stack is empty or new building height > stack top height, push to stack; otherwise ignore
+
+        stack<int> stack;
+        for (int i = heights.size() - 1; i >= 0; -- i) {
+            if (stack.empty() || heights[i] > heights[stack.top()]) stack.push(i);
+        }
+
+        vector<int> ans;
+        while (! stack.empty()) {
+            ans.push_back(stack.top());
+            stack.pop();
+        }
+        return ans;
+    }
+};
+
+/**
+1762. Buildings With an Ocean View
+Medium
+
+There are n buildings in a line. You are given an integer array heights of size n that represents the heights of the buildings in the line.
+
+The ocean is to the right of the buildings. A building has an ocean view if the building can see the ocean without obstructions. 
+Formally, a building has an ocean view if all the buildings to its right have a smaller height.
+
+Return a list of indices (0-indexed) of buildings that have an ocean view, sorted in increasing order.
+
+ */
