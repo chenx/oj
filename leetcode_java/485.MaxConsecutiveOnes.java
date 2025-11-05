9d49d235ec8193d7a7c0fd4304c09dbf907a0963
Tom Chen <txchen@gmail.com>
2025-11-04 20:03:17 -0800

    Create 485.MaxConsecutiveOnes.java

---
+++ leetcode_java/485.MaxConsecutiveOnes.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int findMaxConsecutiveOnes(int[] nums) {
+        int n = nums.length;
+        if (n == 0) return 0;
+
+        int len = (nums[0] == 1) ? 1 : 0;
+        int maxLen = len;
+
+        for (int i = 1; i < n; ++ i) {
+            if (nums[i] == 1) {
+                ++ len;
+            } else {
+                maxLen = Math.max(maxLen, len);
+                len = 0;
+            }
+        }
+        maxLen = Math.max(maxLen, len);
+
+        return maxLen;
+    }
+}
+
+/**
+485. Max Consecutive Ones
+Easy
+
+Given a binary array nums, return the maximum number of consecutive 1's in the array.
+ */
