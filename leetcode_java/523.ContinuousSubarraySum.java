248d48bbd501e5250bc0dd898e06bff29a64cdd4
Tom Chen <txchen@gmail.com>
2025-11-05 20:33:38 -0800

    Create 523.ContinuousSubarraySum.java

---
+++ leetcode_java/523.ContinuousSubarraySum.java
@@ -0,0 +1,41 @@
+class Solution {
+    public boolean checkSubarraySum(int[] nums, int k) {
+        int prefixMod = 0;
+        HashMap<Integer, Integer> modSeen = new HashMap<>();
+        modSeen.put(0, -1);
+
+        for (int i = 0; i < nums.length; i++) {
+            prefixMod = (prefixMod + nums[i]) % k;
+
+            if (modSeen.containsKey(prefixMod)) {
+                // ensures that the size of subarray is at least 2
+                if (i - modSeen.get(prefixMod) >= 2) {
+                    return true;
+                }
+            } else {
+                // mark the value of prefixMod with the current index.
+                modSeen.put(prefixMod, i);
+            }
+        }
+
+        return false;
+    }
+}
+
+
+/**
+523. Continuous Subarray Sum
+Medium
+
+Given an integer array nums and an integer k, return true if nums has a good subarray or false otherwise.
+
+A good subarray is a subarray where:
+
+    its length is at least two, and
+    the sum of the elements of the subarray is a multiple of k.
+
+Note that:
+
+    A subarray is a contiguous part of the array.
+    An integer x is a multiple of k if there exists an integer n such that x = n * k. 0 is always a multiple of k.
+ */
