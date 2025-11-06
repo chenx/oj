1b4f3efd011da71f64833dac5820f1e90430c38a
Tom Chen <txchen@gmail.com>
2025-11-05 22:29:37 -0800

    Create 2011.FinalValueOfVariableAfterPerformingOperations.java

---
+++ leetcode_java/2011.FinalValueOfVariableAfterPerformingOperations.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int finalValueAfterOperations(String[] operations) {
+        int val = 0;
+        for (String op : operations) {
+            if (op.equals("++X") || op.equals("X++")) ++ val;
+            else -- val;
+        }
+        return val;
+    }
+}
+
+
+/**
+2011. Final Value of Variable After Performing Operations
+Easy
+
+There is a programming language with only four operations and one variable X:
+
+    ++X and X++ increments the value of the variable X by 1.
+    --X and X-- decrements the value of the variable X by 1.
+
+Initially, the value of X is 0.
+
+Given an array of strings operations containing a list of operations, return 
+the final value of X after performing all the operations.
+ */
