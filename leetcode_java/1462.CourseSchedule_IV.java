3edab9ed19ee4072c7fdf2c05802b02e30caa670
Tom Chen <txchen@gmail.com>
2025-11-07 22:53:25 -0800

    Create 1462.CourseSchedule_IV.java

---
+++ leetcode_java/1462.CourseSchedule_IV.java
@@ -0,0 +1,53 @@
+// https://leetcode.com/problems/course-schedule-iv/editorial/
+// Approach 4: Floyd Warshall Algorithm
+// Time complexity: O(N3+Q)
+// Space complexity: O(N2)
+
+class Solution {
+    public List<Boolean> checkIfPrerequisite(int numCourses, int[][] prerequisites, int[][] queries) {
+        boolean[][] isPrerequisite = new boolean[numCourses][numCourses];
+
+        for (int[] edge : prerequisites) {
+            isPrerequisite[edge[0]][edge[1]] = true;
+        }
+
+        for (int intermediate = 0; intermediate < numCourses; intermediate++) {
+            for (int src = 0; src < numCourses; src++) {
+                for (int target = 0; target < numCourses; target++) {
+                    // If there is a path i -> intermediate and intermediate -> j, then i -> j exists as well.
+                    isPrerequisite[src][target] =
+                        isPrerequisite[src][target] ||
+                        (isPrerequisite[src][intermediate] &&
+                            isPrerequisite[intermediate][target]);
+                }
+            }
+        }
+
+        List<Boolean> answer = new ArrayList<>();
+        for (int[] query : queries) {
+            answer.add(isPrerequisite[query[0]][query[1]]);
+        }
+
+        return answer;
+    }
+}
+
+
+
+/**
+1462. Course Schedule IV
+Medium
+
+There are a total of numCourses courses you have to take, labeled from 0 to numCourses - 1. You are given an array prerequisites where 
+prerequisites[i] = [ai, bi] indicates that you must take course ai first if you want to take course bi.
+
+    For example, the pair [0, 1] indicates that you have to take course 0 before you can take course 1.
+
+Prerequisites can also be indirect. If course a is a prerequisite of course b, and course b is a prerequisite of course c, then course 
+a is a prerequisite of course c.
+
+You are also given an array queries where queries[j] = [uj, vj]. For the jth query, you should answer whether course uj is a prerequisite 
+of course vj or not.
+
+Return a boolean array answer, where answer[j] is the answer to the jth query.
+ */
