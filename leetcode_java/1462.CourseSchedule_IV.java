3d5d4718b7689da121b27be97f2924143ba887d6
Tom Chen <txchen@gmail.com>
2025-11-07 22:57:06 -0800

    Update 1462.CourseSchedule_IV.java

--- leetcode_java/1462.CourseSchedule_IV.java
+++ leetcode_java/1462.CourseSchedule_IV.java
@@ -3,6 +3,34 @@
 // Time complexity: O(N3+Q)
 // Space complexity: O(N2)
 
+class Solution2 {
+    public List<Boolean> checkIfPrerequisite(int numCourses, int[][] prerequisites, int[][] queries) {
+        boolean[][] isPrerequisite = new boolean[numCourses][numCourses];
+
+        for (int[] edge : prerequisites) {
+            isPrerequisite[edge[0]][edge[1]] = true;
+        }
+
+        for (int intermediate = 0; intermediate < numCourses; intermediate++) {
+            for (int src = 0; src < numCourses; src++) {
+                for (int target = 0; target < numCourses; target++) {
+                    // If there is a path i -> intermediate and intermediate -> j, then i -> j exists as well.
+                    if (isPrerequisite[src][intermediate] && isPrerequisite[intermediate][target]) {
+                        isPrerequisite[src][target] = true;
+                    }
+                }
+            }
+        }
+
+        List<Boolean> answer = new ArrayList<>();
+        for (int[] query : queries) {
+            answer.add(isPrerequisite[query[0]][query[1]]);
+        }
+
+        return answer;
+    }
+}
+
 class Solution {
     public List<Boolean> checkIfPrerequisite(int numCourses, int[][] prerequisites, int[][] queries) {
         boolean[][] isPrerequisite = new boolean[numCourses][numCourses];
