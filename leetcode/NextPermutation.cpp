7729a981f04cb1631c87a4fce3e7afe6856b490a
Tom Chen <txchen@gmail.com>
2025-11-09 08:28:13 +0000

    Update NextPermutation.cpp

--- leetcode/NextPermutation.cpp
+++ leetcode/NextPermutation.cpp
@@ -5,6 +5,28 @@
 // @Last modified: 1/9/2012
 //
 
+class Solution4 {
+public:
+    void nextPermutation(vector<int>& nums) {
+        int n = nums.size();
+        if (n <= 1) return;
+
+        int i, j;
+
+        // find first i, nums[i] < nums[i+1]
+        for (i = n - 2; i >= 0 && nums[i] >= nums[i+1]; -- i) ;
+        if (i == -1) {
+            reverse(nums.begin(), nums.end());
+            return;
+        }
+
+        // find first j from RHS, nums[i] < nums[j]
+        for (j = n-1; nums[i] >= nums[j]; -- j) ;
+        swap(nums[i], nums[j]);
+        reverse(nums.begin() + i + 1, nums.end());
+    }
+};
+
 // This works too. more clean.
 class Solution3 {
 public:
@@ -115,4 +137,5 @@ its corresponding outputs are in the right-hand column.
 3,2,1 ¡ú 1,2,3
 1,1,5 ¡ú 1,5,1        
  */
+
 
