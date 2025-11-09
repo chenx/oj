db310fe538d13a15ba30a1e75e6a1a02494ada08
Tom Chen <txchen@gmail.com>
2025-11-09 23:05:01 +0000

    Create 378.KthSmallestElementInASortedMatrix.java

---
+++ leetcode/378.KthSmallestElementInASortedMatrix.java
@@ -0,0 +1,42 @@
+struct comp {
+    bool operator()(vector<int> &a, vector<int> &b) {
+        return a[0] > b[0];
+    }
+};
+
+class Solution {
+public:
+    int kthSmallest(vector<vector<int>>& matrix, int k) {
+        int n = matrix.size();
+
+        priority_queue<vector<int>, vector<vector<int>>, comp> minHeap;
+
+        for (int i = 0; i < n; ++ i) {
+            minHeap.push({matrix[0][i], 0, i});
+        }
+
+        while (! minHeap.empty()) {
+            int val = minHeap.top()[0];
+            int x = minHeap.top()[1];
+            int y = minHeap.top()[2];
+            minHeap.pop();
+            -- k;
+            if (k == 0) return val;
+
+            if (x+1 < n) minHeap.push({matrix[x+1][y], x+1, y});
+        }
+        return 0;
+    }
+};
+
+
+/**
+378. Kth Smallest Element in a Sorted Matrix
+Medium
+
+Given an n x n matrix where each of the rows and columns is sorted in ascending order, return the kth smallest element in the matrix.
+
+Note that it is the kth smallest element in the sorted order, not the kth distinct element.
+
+You must find a solution with a memory complexity better than O(n2).
+ */
