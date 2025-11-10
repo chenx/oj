fc0a800ae67201d326632f1c238f7d65c041d818
Tom Chen <txchen@gmail.com>
2025-11-10 02:54:27 +0000

    Create 1094.CarPooling.cpp

---
+++ leetcode/1094.CarPooling.cpp
@@ -0,0 +1,33 @@
+
+class Solution {
+public:
+    bool carPooling(vector<vector<int>>& trips, int capacity) {
+        map<int, int> map; // <stop, psg_change>
+        for (auto e : trips) {
+            map[e[1]] += e[0];
+            map[e[2]] -= e[0];
+        }
+
+        // map is ordered by key
+
+        int total = 0;
+        for (auto [stop, change] : map) {
+            total += change;
+            if (total > capacity) return false;
+        }
+        return true;
+    }
+};
+
+/**
+1094. Car Pooling
+Medium
+
+There is a car with capacity empty seats. The vehicle only drives east (i.e., it cannot turn around and drive west).
+
+You are given the integer capacity and an array trips where trips[i] = [numPassengersi, fromi, toi] indicates that the ith trip has 
+numPassengersi passengers and the locations to pick them up and drop them off are fromi and toi respectively. The locations are 
+given as the number of kilometers due east from the car's initial location.
+
+Return true if it is possible to pick up and drop off all passengers for all the given trips, or false otherwise.
+ */
