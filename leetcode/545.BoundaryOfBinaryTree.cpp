553f7cc00e674812e4cf9216845e2b69dd9039c6
Tom Chen <txchen@gmail.com>
2025-11-10 07:38:11 +0000

    Create 545.BoundaryOfBinaryTree.cpp

---
+++ leetcode/545.BoundaryOfBinaryTree.cpp
@@ -0,0 +1,80 @@
+class Solution {
+    bool isLeaf(TreeNode* root) {
+        return root && !root->left && !root->right;
+    }
+
+    void addLeaf(TreeNode* node, vector<int>& v) {
+        if (! node) return;
+        if (isLeaf(node)) {
+            v.push_back(node->val);
+            return;
+        }
+        addLeaf(node->left, v);
+        addLeaf(node->right, v);
+    }
+
+public:
+    vector<int> boundaryOfBinaryTree(TreeNode* root) {
+        vector<int> ans;
+        if (!root) return ans;
+
+        if (! isLeaf(root)) ans.push_back(root->val);
+
+        TreeNode *n = root->left;
+        while (n) {
+            if (!isLeaf(n)) {
+                ans.push_back(n->val);
+            }
+
+            if (n->left) {
+                n = n->left;
+            } else {
+                n = n->right;
+            }
+        }
+
+        addLeaf(root, ans);
+
+        n = root->right;
+        stack<int> st;
+        while (n) {
+            if (!isLeaf(n)) {
+                st.push(n->val);
+            }
+            
+            if (n->right) {
+                n = n->right;
+            } else {
+                n = n->left;
+            }
+        }
+        while (!st.empty()) {
+            ans.push_back(st.top());
+            st.pop();
+        }
+        return ans;
+    }
+};
+
+
+/**
+545. Boundary of Binary Tree
+Medium
+
+The boundary of a binary tree is the concatenation of the root, the left boundary, the leaves ordered from left-to-right, 
+and the reverse order of the right boundary.
+
+The left boundary is the set of nodes defined by the following:
+
+    The root node's left child is in the left boundary. If the root does not have a left child, then the left boundary is empty.
+    If a node is in the left boundary and has a left child, then the left child is in the left boundary.
+    If a node is in the left boundary, has no left child, but has a right child, then the right child is in the left boundary.
+    The leftmost leaf is not in the left boundary.
+
+The right boundary is similar to the left boundary, except it is the right side of the root's right subtree. Again, the leaf 
+is not part of the right boundary, and the right boundary is empty if the root does not have a right child.
+
+The leaves are nodes that do not have any children. For this problem, the root is not a leaf.
+
+Given the root of a binary tree, return the values of its boundary.
+ */
