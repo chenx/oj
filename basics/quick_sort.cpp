6f369a581390fc013295b2d1dbf05aacbbec2c04
Tom Chen <txchen@gmail.com>
2025-11-10 02:20:42 +0000

    Update quick_sort.cpp

--- basics/quick_sort.cpp
+++ basics/quick_sort.cpp
@@ -12,11 +12,31 @@ void qsort(vector<int>& nums, int left, int right) {
   while (L <= R) {
     while (L <= R && nums[L] < pivot) ++ L;
     while (L <= R && nums[R] > pivot) -- R;
-    if (L < R) swap(nums[L], nums[R]);
-    ++ L;
+    if (L < R) swap(nums[L ++], nums[R --]);
+    else ++ L;
   }
 
   swap(nums[left], nums[R]);
-  if (k < R) qsort(nums, left, R - 1);
-  if (k > R) qsort(nums, R + 1, right);
+  qsort(nums, left, R - 1);
+  qsort(nums, R + 1, right);
+}
+
+
+void quick_select(vector<int>& nums, int left, int right, int k) {
+  if (left >= right) return;
+
+  int M = left + (right - left)/2;
+  swap(nums[left], nums[M]);
+  int pivot = nums[left], L = left + 1, R = right;
+
+  while (L <= R) {
+    while (L <= R && nums[L] < pivot) ++ L;
+    while (L <= R && nums[R] > pivot) -- R;
+    if (L < R) swap(nums[L ++], nums[R --]);
+    else ++ L;
+  }
+
+  swap(nums[left], nums[R]);
+  if (k < R) quick_select(nums, left, R - 1, k);
+  if (k > R) quick_select(nums, R + 1, right, k);
 }
