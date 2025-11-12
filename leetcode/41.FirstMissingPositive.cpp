febe1b676e5e45560a6654063de7a32d8a232e3e
Tom Chen <txchen@gmail.com>
2025-11-12 00:35:15 +0000

    Update and rename FirstMissingPositive.cpp to 41.FirstMissingPositive.cpp

--- leetcode/41.FirstMissingPositive.cpp
+++ leetcode/41.FirstMissingPositive.cpp
@@ -119,7 +119,7 @@ public:
 };
 
 /**
-First Missing Positive
+41. First Missing Positive
 Difficulty: Hard
 
 Given an unsorted integer array, find the first missing positive integer.
@@ -130,3 +130,4 @@ and [3,4,-1,1] return 2.
 
 Your algorithm should run in O(n) time and uses constant space. 
  */
+
