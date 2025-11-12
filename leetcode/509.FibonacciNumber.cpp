de3b9f44b72dcb645997662bebe4e8c57c43e88f
Tom Chen <txchen@gmail.com>
2025-11-11 21:39:36 -0800

    Create 509.FibonacciNumber.cpp

---
+++ leetcode/509.FibonacciNumber.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int fib(int n) {
+        if (n == 0) return 0;
+        
+        int a = 0, b = 1;
+        for (int i = 2; i <= n; ++ i) {
+            int tmp = a + b;
+            a = b;
+            b = tmp;
+        }
+        return b;
+    }
+};
+
+/**
+509. Fibonacci Number
+
+The Fibonacci numbers, commonly denoted F(n) form a sequence, called the Fibonacci sequence, 
+such that each number is the sum of the two preceding ones, starting from 0 and 1. That is,
+
+F(0) = 0, F(1) = 1
+F(n) = F(n - 1) + F(n - 2), for n > 1.
+
+Given n, calculate F(n).
+ */
