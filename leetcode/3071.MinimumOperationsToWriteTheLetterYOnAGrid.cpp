d4290f6d3b2881b933aa71255e71dc9e05a44a47
Tom Chen <txchen@gmail.com>
2025-11-11 23:50:45 -0800

    Update and rename MinimumOperationsToWriteTheLetterYOnAGrid.cpp to 3071.MinimumOperationsToWriteTheLetterYOnAGrid.cpp

--- leetcode/3071.MinimumOperationsToWriteTheLetterYOnAGrid.cpp
+++ leetcode/3071.MinimumOperationsToWriteTheLetterYOnAGrid.cpp
@@ -1,3 +1,32 @@
+class Solution4 {
+public:
+    int minimumOperationsToWriteY(vector<vector<int>>& grid) {
+        int n = grid.size();
+
+        int yCells[3] = {0}, nonYCells[3] = {0};
+        for (int i = 0; i < n; ++ i) {
+            for (int j = 0; j < n; ++ j) {
+                if (i == j && i <= n/2) ++ yCells[grid[i][j]];
+                else if (i+j == n-1 && i <= n/2) ++ yCells[grid[i][j]];
+                else if (j == n/2 && i >= n/2) ++ yCells[grid[i][j]];
+                else ++ nonYCells[grid[i][j]];
+            }
+        }
+
+        int total = n*n, minOp = total;
+
+        for (int i = 0; i < 3; ++ i) {
+            for (int j = 0; j < 3; ++ j) {
+                if (i != j) {
+                    int op = total - yCells[i] - nonYCells[j];
+                    minOp = min(minOp, op);
+                }
+            }
+        }
+        return minOp;
+    }
+};
+
 class Solution3 {
 public:
     int minimumOperationsToWriteY(vector<vector<int>>& grid) {
