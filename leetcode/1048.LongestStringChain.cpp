7b0eda29c210e076510dbfda24c0fbb50c23fcfc
Tom Chen <txchen@gmail.com>
2025-11-14 14:05:30 -0800

    Create 1048.LongestStringChain.cpp

---
+++ leetcode/1048.LongestStringChain.cpp
@@ -0,0 +1,59 @@
+
+class Solution {
+    
+private:
+
+    int dfs(unordered_set<string> &words, unordered_map<string, int> &memo, string currentWord) {
+        // If the word is encountered previously we just return its value present in the map (memoization).
+        if (memo.find(currentWord) != memo.end()) {
+            return memo[currentWord];
+        }
+        // This stores the maximum length of word sequence possible with the 'currentWord' as the
+        int maxLength = 1;
+
+        // creating all possible strings taking out one character at a time from the `currentWord`
+        for (int i = 0; i < currentWord.length(); i++) {
+            string newWord = currentWord.substr(0, i) + currentWord.substr(i + 1);
+            // If the new word formed is present in the list, we do a dfs search with this newWord.
+            if (words.find(newWord) != words.end()) {
+                int currentLength = 1 + dfs(words, memo, newWord);
+                maxLength = max(maxLength, currentLength);
+            }
+        }
+        memo[currentWord] = maxLength;
+
+        return maxLength;
+    }
+
+public :
+    int longestStrChain(vector<string> &words) {
+        unordered_map<string, int> memo;
+        unordered_set<string> wordsPresent;
+        for (const string &word : words) {
+            wordsPresent.insert(word);
+        }
+        int ans = 0;
+        for (const string &word : words) {
+            ans = max(ans, dfs(wordsPresent, memo, word));
+        }
+        return ans;
+    }
+};
+
+
+/**
+1048. Longest String Chain
+Medium
+
+You are given an array of words where each word consists of lowercase English letters.
+
+wordA is a predecessor of wordB if and only if we can insert exactly one letter anywhere in wordA without 
+changing the order of the other characters to make it equal to wordB.
+
+    For example, "abc" is a predecessor of "abac", while "cba" is not a predecessor of "bcad".
+
+A word chain is a sequence of words [word1, word2, ..., wordk] with k >= 1, where word1 is a predecessor 
+of word2, word2 is a predecessor of word3, and so on. A single word is trivially a word chain with k == 1.
+
+Return the length of the longest possible word chain with words chosen from the given list of words.
+ */
