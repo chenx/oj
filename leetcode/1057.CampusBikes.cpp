9bc1bd56eaa2614e9965e5556e74f9ee36449599
Tom Chen <txchen@gmail.com>
2025-11-18 16:15:53 -0800

    Create 1057.CampusBikes.cpp

---
+++ leetcode/1057.CampusBikes.cpp
@@ -0,0 +1,70 @@
+class Solution {
+public:
+    int findDistance(vector<int>& worker, vector<int>& bike) {
+        return abs(worker[0] - bike[0]) + abs(worker[1] - bike[1]);
+    }
+
+    vector<int> assignBikes(vector<vector<int>>& workers, vector<vector<int>>& bikes) {
+        // List of WorkerBikeTuples's to store all the possible triplets
+        vector<tuple<int, int, int>> allTriplets;
+        
+        // Generate all the possible pairs
+        for (int worker = 0; worker < workers.size(); worker++) {
+            for (int bike = 0; bike < bikes.size(); bike++) {
+                int distance = findDistance(workers[worker], bikes[bike]);        
+                allTriplets.push_back({distance, worker, bike});
+            }
+        }
+        
+        // Sort the triplets. By default, each sorting will prioritize the
+        // Tuple's first value, then second value, and finally the third value
+        sort(allTriplets.begin(), allTriplets.end());  
+        
+        // Initialize all values to false, to signify no bikes have been taken
+        vector<int> bikeStatus(bikes.size(), false);
+        // Initialize all index to -1, to signify no worker has a bike
+        vector<int> workerStatus(workers.size(), -1);
+        // Keep track of how many worker-bike pairs have been made
+        int pairCount = 0;
+        
+        for (auto[dist, worker, bike] : allTriplets) { 
+            // If both worker and bike are free, assign them to each other
+            if (workerStatus[worker] == -1 && !bikeStatus[bike]) {
+                bikeStatus[bike] = true;
+                workerStatus[worker] = bike;
+                pairCount++;
+                
+                // If all the workers have the bike assigned, we can stop
+                if (pairCount == workers.size()) {
+                    return workerStatus;
+                }
+            }
+        }
+        
+        return workerStatus;
+    }
+};
+
+
+/**
+1057. Campus Bikes
+Medium
+
+On a campus represented on the X-Y plane, there are n workers and m bikes, with n <= m.
+
+You are given an array workers of length n where workers[i] = [xi, yi] is the position of the ith worker. 
+You are also given an array bikes of length m where bikes[j] = [xj, yj] is the position of the jth bike. 
+All the given positions are unique.
+
+Assign a bike to each worker. Among the available bikes and workers, we choose the (workeri, bikej) pair 
+with the shortest Manhattan distance between each other and assign the bike to that worker.
+
+If there are multiple (workeri, bikej) pairs with the same shortest Manhattan distance, we choose the pair 
+with the smallest worker index. If there are multiple ways to do that, we choose the pair with the smallest 
+bike index. Repeat this process until there are no available workers.
+
+Return an array answer of length n, where answer[i] is the index (0-indexed) of the bike that the ith worker 
+is assigned to.
+
+The Manhattan distance between two points p1 and p2 is Manhattan(p1, p2) = |p1.x - p2.x| + |p1.y - p2.y|.
+ */
