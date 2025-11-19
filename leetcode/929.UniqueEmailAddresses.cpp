7eb017ce30f9dd4b012a1c8744ed745cdbf341e3
Tom Chen <txchen@gmail.com>
2025-11-19 15:26:28 -0800

    Create 929.UniqueEmailAddresses.cpp

---
+++ leetcode/929.UniqueEmailAddresses.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    int numUniqueEmails(vector<string>& emails) {
+        set<string> result;
+        for (string& email : emails) {
+            result.insert(processEmail(email));
+        }
+        return result.size();
+    }
+
+    string processEmail(string& email) {
+        int indexAt = email.find("@");
+        string localName = email.substr(0, indexAt);
+        string suffix = email.substr(indexAt);
+
+        string actualLocalName;
+        for (char ch : localName) {
+            if (ch == '+') break;
+            if (ch != '.') actualLocalName += ch;
+        }
+        return actualLocalName + suffix;
+    }
+};
+
+/**
+929. Unique Email Addresses
+Easy
+
+Every valid email consists of a local name and a domain name, separated by the '@' sign. Besides lowercase letters, 
+the email may contain one or more '.' or '+'.
+
+    For example, in "alice@leetcode.com", "alice" is the local name, and "leetcode.com" is the domain name.
+
+If you add periods '.' between some characters in the local name part of an email address, mail sent there will be 
+forwarded to the same address without dots in the local name. Note that this rule does not apply to domain names.
+
+    For example, "alice.z@leetcode.com" and "alicez@leetcode.com" forward to the same email address.
+
+If you add a plus '+' in the local name, everything after the first plus sign will be ignored. This allows certain 
+emails to be filtered. Note that this rule does not apply to domain names.
+
+    For example, "m.y+name@email.com" will be forwarded to "my@email.com".
+
+It is possible to use both of these rules at the same time.
+
+Given an array of strings emails where we send one email to each emails[i], return the number of different addresses 
+that actually receive mails.
+ */
