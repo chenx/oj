547608e3968442aec04fb97b362b48285f24da95
Tom Chen <txchen@gmail.com>
2025-11-18 23:01:09 -0800

    Create 448.FindAllNumbersDisappearedInAnArray.cpp

---
+++ leetcode/448.FindAllNumbersDisappearedInAnArray.cpp
@@ -0,0 +1,34 @@
+
+class Solution {
+public:
+    vector<int> findDisappearedNumbers(vector<int>& nums) {
+        set<int> set;
+        for (int num : nums) set.insert(num);
+
+        vector<int> result;
+        for (int i = 1; i <= nums.size(); ++ i) {
+            if (! set.contains(i)) result.push_back(i);
+        }
+        return result;
+    }
+};
+
+/**
+448. Find All Numbers Disappeared in an Array
+Easy
+
+Given an array nums of n integers where nums[i] is in the range [1, n], return an array of all the integers 
+in the range [1, n] that do not appear in nums.
+
+ 
+
+Example 1:
+
+Input: nums = [4,3,2,7,8,2,3,1]
+Output: [5,6]
+
+Example 2:
+
+Input: nums = [1,1]
+Output: [2]
+ */
