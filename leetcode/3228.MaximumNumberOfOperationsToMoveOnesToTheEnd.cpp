09c90375cdd5ac8848e368d30897843b025af715
Tom Chen <txchen@gmail.com>
2025-11-19 23:29:06 -0800

    Update 3228.MaximumNumberOfOperationsToMoveOnesToTheEnd.cpp

--- leetcode/3228.MaximumNumberOfOperationsToMoveOnesToTheEnd.cpp
+++ leetcode/3228.MaximumNumberOfOperationsToMoveOnesToTheEnd.cpp
@@ -2,19 +2,17 @@
 class Solution {
 public:
     int maxOperations(string s) {
-        int countOne = 0;
-        int ans = 0;
-        for (int i = 0; i < s.length(); i++) {
+        int result = 0, numOfOnes = 0, n = s.length();
+
+        for (int i = 0; i < n; ++ i) {
             if (s[i] == '0') {
-                while ((i + 1) < s.length() && s[i + 1] == '0') {
-                    i++;
-                }
-                ans += countOne;
+                while (i + 1 < n && s[i+1] == '0') ++ i;
+                result += numOfOnes;
             } else {
-                countOne++;
+                numOfOnes ++;
             }
         }
-        return ans;
+        return result;
     }
 };
 
