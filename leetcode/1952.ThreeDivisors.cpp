70854d035dc0cc202d5f8ad3824fc4fa1a4498c9
Tom Chen <txchen@gmail.com>
2025-11-19 17:51:36 -0800

    Update 1952.ThreeDivisors.cpp

--- leetcode/1952.ThreeDivisors.cpp
+++ leetcode/1952.ThreeDivisors.cpp
@@ -4,9 +4,8 @@ public:
         int count = 0;
         for (int i = 1; i <= n; ++ i) {
             if (n % i == 0) ++ count;
-            if (count == 3) return true;
         }
-        return false;
+        return count == 3;
     }
 };
 
