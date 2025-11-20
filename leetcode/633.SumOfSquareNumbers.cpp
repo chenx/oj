0e10feca805e361ac64a8900b25bce1ee69f84f9
Tom Chen <txchen@gmail.com>
2025-11-19 20:15:43 -0800

    Update 633.SumOfSquareNumbers.cpp

--- leetcode/633.SumOfSquareNumbers.cpp
+++ leetcode/633.SumOfSquareNumbers.cpp
@@ -1,7 +1,7 @@
 // From: https://leetcode.com/problems/sum-of-square-numbers/editorial/
 // Time complexity : O(sqrt(c) * logc).
 //
-class Solution {
+class Solution2 {
 public:
     bool judgeSquareSum(int c) {
         for (long a = 0; a * a <= c; ++ a) {
@@ -9,6 +9,23 @@ public:
             if ((int) b == b) return true;
         }
         return false;
+    }
+};
+
+// Fermat theorem.
+// Time complexity : O(sqrt(c))
+class Solution {
+public:
+    bool judgeSquareSum(int c) {
+        for (int i = 2; i * i <= c; i++) {
+            int count = 0;
+            while (c % i == 0) {
+                count++;
+                c /= i;
+            }
+            if (i % 4 == 3 && count % 2 != 0) return false;
+        }
+        return c % 4 != 3;
     }
 };
 
