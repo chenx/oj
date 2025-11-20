21a840f4d08b615099f2a8ff942c1cb389d7b91b
Tom Chen <txchen@gmail.com>
2025-11-19 20:11:49 -0800

    Create 633.SumOfSquareNumbers.cpp

---
+++ leetcode/633.SumOfSquareNumbers.cpp
@@ -0,0 +1,33 @@
+// From: https://leetcode.com/problems/sum-of-square-numbers/editorial/
+// Time complexity : O(sqrt(c) * logc).
+//
+class Solution {
+public:
+    bool judgeSquareSum(int c) {
+        for (long a = 0; a * a <= c; ++ a) {
+            double b = sqrt(c - a*a);
+            if ((int) b == b) return true;
+        }
+        return false;
+    }
+};
+
+/**
+633. Sum of Square Numbers
+Medium
+
+Given a non-negative integer c, decide whether there're two integers a and b such that a2 + b2 = c.
+
+ 
+
+Example 1:
+
+Input: c = 5
+Output: true
+Explanation: 1 * 1 + 2 * 2 = 5
+
+Example 2:
+
+Input: c = 3
+Output: false
+ */
