191bcbcfdc90ab65417cddb299f6e96795322def
Tom Chen <txchen@gmail.com>
2025-11-20 23:36:30 -0800

    Create 983.MinimumCostForTickets.cpp

---
+++ leetcode/983.MinimumCostForTickets.cpp
@@ -0,0 +1,46 @@
+// From: https://leetcode.com/problems/minimum-cost-for-tickets/editorial/
+// Bottom up Dynamic Programming
+// Time complexity: O(K).
+// Space complexity: O(K).
+class Solution {
+public:
+    int mincostTickets(vector<int>& days, vector<int>& costs) {
+        int lastDay = days[days.size() - 1];
+        vector<int> dp(lastDay + 1, 0);
+        
+        int i = 0;
+        for (int day = 1; day <= lastDay; day++) {
+            if (day < days[i]) {
+                dp[day] = dp[day - 1];
+            } else {
+                i++;
+                dp[day] = min({dp[day - 1] + costs[0],
+                               dp[max(0, day - 7)] + costs[1],
+                               dp[max(0, day - 30)] + costs[2]});
+            }
+        }
+     
+        return dp[lastDay];
+    }
+};
+
+
+/**
+983. Minimum Cost For Tickets
+Medium
+
+You have planned some train traveling one year in advance. The days of the year in which 
+you will travel are given as an integer array days. Each day is an integer from 1 to 365.
+
+Train tickets are sold in three different ways:
+
+    a 1-day pass is sold for costs[0] dollars,
+    a 7-day pass is sold for costs[1] dollars, and
+    a 30-day pass is sold for costs[2] dollars.
+
+The passes allow that many days of consecutive travel.
+
+    For example, if we get a 7-day pass on day 2, then we can travel for 7 days: 2, 3, 4, 5, 6, 7, and 8.
+
+Return the minimum number of dollars you need to travel every day in the given list of days.
+ */
