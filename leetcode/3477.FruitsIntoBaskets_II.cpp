fabc24debc53790e5432f650e6d1a8b9b0467a5f
Tom Chen <txchen@gmail.com>
2025-11-20 16:43:15 -0800

    Create 3477.FruitsIntoBaskets_II.cpp

---
+++ leetcode/3477.FruitsIntoBaskets_II.cpp
@@ -0,0 +1,37 @@
+// From: https://leetcode.com/problems/fruits-into-baskets-ii/editorial/
+class Solution {
+public:
+    int numOfUnplacedFruits(vector<int>& fruits, vector<int>& baskets) {
+        int count = 0;
+        int n = baskets.size();
+        for (auto fruit : fruits) {
+            int unset = 1;
+            for (int i = 0; i < n; i++) {
+                if (fruit <= baskets[i]) {
+                    baskets[i] = 0;
+                    unset = 0;
+                    break;
+                }
+            }
+            count += unset;
+        }
+        return count;
+    }
+};
+
+/**
+3477. Fruits Into Baskets II
+Easy
+
+You are given two arrays of integers, fruits and baskets, each of length n, where fruits[i] represents 
+the quantity of the ith type of fruit, and baskets[j] represents the capacity of the jth basket.
+
+From left to right, place the fruits according to these rules:
+
+    Each fruit type must be placed in the leftmost available basket with a capacity greater than or 
+        equal to the quantity of that fruit type.
+    Each basket can hold only one type of fruit.
+    If a fruit type cannot be placed in any basket, it remains unplaced.
+
+Return the number of fruit types that remain unplaced after all possible allocations are made.
+ */
