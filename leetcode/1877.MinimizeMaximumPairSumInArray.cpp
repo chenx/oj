45a1e7d1263f6ccc6a18676de2010e2a2d2144cd
Tom Chen <txchen@gmail.com>
2025-11-20 20:24:05 -0800

    Create 1877.MinimizeMaximumPairSumInArray.cpp

---
+++ leetcode/1877.MinimizeMaximumPairSumInArray.cpp
@@ -0,0 +1,43 @@
+// From: https://leetcode.com/problems/minimize-maximum-pair-sum-in-array/editorial/
+// Sorting.
+// Time complexity O(NlogN)
+// Space complexity O(logN)
+//
+class Solution {
+public:
+    int minPairSum(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        
+        int maxSum = 0;
+        for (int i = 0; i < nums.size() / 2; i++) {
+            maxSum = max(maxSum, nums[i] + nums[nums.size() - 1 - i]);
+        }
+        
+        return maxSum;
+    }
+};
+
+/**
+1877. Minimize Maximum Pair Sum in Array
+Medium
+
+The pair sum of a pair (a,b) is equal to a + b. The maximum pair sum is the largest pair sum in a list of pairs.
+
+    For example, if we have pairs (1,5), (2,3), and (4,4), the maximum pair sum would be max(1+5, 2+3, 4+4) = max(6, 5, 8) = 8.
+
+Given an array nums of even length n, pair up the elements of nums into n / 2 pairs such that:
+
+    Each element of nums is in exactly one pair, and
+    The maximum pair sum is minimized.
+
+Return the minimized maximum pair sum after optimally pairing up the elements.
+
+ 
+
+Example 1:
+
+Input: nums = [3,5,2,3]
+Output: 7
+Explanation: The elements can be paired up into pairs (3,3) and (5,2).
+The maximum pair sum is max(3+3, 5+2) = max(6, 7) = 7.
+ */
