7044a91b62f7a39bb083ea5c9cd812ae158e785f
Tom Chen <txchen@gmail.com>
2025-11-21 17:04:57 -0800

    Update 1216.ValidPalindrome_III.cpp

--- leetcode/1216.ValidPalindrome_III.cpp
+++ leetcode/1216.ValidPalindrome_III.cpp
@@ -1,3 +1,20 @@
+// Same as Solution.
+class Solution2 {
+public:
+    bool isValidPalindrome(string s, int k) {
+        int n = s.length();
+        vector<vector<int>> memo(n, vector<int>(n, 0));
+
+        for (int i = n - 2; i >= 0; i--)
+            for (int j = i + 1; j < n; j++) {
+                if (s[i] == s[j]) memo[i][j] = memo[i + 1][j - 1];
+                else memo[i][j] = 1 + min(memo[i + 1][j], memo[i][j - 1]);
+            }
+
+        return memo[0][n - 1] <= k;
+    }
+};
+
 // From: https://leetcode.com/problems/valid-palindrome-iii/editorial/
 // Bottom up DP.
 // Time complexity : O(n^2). Where n is the length of string s
