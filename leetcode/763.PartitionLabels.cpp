f963a5f46f1d711a9381b1f052f03a922d73fd60
Tom Chen <txchen@gmail.com>
2025-11-24 00:10:50 -0800

    Create 763.PartitionLabels.cpp

---
+++ leetcode/763.PartitionLabels.cpp
@@ -0,0 +1,57 @@
+// From: https://leetcode.com/problems/partition-labels/editorial/
+// Let n be the size of the input string s and k be the number of unique characters in s.
+// Time complexity: O(n)
+// Space complexity: O(k)
+//
+class Solution {
+public:
+    vector<int> partitionLabels(string s) {
+        // Stores the last index of each character in 's'
+        vector<int> lastOccurrence(26, 0);
+        for (int i = 0; i < s.size(); ++i) {
+            lastOccurrence[s[i] - 'a'] = i;
+        }
+
+        int partitionEnd = 0, partitionStart = 0;
+        vector<int> partitionSizes;
+        for (int i = 0; i < s.size(); ++i) {
+            partitionEnd = max(partitionEnd, lastOccurrence[s[i] - 'a']);
+            // End of the current partition
+            if (i == partitionEnd) {
+                partitionSizes.push_back(i - partitionStart + 1);
+                partitionStart = i + 1;
+            }
+        }
+        return partitionSizes;
+    }
+};
+
+
+/**
+763. Partition Labels
+Medium
+
+You are given a string s. We want to partition the string into as many parts as possible so that each letter appears 
+in at most one part. For example, the string "ababcc" can be partitioned into ["abab", "cc"], but partitions such as 
+["aba", "bcc"] or ["ab", "ab", "cc"] are invalid.
+
+Note that the partition is done so that after concatenating all the parts in order, the resultant string should be s.
+
+Return a list of integers representing the size of these parts.
+
+ 
+
+Example 1:
+
+Input: s = "ababcbacadefegdehijhklij"
+Output: [9,7,8]
+Explanation:
+The partition is "ababcbaca", "defegde", "hijhklij".
+This is a partition so that each letter appears in at most one part.
+A partition like "ababcbacadefegde", "hijhklij" is incorrect, because it splits s into less parts.
+
+Example 2:
+
+Input: s = "eccbbbbdec"
+Output: [10]
+ */
