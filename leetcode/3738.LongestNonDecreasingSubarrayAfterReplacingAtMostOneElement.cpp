a56e57689748fb32dbf02a332f923c2e4d9d5610
Tom Chen <txchen@gmail.com>
2025-11-23 23:43:16 -0800

    Update 3738.LongestNonDecreasingSubarrayAfterReplacingAtMostOneElement.cpp

--- leetcode/3738.LongestNonDecreasingSubarrayAfterReplacingAtMostOneElement.cpp
+++ leetcode/3738.LongestNonDecreasingSubarrayAfterReplacingAtMostOneElement.cpp
@@ -1,3 +1,46 @@
+// Works. 
+// From: 
+// - https://www.youtube.com/watch?v=eWBhDWkxrtU
+// - https://leetcode.com/problems/longest-non-decreasing-subarray-after-replacing-at-most-one-element/submissions/1824171945/
+//
+class Solution {
+public:
+    int longestSubarray(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> left(n, 1), right(n, 1);
+
+        for (int i = 1; i < n; i++) {
+            if (nums[i] >= nums[i - 1]) {
+                left[i] = 1 + left[i - 1];
+            }
+        }
+
+        for (int i = n - 2; i >= 0; i--) {
+            if (nums[i] <= nums[i + 1]) {
+                right[i] = 1 + right[i + 1];
+            }
+        }
+
+        int ans = *max_element(left.begin(), left.end());
+
+        for (int i = 0; i < n; i++) {
+            if (i > 0) {
+                ans = max(ans, left[i - 1] + 1);
+            }
+            if (i < (n - 1)) {
+                ans = max(ans, right[i + 1] + 1);
+            }
+
+            if (i > 0 && i < (n - 1) && nums[i - 1] <= nums[i + 1]) {
+                ans = max(ans, left[i - 1] + 1 + right[i + 1]);
+            }
+        }
+
+        return ans;
+    }
+};
+
+
 // DP
 // See Python solution: https://blog.csdn.net/codename_cys/article/details/154679776
 
