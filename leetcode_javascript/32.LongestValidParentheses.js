e848a3e52968824cf143f6d94147da326613be1c
Tom Chen <txchen@gmail.com>
2025-11-24 16:09:48 -0800

    Create 32.LongestValidParentheses.js

---
+++ leetcode_javascript/32.LongestValidParentheses.js
@@ -0,0 +1,62 @@
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var longestValidParentheses = function(s) {
+    let openCount = 0, total = 0, maxLen = 0;
+
+    for (let i = 0; i < s.length; ++ i) {
+        getMaxLen(s.charAt(i) === '(');
+    }
+
+    openCount = total = 0;
+    for (let i = s.length - 1; i >= 0; -- i) {
+        getMaxLen(s.charAt(i) === ')');
+    }
+    return maxLen;
+    
+    function getMaxLen(isOpenChar) {
+            if (isOpenChar) {
+                ++ openCount;
+                ++ total;
+            } else {
+                -- openCount;
+                ++ total;
+            }
+
+            if (openCount == 0) {
+                maxLen = Math.max(maxLen, total);
+            } else if (openCount < 0) {
+                openCount = 0;
+                total = 0;
+            }
+    }
+};
+
+
+/**
+32. Longest Valid Parentheses
+Hard
+
+Given a string containing just the characters '(' and ')', return the length of the longest valid (well-formed) parentheses.
+
+ 
+
+Example 1:
+
+Input: s = "(()"
+Output: 2
+Explanation: The longest valid parentheses substring is "()".
+
+Example 2:
+
+Input: s = ")()())"
+Output: 4
+Explanation: The longest valid parentheses substring is "()()".
+
+Example 3:
+
+Input: s = ""
+Output: 0
+ */
