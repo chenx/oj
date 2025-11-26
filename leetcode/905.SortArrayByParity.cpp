e465c0ca00c32e1bb86e2a389c3564c2d6f51135
Tom Chen <txchen@gmail.com>
2025-11-26 15:48:06 -0800

    Create 905.SortArrayByParity.cpp

---
+++ leetcode/905.SortArrayByParity.cpp
@@ -0,0 +1,36 @@
+// Time: O(n)
+class Solution {
+public:
+    vector<int> sortArrayByParity(vector<int>& nums) {
+        int p = 0;
+        for (int i = 0; i < nums.size(); ++ i) {
+            if (nums[i] % 2 == 0) {
+                swap(nums[i], nums[p ++]);
+            }
+        }
+        return nums;
+    }
+};
+
+
+/**
+905. Sort Array By Parity
+Easy
+
+Given an integer array nums, move all the even integers at the beginning of the array followed by all the odd integers.
+
+Return any array that satisfies this condition.
+
+ 
+
+Example 1:
+
+Input: nums = [3,1,2,4]
+Output: [2,4,3,1]
+Explanation: The outputs [4,2,3,1], [2,4,1,3], and [4,2,1,3] would also be accepted.
+
+Example 2:
+
+Input: nums = [0]
+Output: [0]
+ */
