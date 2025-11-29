d45b76847b6f3dcda6007709d6ff04fd2407c800
Tom Chen <txchen@gmail.com>
2025-11-28 16:35:35 -0800

    Update and rename TwoSum.cpp to 1.TwoSum.cpp

--- leetcode/1.TwoSum.cpp
+++ leetcode/1.TwoSum.cpp
@@ -1,3 +1,30 @@
+class Solution8 {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        map<int, int> map;
+        for (int i = 0; i < nums.size(); ++ i) {
+            if (map.contains(target - nums[i])) {
+                return {map[target - nums[i]], i};
+            } else {
+                map[nums[i]] = i;
+            }
+        }
+        return {-1,-1};
+    }
+
+    vector<int> twoSum2(vector<int>& nums, int target) {
+        map<int, int> map;
+        for (int i = 0; i < nums.size(); ++ i) {
+            if (map.contains(nums[i])) {
+                return {map[nums[i]], i};
+            } else {
+                map[target - nums[i]] = i;
+            }
+        }
+        return {-1,-1};
+    }
+};
+
 //
 // http://www.leetcode.com/onlinejudge#
 // 3sum.
@@ -208,4 +235,5 @@ You may assume that each input would have exactly one solution.
 Input: numbers={2, 7, 11, 15}, target=9
 Output: index1=1, index2=2 
  */
+
 
