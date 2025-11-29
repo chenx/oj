e4c9c523cca8440ec3a407da3f9faced44cc8ba3
Tom Chen <txchen@gmail.com>
2025-11-28 23:02:22 -0800

    Update 973.KClosestPointsToOrigin.cpp

--- leetcode/973.KClosestPointsToOrigin.cpp
+++ leetcode/973.KClosestPointsToOrigin.cpp
@@ -1,3 +1,115 @@
+// Quick Select.
+// See: https://leetcode.com/problems/k-closest-points-to-origin/editorial/
+// O(n) time.
+// O(1) space.
+class Solution4 {
+public:
+    vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
+        return quickSelect(points, k);
+    }
+    
+private:
+    vector<vector<int>> quickSelect(vector<vector<int>>& points, int k) {
+        int left = 0, right = points.size() - 1;
+        int pivotIndex = points.size();
+        while (pivotIndex != k) {
+            // Repeatedly partition the vector while narrowing in on the kth element.
+            pivotIndex = partition(points, left, right);
+            if (pivotIndex < k) {
+                left = pivotIndex;
+            } else {
+                right = pivotIndex - 1;
+            }
+        }
+        
+        // Return the first k elements of the partially sorted vector
+        return vector<vector<int>>(points.begin(), points.begin() + k);
+    };
+
+    int partition(vector<vector<int>>& points, int left, int right) {
+        vector<int>& pivot = choosePivot(points, left, right);
+        int pivotDist = squaredDistance(pivot);
+        while (left < right) {
+            // Iterate through the range and swap elements to make sure
+            // that all points closer than the pivot are to the left
+            if (squaredDistance(points[left]) >= pivotDist) {
+                points[left].swap(points[right]);
+                right--;
+            } else {
+                left++;
+            }
+        }
+        
+        // Ensure the left pointer is just past the end of
+        // the left range then return it as the new pivotIndex
+        if (squaredDistance(points[left]) < pivotDist) left++;
+        return left;
+    };
+
+    vector<int>& choosePivot(vector<vector<int>>& points, int left, int right) {
+        // Choose a pivot element of the vector
+        return points[left + (right - left) / 2];
+    }
+    
+    int squaredDistance(vector<int>& point) {
+        // Calculate and return the squared Euclidean distance
+        return point[0] * point[0] + point[1] * point[1];
+    }
+};
+
+
+class Solution3 {
+public:
+    vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
+        auto comp = [&](vector<int> a, vector<int> b) {
+            return (a[0]*a[0] + a[1]*a[1]) < (b[0]*b[0] + b[1]*b[1]);
+        };
+
+        priority_queue<vector<int>, vector<vector<int>>, decltype(comp)> maxHeap(comp);
+
+        for (auto point : points) {
+            maxHeap.push(point);
+            if (maxHeap.size() > k) maxHeap.pop();
+        }
+
+        vector<vector<int>> result;
+        while (! maxHeap.empty()) {
+            result.push_back(maxHeap.top());
+            maxHeap.pop();
+        }
+        return result;
+    }
+};
+
+
+// Time complexity: O(N⋅logk)
+// Space complexity: O(k)
+class Solution2 {
+    struct comp {
+        bool operator()(vector<int>& a, vector<int>& b) {
+            return (a[0]*a[0] + a[1]*a[1]) < (b[0]*b[0] + b[1]*b[1]);
+        }
+    };
+
+public:
+    vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
+        priority_queue<vector<int>, vector<vector<int>>, comp> maxHeap;
+
+        for (auto point : points) {
+            maxHeap.push(point);
+            if (maxHeap.size() > k) maxHeap.pop();
+        }
+
+        vector<vector<int>> result;
+        while (! maxHeap.empty()) {
+            result.push_back(maxHeap.top());
+            maxHeap.pop();
+        }
+        return result;
+    }
+};
+
+
 struct comp {
     bool operator()(pair<vector<int>, int>&a, pair<vector<int>, int>&b) {
         return a.second < b.second;
