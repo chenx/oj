a0bc8616a705047b2974b856db475603153a08d4
Tom Chen <txchen@gmail.com>
2025-11-28 17:26:56 -0800

    Update and rename ClimbingStairs.cpp to 70.ClimbingStairs.cpp

--- leetcode/70.ClimbingStairs.cpp
+++ leetcode/70.ClimbingStairs.cpp
@@ -1,10 +1,15 @@
-//
-// http://www.leetcode.com/onlinejudge#
-// 3sum.
-// @Author: Xin Chen
-// @Created on: 12/13/2012
-// @Last modified: 12/13/2012
-//
+class Solution3 {
+public:
+    int climbStairs(int n) {
+        int a = 1, b = 1;
+        for (int i = 2; i <= n; ++ i) {
+            int tmp = a + b;
+            a = b;
+            b = tmp;
+        }
+        return b;
+    }
+};
 
 // This works too. 10/26/2014
 class Solution2 {
@@ -54,3 +59,33 @@ public:
         return f;
     }
 };
+
+
+
+/**
+70. Climbing Stairs
+Easy
+
+You are climbing a staircase. It takes n steps to reach the top.
+
+Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+
+ 
+
+Example 1:
+
+Input: n = 2
+Output: 2
+Explanation: There are two ways to climb to the top.
+1. 1 step + 1 step
+2. 2 steps
+
+Example 2:
+
+Input: n = 3
+Output: 3
+Explanation: There are three ways to climb to the top.
+1. 1 step + 1 step + 1 step
+2. 1 step + 2 steps
+3. 2 steps + 1 step
+ */
