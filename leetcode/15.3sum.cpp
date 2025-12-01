d0241c75c494fc421be5c0178f9be60502f4e19a
Tom Chen <txchen@gmail.com>
2025-12-01 12:44:27 -0800

    Update and rename 3sum.cpp to 15.3sum.cpp

--- leetcode/15.3sum.cpp
+++ leetcode/15.3sum.cpp
@@ -470,7 +470,7 @@ int main() {
 }
 
 /*
-Problem:
+15. 3 Sum
         
 Given an array S of n integers, are there elements a, b, c in S 
 such that a + b + c = 0? Find all unique triplets in the array 
@@ -488,4 +488,5 @@ Note:
     (-1, -1, 2)
 
  */
+
 
