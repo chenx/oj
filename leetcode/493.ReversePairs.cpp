ff766992461459ce973073d34abcd32f5a9da031
Tom Chen <txchen@gmail.com>
2025-12-01 20:21:59 -0800

    Update 493.ReversePairs.cpp

--- leetcode/493.ReversePairs.cpp
+++ leetcode/493.ReversePairs.cpp
@@ -1,3 +1,41 @@
+// Modified from Solution2, but using my own merge method.
+class Solution3 {
+    vector<int> B;
+public:
+    void merge(vector<int>& A, int start, int mid, int end) {
+        int i = start, j = mid + 1, k = start;
+        for (; i <= mid && j <= end; ) {
+            B[k ++] = A[i] < A[j] ? A[i ++] : A[j ++];
+        }
+        while (i <= mid) B[k ++] = A[i ++];
+        while (j <= end) B[k ++] = A[j ++];
+
+        for (int i = start; i <= end; ++ i) {
+            A[i] = B[i];
+        }
+    }
+
+    int mergesort_and_count(vector<int>& A, int start, int end) {
+        if (start >= end) return 0;
+
+        int mid = (start + end) / 2;
+        int count = mergesort_and_count(A, start, mid) + mergesort_and_count(A, mid + 1, end);
+        int j = mid + 1;
+        for (int i = start; i <= mid; i++) {
+            while (j <= end && A[i] > A[j] * 2LL) j++;
+            count += j - (mid + 1);
+        }
+        merge(A, start, mid, end);
+        return count;
+    }
+
+    int reversePairs(vector<int>& nums) {
+        B.resize(nums.size());
+        return mergesort_and_count(nums, 0, nums.size() - 1);
+    }
+};
+
+
 // From: https://leetcode.com/problems/reverse-pairs/editorial/
 // Modified Merge Sort
 // Time complexity: O(nlogn)
