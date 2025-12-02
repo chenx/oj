f85b7fbe449cb83e40d9e72af4f324e1a159e13e
Tom Chen <txchen@gmail.com>
2025-12-01 19:17:07 -0800

    Update 3607.PowerGridMaintenance.cpp

--- leetcode/3607.PowerGridMaintenance.cpp
+++ leetcode/3607.PowerGridMaintenance.cpp
@@ -1,5 +1,83 @@
+// using PowerGrid = priority_queue<int, vector<int>, greater<int>>; // minHeap.
+// using Graph = vector<vector<int>>;
+
+typedef priority_queue<int, vector<int>, greater<int>> PowerGrid;
+typedef vector<vector<int>> Graph;
+
+class Solution {
+private:
+    struct Vertex {
+        int vertexId;
+        bool offline = false;
+        int powerGridId = -1;
+
+        Vertex() {}
+        Vertex(int id) : vertexId(id) {}
+    };
+
+    vector<Vertex> vertices = vector<Vertex>();
+
+    void traverse(Vertex& u, int powerGridId, PowerGrid& powerGrid, Graph& graph) {
+        u.powerGridId = powerGridId;
+        powerGrid.push(u.vertexId);
+        for (int vid : graph[u.vertexId]) {
+            Vertex& v = vertices[vid];
+            if (v.powerGridId == -1) {
+                traverse(v, powerGridId, powerGrid, graph);
+            }
+        }
+    }
+
+public:
+    vector<int> processQueries(int c, vector<vector<int>>& connections, vector<vector<int>>& queries) {
+        Graph graph(c + 1);
+        vertices.resize(c + 1);
+
+        for (int i = 1; i <= c; i++) {
+            vertices[i] = Vertex(i);
+        }
+
+        for (auto& conn : connections) {
+            graph[conn[0]].push_back(conn[1]);
+            graph[conn[1]].push_back(conn[0]);
+        }
+
+        vector<PowerGrid> powerGrids;
+        for (int i = 1, powerGridId = 0; i <= c; ++ i) {
+            auto& v = vertices[i];
+            if (v.powerGridId == -1) {
+                PowerGrid powerGrid;
+                traverse(v, powerGridId, powerGrid, graph);
+                powerGrids.push_back(powerGrid);
+                ++ powerGridId;
+            }
+        }
+
+        vector<int> ans;
+        for (auto& q : queries) {
+            int op = q[0], x = q[1];
+            if (op == 1) {
+                if (!vertices[x].offline) {
+                    ans.push_back(x);
+                } else {
+                    auto& powerGrid = powerGrids[vertices[x].powerGridId];
+                    while (!powerGrid.empty() && vertices[powerGrid.top()].offline) {
+                        powerGrid.pop();
+                    }
+                    ans.push_back(!powerGrid.empty() ? powerGrid.top() : -1);
+                }
+            } else if (op == 2) {
+                vertices[x].offline = true;
+            }
+        }
+        return ans;
+    }
+};
+
+
 // See: https://leetcode.com/problems/power-grid-maintenance/editorial/
 // Approach 2: DFS/BFS + Priority Queue
+// Let m be the length of connections, i.e., the number of edges in the graph; q is the length of queries.
 // Time complexity: O(m+clogc+q).
 // Space complexity: O(c+m).
 class Vertex {
