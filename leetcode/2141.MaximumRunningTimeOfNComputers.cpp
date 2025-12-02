024d79070a0c0b902858c18fff715fdd9826f93d
Tom Chen <txchen@gmail.com>
2025-12-01 22:19:17 -0800

    Create 2141.MaximumRunningTimeOfNComputers.cpp

---
+++ leetcode/2141.MaximumRunningTimeOfNComputers.cpp
@@ -0,0 +1,50 @@
+// From: https://leetcode.com/problems/maximum-running-time-of-n-computers/editorial/
+// Binary search.
+//
+// Let m be the length of the input array batteries and k be the maximum power of one battery.
+// Time complexity: O(m⋅logk)
+// Space complexity: O(1)
+//
+class Solution {
+public:
+    long long maxRunTime(int n, vector<int>& batteries) {
+        long sumPower = 0;
+        for (int power : batteries) sumPower += power;
+
+        long left = 1, right = sumPower / n;
+        while (left < right){
+            long target = right - (right - left) / 2;
+            long extra = 0;
+            
+            for (int power : batteries)
+                extra += min(power, target);
+
+            if (extra >= (long)(n * target)) left = target;
+            else right = target - 1;
+        }
+        return left;
+    }
+
+    long min(long a, long b) {
+        return a < b ? a : b;
+    }
+};
+
+
+/**
+2141. Maximum Running Time of N Computers
+Hard
+
+You have n computers. You are given the integer n and a 0-indexed integer array batteries where the 
+ith battery can run a computer for batteries[i] minutes. You are interested in running all n computers 
+simultaneously using the given batteries.
+
+Initially, you can insert at most one battery into each computer. After that and at any integer time 
+moment, you can remove a battery from a computer and insert another battery any number of times. The 
+inserted battery can be a totally new battery or a battery from another computer. You may assume that 
+the removing and inserting processes take no time.
+
+Note that the batteries cannot be recharged.
+
+Return the maximum number of minutes you can run all the n computers simultaneously.
+ */
