b4b2f1b22dcd32c4f8f7a7c9fa45fdefa3faa37f
Tom Chen <txchen@gmail.com>
2025-12-04 22:54:21 -0800

    Create 2703.ReturnLengthOfArgumentsPassed.cpp

---
+++ leetcode_javascript/2703.ReturnLengthOfArgumentsPassed.cpp
@@ -0,0 +1,50 @@
+/**
+ * @param {...(null|boolean|number|string|Array|Object)} args
+ * @return {number}
+ */
+var argumentsLength = function(...args) {
+    return [...args].length;
+};
+
+
+/**
+ * @param {...(null|boolean|number|string|Array|Object)} args
+ * @return {number}
+ */
+var argumentsLength = function(...args) {
+    return args.length;
+};
+
+
+/**
+ * @param {...(null|boolean|number|string|Array|Object)} args
+ * @return {number}
+ */
+var argumentsLength = function(...args) {
+    return arguments.length;
+};
+
+
+/**
+2703. Return Length of Arguments Passed
+Easy
+ 
+
+Example 1:
+
+Input: args = [5]
+Output: 1
+Explanation:
+argumentsLength(5); // 1
+
+One value was passed to the function so it should return 1.
+
+Example 2:
+
+Input: args = [{}, null, "3"]
+Output: 3
+Explanation: 
+argumentsLength({}, null, "3"); // 3
+
+Three values were passed to the function so it should return 3.
+ */
