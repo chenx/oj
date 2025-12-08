acd8280cd72a6efaa8165c7c0610de87cfed7653
Tom Chen <txchen@gmail.com>
2025-12-07 22:28:11 -0800

    Create 3623.CountNumberOfTrapezoids_I.cpp

---
+++ leetcode/3623.CountNumberOfTrapezoids_I.cpp
@@ -0,0 +1,42 @@
+// See: https://leetcode.com/problems/count-number-of-trapezoids-i/editorial/
+// Time: O(n)
+// Space: O(n)
+class Solution {
+public:
+    int countTrapezoids(vector<vector<int>>& points) {
+        unordered_map<int, int> pointNum;
+        const int mod = 1e9 + 7;
+        long long ans = 0, sum = 0;
+        for (auto& point : points) {
+            pointNum[point[1]]++;
+        }
+        for (auto& [_, pNum] : pointNum) {
+            long long edge = (long long)pNum * (pNum - 1) / 2;
+            ans += edge * sum;
+            sum += edge;
+        }
+        return ans % mod;
+    }
+};
+
+
+/**
+3623. Count Number of Trapezoids I
+Medium
+
+You are given a 2D integer array points, where points[i] = [xi, yi] represents the coordinates of the ith point on the Cartesian plane.
+
+A horizontal trapezoid is a convex quadrilateral with at least one pair of horizontal sides (i.e. parallel to the x-axis). Two lines are parallel if and only if they have the same slope.
+
+Return the number of unique horizontal trapezoids that can be formed by choosing any four distinct points from points.
+
+Since the answer may be very large, return it modulo 109 + 7.
+
+ 
+
+Example 1:
+
+Input: points = [[1,0],[2,0],[3,0],[2,2],[3,2]]
+
+Output: 3
+ */
