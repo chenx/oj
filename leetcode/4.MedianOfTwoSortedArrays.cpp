c53f814d77bcad826856a4a929d2b224b3078917
Tom Chen <txchen@gmail.com>
2025-12-08 21:03:56 -0800

    Update 4.MedianOfTwoSortedArrays.cpp

--- leetcode/4.MedianOfTwoSortedArrays.cpp
+++ leetcode/4.MedianOfTwoSortedArrays.cpp
@@ -1,3 +1,45 @@
+// From: https://leetcode.com/problems/median-of-two-sorted-arrays/editorial/
+// Time: O(log(min(m, n)))
+// Space: O(1)
+class Solution4 {
+public:
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        if (nums1.size() > nums2.size()) {
+            return findMedianSortedArrays(nums2, nums1);
+        }
+
+        int m = nums1.size(), n = nums2.size();
+        int left = 0, right = m;
+
+        while (left <= right) {
+            int partitionA = (left + right) / 2;
+            int partitionB = (m + n + 1) / 2 - partitionA;
+
+            int maxLeftA = (partitionA == 0) ? INT_MIN : nums1[partitionA - 1];
+            int minRightA = (partitionA == m) ? INT_MAX : nums1[partitionA];
+            int maxLeftB = (partitionB == 0) ? INT_MIN : nums2[partitionB - 1];
+            int minRightB = (partitionB == n) ? INT_MAX : nums2[partitionB];
+
+            if (maxLeftA <= minRightB && maxLeftB <= minRightA) {
+                if ((m + n) % 2 == 0) {
+                    return (max(maxLeftA, maxLeftB) +
+                            min(minRightA, minRightB)) /
+                           2.0;
+                } else {
+                    return max(maxLeftA, maxLeftB);
+                }
+            } else if (maxLeftA > minRightB) {
+                right = partitionA - 1;
+            } else {
+                left = partitionA + 1;
+            }
+        }
+
+        return 0.0;
+    }
+};
+
+
 //
 // http://www.leetcode.com/onlinejudge#
 // @Author: Xin Chen
@@ -346,4 +388,5 @@ There are two sorted arrays A and B of size m and n respectively.
 Find the median of the two sorted arrays. The overall run time 
 complexity should be O(log (m+n)).
  */
+
 
