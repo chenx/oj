34347f9d79eb8fec86419acabbda47728ebe2468
Tom Chen <txchen@gmail.com>
2025-12-09 22:11:43 -0800

    Update and rename ContainsDuplicate_II.cpp to 219.ContainsDuplicate_II.cpp

--- leetcode/219.ContainsDuplicate_II.cpp
+++ leetcode/219.ContainsDuplicate_II.cpp
@@ -1,3 +1,21 @@
+// See: https://leetcode.com/problems/contains-duplicate-ii/editorial/
+// Time: O(n)
+// Space: O(min(n, k))
+class Solution4 {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        unordered_set<int> set;
+        for (int i = 0; i < nums.size(); ++i) {
+            if (set.contains(nums[i])) return true;
+            set.insert(nums[i]);
+            if (set.size() > k) {
+                set.erase(nums[i - k]);
+            }
+        }
+        return false;
+    }
+};
+
 // Works.
 class Solution3 {
 public:
