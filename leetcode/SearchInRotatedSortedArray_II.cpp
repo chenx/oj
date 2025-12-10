a6c79bd36451e1be3a5ecab4413b65d196f56a49
Tom Chen <txchen@gmail.com>
2025-12-09 16:11:47 -0800

    Update SearchInRotatedSortedArray_II.cpp

--- leetcode/SearchInRotatedSortedArray_II.cpp
+++ leetcode/SearchInRotatedSortedArray_II.cpp
@@ -1,3 +1,50 @@
+// From: https://leetcode.com/problems/search-in-rotated-sorted-array-ii/editorial/
+// Time: O(n) worst case, O(logn) best case.
+// Space: O(1)
+class Solution2 {
+public:
+    bool search(vector<int>& nums, int target) {
+        int n = nums.size();
+        if (n == 0) return false;
+        int end = n - 1, start = 0;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            if (nums[mid] == target) {
+                return true;
+            }
+
+            if (nums[start] == nums[mid]) {
+                start++;
+                continue;
+            }
+
+            // which array does pivot belong to.
+            bool pivotArray = nums[start] <= nums[mid];
+            // which array does target belong to.
+            bool targetArray = nums[start] <= target;
+
+            if (pivotArray ^ targetArray) {
+                // If pivot and target exist in different sorted arrays, recall 
+                // that xor is true only when both the operands are distinct
+                if (pivotArray) {
+                    start = mid + 1;  // pivot in the first, target in the second
+                } else {
+                    end = mid - 1;  // target in the first, pivot in the second
+                }
+            } else {  // If pivot and target exist in same sorted array
+                if (nums[mid] < target) {
+                    start = mid + 1;
+                } else {
+                    end = mid - 1;
+                }
+            }
+        }
+        return false;
+    }
+};
+
 //
 // http://www.leetcode.com/onlinejudge#
 // @Author: Xin Chen
@@ -34,3 +81,4 @@ public:
         return false;
     }
 };
+
