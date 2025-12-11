f7a0c8ebf56cf54bc403934863847c0a169c1e6f
Tom Chen <txchen@gmail.com>
2025-12-10 18:30:08 -0800

    Update and rename MeetingRooms_II.cpp to 253.MeetingRooms_II.cpp

--- leetcode/253.MeetingRooms_II.cpp
+++ leetcode/253.MeetingRooms_II.cpp
@@ -3,7 +3,7 @@ public:
     int minMeetingRooms(vector<vector<int>>& intervals) {
         sort(intervals.begin(), intervals.end(), comp);
 
-        priority_queue<int, vector<int>, greater<int>> pq;
+        priority_queue<int, vector<int>, greater<int>> pq; // minHeap.
         for (int i = 0; i < intervals.size(); ++ i) {
             if (!pq.empty() && intervals[i][0] >= pq.top()) pq.pop();  // "if", not "while"!!
             pq.push(intervals[i][1]);
