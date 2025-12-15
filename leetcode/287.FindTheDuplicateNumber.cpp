953a5e4bc532a0bff7802077790b95351ffd69ce
Tom Chen <txchen@gmail.com>
2025-12-14 18:07:09 -0800

    Add cyclic sort solution for finding duplicate number
    Implemented a cyclic sort algorithm to find the duplicate number in an array.

--- leetcode/287.FindTheDuplicateNumber.cpp
+++ leetcode/287.FindTheDuplicateNumber.cpp
@@ -1,3 +1,18 @@
+// From: https://leetcode.com/problems/find-the-duplicate-number/editorial/
+// Cyclic sort
+// Time: O(n)
+// Space: O(1)
+class Solution2 {
+public:
+    int findDuplicate(vector<int>& nums) {
+        while (nums[0] != nums[nums[0]]) {
+            swap(nums[0], nums[nums[0]]);
+        }
+        return nums[0];
+    }
+};
+
+
 // All 3 methods below are O(N) time, O(1) space.
 class Solution {
 public:
