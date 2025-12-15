599fa0c7c0b82d0b3803b1b0b23f4490239a0a99
Tom Chen <txchen@gmail.com>
2025-12-15 15:49:06 -0800

    Refactor kSmallestPairs to use vector and MinHeap
    Refactor kSmallestPairs method to use vector and MinHeap for better clarity and efficiency.

--- leetcode/FindKPairsWithSmallestSums.cpp
+++ leetcode/FindKPairsWithSmallestSums.cpp
@@ -1,3 +1,39 @@
+// Similar to Solution. Use vector instead of pair.
+//
+// m is the size of nums1 and n is the size of nums2.
+// Time complexity: O(min(k⋅logk,m⋅n⋅log(m⋅n)))
+// Space complexity: O(min(k,m⋅n))
+class Solution2 {
+public:
+    vector<vector<int>> kSmallestPairs(vector<int>& nums1, vector<int>& nums2, int k) {
+        vector<vector<int>> ans;
+        int n1 = nums1.size(), n2 = nums2.size();
+
+        priority_queue<vector<int>, vector<vector<int>>, greater<vector<int>>> minHeap;
+
+        minHeap.push({nums1[0] + nums2[0], 0, 0});
+        set<vector<int>> visited;
+
+        for (; k > 0 && !minHeap.empty(); -- k) {
+            auto top = minHeap.top();
+            int sum = top[0], i = top[1], j = top[2];
+            minHeap.pop();
+            ans.push_back({nums1[i], nums2[j]});
+    
+            if (i + 1 < n1 && !visited.contains({i + 1, j})) {
+                minHeap.push({nums1[i + 1] + nums2[j], i + 1, j});
+                visited.insert({i + 1, j});
+            }
+            if (j + 1 < n2 && !visited.contains({1, j + 1})) {
+                minHeap.push({nums1[i] + nums2[j + 1], i, j + 1});
+                visited.insert({i, j + 1});
+            }
+        }
+        return ans;
+    }
+};
+
+
 // Works. Use MinHeap.
 // m is the size of nums1 and n is the size of nums2.
 // Time complexity: O(min(k⋅logk,m⋅n⋅log(m⋅n)))
