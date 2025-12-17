0fde05e30c85b2597bdd4734ec7636441747cc4d
Tom Chen <txchen@gmail.com>
2025-12-16 20:29:39 -0800

    Update and rename SubarraySumEqualsK.cpp to 560.SubarraySumEqualsK.cpp

--- leetcode/560.SubarraySumEqualsK.cpp
+++ leetcode/560.SubarraySumEqualsK.cpp
@@ -4,7 +4,7 @@ public:
     int subarraySum(vector<int>& nums, int k) {
         int count = 0, sum = 0;
         map<int, int> mp;
-        mp[0] = 1; // 1 subarray that sums to 0
+        mp[0] = 1; // 1 subarray that sums to 0. 1, NOT -1 !!!
         for (int i = 0; i < nums.size(); i++) {
             sum += nums[i];
             if (mp.contains(sum - k)) {
