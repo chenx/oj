abcd1fd1da4dde62ee03686548bb90d24d4184ed
Tom Chen <txchen@gmail.com>
2025-12-25 22:56:01 -0800

    Create 9.kv_store_seralize_deserialize.cpp

---
+++ open_ai/9.kv_store_seralize_deserialize.cpp
@@ -0,0 +1,8 @@
+
+
+/**
+Implement serialization and deserialization for a key-value store where both keys and values 
+can contain any characters including delimiters. You can't use simple delimiters because they 
+might appear in the data. Most candidates land on length-prefix encoding (like 3:key5:value), 
+which is the same pattern used in Redis protocol.
+ */
