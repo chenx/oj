e3a3cd14638016e2e25026ea8a2d4ca58d846d72
Tom Chen <txchen@gmail.com>
2025-12-30 11:35:40 +0800

    Update and rename MergeSortedArray.cpp to 88.MergeSortedArray.cpp

--- leetcode/88.MergeSortedArray.cpp
+++ leetcode/88.MergeSortedArray.cpp
@@ -1,3 +1,16 @@
+class Solution3 {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int k = nums1.size() - 1;
+        for (--m, --n; m >= 0 && n >= 0; ) {
+            nums1[k --] = nums1[m] > nums2[n] ? nums1[m --] : nums2[n --];
+        }
+        while (n >= 0) {
+            nums1[k --] = nums2[n --];
+        }
+    }
+};
+
 //
 // http://www.leetcode.com/onlinejudge#
 // @Author: Xin Chen
@@ -39,3 +52,4 @@ public:
         while (n >= 0) { A[k --] = B[n --]; }
     }
 };
+
