dc242695f37297aad82118f657e7454a30f3cff1
Tom Chen <txchen@gmail.com>
2026-01-02 11:43:58 +0800

    Update and rename IsomorphicStrings.cpp to 205.IsomorphicStrings.cpp

--- leetcode/205.IsomorphicStrings.cpp
+++ leetcode/205.IsomorphicStrings.cpp
@@ -1,3 +1,21 @@
+class Solution4 {
+public:
+    bool isIsomorphic(string s, string t) {
+        if (s.length() != t.length()) return false;
+
+        map<char, char> m1, m2;
+        for (int i = 0; i < s.length(); ++ i) {
+            if (! m1.contains(s[i]) && ! m2.contains(t[i])) {
+                m1[s[i]] = t[i];
+                m2[t[i]] = s[i];
+            } else if (m1[s[i]] != t[i] || m2[t[i]] != s[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+};
+
 // Works.
 class Solution3 {
 public:
