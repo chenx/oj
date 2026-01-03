575bbc6f2e8a1d070f59212febc7c30a835e586c
Tom Chen <txchen@gmail.com>
2026-01-03 14:39:42 +0800

    Update and rename IPO.cpp to 502.IPO.cpp

--- leetcode/502.IPO.cpp
+++ leetcode/502.IPO.cpp
@@ -1,3 +1,27 @@
+class Solution2 {
+public:
+    int findMaximizedCapital(int k, int w, vector<int>& profits, vector<int>& capital) {
+        int n = profits.size();
+
+        vector<vector<int>> projects; // <capital, profit>
+        for (int i = 0; i < n; ++ i) {
+            projects.push_back({capital[i], profits[i]});
+        }
+        sort(projects.begin(), projects.end()); // by capital, profit
+
+        priority_queue<int> maxHeap;
+        for (int i = 0, p = 0; i < k; ++ i) {
+            for (; p < n && projects[p][0] <= w; ++ p) {
+                maxHeap.push(projects[p][1]);
+            }
+            if (maxHeap.empty()) break;
+            w += maxHeap.top();
+            maxHeap.pop();
+        }
+        return w;
+    }
+};
+
 // Works.
 // From: https://leetcode.com/problems/ipo/editorial/?envType=study-plan-v2&envId=top-interview-150
 // Complexity: Time: O(n log(n)), Space: O(n).
