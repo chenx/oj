551b6acdf59cf736d4166dc14407bd0299b7cadb
Tom Chen <txchen@gmail.com>
2026-01-09 15:07:46 +0800

    Update 451.SortCharactersByFrequency.cpp

--- leetcode/451.SortCharactersByFrequency.cpp
+++ leetcode/451.SortCharactersByFrequency.cpp
@@ -1,3 +1,31 @@
+// Time: O(n)
+// Space: O(n)
+class Solution2 {
+public:
+    string frequencySort(string s) {
+        map<char, int> frequency;
+        for (char ch : s) ++ frequency[ch];
+
+        map<int, vector<char>> bucket;
+        int maxFreq = 0;
+        for (auto [ch, count] : frequency) {
+            bucket[count].push_back(ch);
+            maxFreq = max(maxFreq, count);
+        }
+
+        string result;
+        for (int i = maxFreq; i > 0; -- i) {
+            for (auto ch : bucket[i]) {
+                result += string(i, ch);
+            }
+        }
+
+        return result;
+    }
+};
+
+// Time: O(nlogn)
+// Space: O(n)
 class Solution {
 public:
     string frequencySort(string s) {
