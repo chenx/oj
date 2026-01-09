13fe8c6bfc10a9ebd5d7b4418377618a5c8065f0
Tom Chen <txchen@gmail.com>
2026-01-09 15:49:07 +0800

    Update 416.PartitionEqualSubsetSum.cpp

--- leetcode/416.PartitionEqualSubsetSum.cpp
+++ leetcode/416.PartitionEqualSubsetSum.cpp
@@ -1,3 +1,27 @@
+// From Leetcode 101 - A grinding guide
+class Solution2 {
+public:
+    bool canPartition(vector<int> &nums) {
+        int nums_sum = accumulate(nums.begin(), nums.end(), 0);
+        if (nums_sum % 2 != 0) {
+            return false;
+        }
+        int target = nums_sum / 2, n = nums.size();
+        vector<vector<bool>> dp(n + 1, vector<bool>(target + 1, false));
+        dp[0][0] = true;
+        for (int i = 1; i <= n; ++i) {
+            for (int j = 0; j <= target; ++j) {
+                if (j < nums[i - 1]) {
+                    dp[i][j] = dp[i - 1][j];
+                } else {
+                    dp[i][j] = dp[i - 1][j] || dp[i - 1][j - nums[i - 1]];
+                }
+            }
+        }
+        return dp[n][target];
+    }
+};
+
 // From: https://leetcode.com/problems/partition-equal-subset-sum/editorial/
 // Bottom up DP
 // Time Complexity : O(m⋅n), where m is the subSetSum, and n is the number of array elements.
