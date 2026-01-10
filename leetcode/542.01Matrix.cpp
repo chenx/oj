e4eee633dc50e1f85b95d1f3fc34703957ed0dfa
Tom Chen <txchen@gmail.com>
2026-01-10 14:05:43 +0800

    Update 542.01Matrix.cpp

--- leetcode/542.01Matrix.cpp
+++ leetcode/542.01Matrix.cpp
@@ -1,3 +1,49 @@
+// Works.
+// Time: O(mn)
+// Space: O(mn)
+class Solution2 {
+public:
+    vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
+        int m = mat.size(), n = mat[0].size();
+        vector<vector<int>> ans(m, vector<int>(n, 0));
+        static int directions[4][2] = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
+
+        queue<vector<int>> q;
+        set<vector<int>> visited;
+
+        for (int i = 0; i < m; ++ i) {
+            for (int j = 0; j < n; ++ j) {
+                if (mat[i][j] == 0) {
+                    q.push({i, j, 0});
+                    visited.insert({i, j});
+                }
+            }
+        }
+
+        while (! q.empty()) {
+            int i = q.front()[0], j = q.front()[1], dist = q.front()[2];
+            q.pop();
+
+            for (int k = 0; k < 4; ++ k) {
+                int x = i + directions[k][0], y = j + directions[k][1];
+                if (x >= 0 && x < mat.size() && y >= 0 && y < mat[0].size()) {
+                    if (! visited.contains({x, y})) {
+                        if (mat[x][y] != 0) {
+                            ans[x][y] = 1 + dist;
+
+                            visited.insert({x, y});
+                            q.push({x, y, 1 + dist});
+                        }
+                    }
+                }
+            }
+        }
+
+        return ans;
+    }
+};
+
+
 class Solution {
     vector<vector<int>> ans;//(m, vector<int>(n, 0));
 public:
