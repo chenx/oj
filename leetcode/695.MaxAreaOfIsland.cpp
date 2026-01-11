7ecd482cebdfa687cc6a4473c7895c6a90478ad4
Tom Chen <txchen@gmail.com>
2026-01-11 15:26:24 +0800

    Update 695.MaxAreaOfIsland.cpp

--- leetcode/695.MaxAreaOfIsland.cpp
+++ leetcode/695.MaxAreaOfIsland.cpp
@@ -1,3 +1,59 @@
+// DFS. Best.
+class Solution3 {
+public:
+    int maxAreaOfIsland(vector<vector<int>>& grid) {
+        int m = grid.size(), n = grid[0].size(), maxArea = 0;
+
+        for (int i = 0; i < m; ++ i) {
+            for (int j = 0; j < n; ++ j) {
+                if (grid[i][j] == 1) {
+                    maxArea = max(maxArea, dfs(grid, i, j));
+                }
+            }
+        }
+        return maxArea;
+    }
+
+    int dfs(vector<vector<int>>& grid, int x, int y) {
+        if (x < 0 || x >= grid.size() || y < 0 || y >= grid[0].size() || grid[x][y] != 1) return 0;
+        grid[x][y] = 2; // mark as visited.
+        
+        return 1 + dfs(grid, x+1, y) + dfs(grid, x-1, y) + dfs(grid, x, y+1) + dfs(grid, x, y-1);
+    }
+};
+
+
+// DFS.
+class Solution2 {
+public:
+    int maxAreaOfIsland(vector<vector<int>>& grid) {
+        int m = grid.size(), n = grid[0].size(), maxArea = 0;
+
+        for (int i = 0; i < m; ++ i) {
+            for (int j = 0; j < n; ++ j) {
+                if (grid[i][j] == 1) {
+                    int area = 0;
+                    dfs(grid, i, j, area);
+                    maxArea = max(maxArea, area);
+                }
+            }
+        }
+        return maxArea;
+    }
+
+    void dfs(vector<vector<int>>& grid, int x, int y, int& area) {
+        if (x < 0 || x >= grid.size() || y < 0 || y >= grid[0].size() || grid[x][y] != 1) return;
+        grid[x][y] = 2; // mark as visited.
+        ++ area;
+
+        dfs(grid, x+1, y, area);
+        dfs(grid, x-1, y, area);
+        dfs(grid, x, y+1, area);
+        dfs(grid, x, y-1, area);
+    }
+};
+
+
 class Solution {
 public:
     int maxAreaOfIsland(vector<vector<int>>& grid) {
