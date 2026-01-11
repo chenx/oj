f98df43af9883edd7e4c539a9ace15bf71ef1049
Tom Chen <txchen@gmail.com>
2026-01-11 11:35:10 +0800

    Update 572.SubtreeOfAnotherTree.cpp

--- leetcode/572.SubtreeOfAnotherTree.cpp
+++ leetcode/572.SubtreeOfAnotherTree.cpp
@@ -1,3 +1,46 @@
+// Tree hash (merkel tree)
+// From: https://leetcode.com/problems/subtree-of-another-tree/editorial/
+// Time: O(m + n)
+// Space: O(m + n)
+typedef unsigned long long ull;
+class Solution2 {
+public:
+    const int MOD_1 = 1000000007;
+    const int MOD_2 = 2147483647;
+
+    // Hashing a Node
+    pair<ull, ull> hashSubtreeAtNode(TreeNode* node, bool needToAdd) {
+        if (node == nullptr) return {3, 7};
+
+        auto left = hashSubtreeAtNode(node->left, needToAdd);
+        auto right = hashSubtreeAtNode(node->right, needToAdd);
+
+        auto left1 = (left.first << 5) % MOD_1;
+        auto right1 = (right.first << 1) % MOD_1;
+        auto left2 = (left.second << 7) % MOD_2;
+        auto right2 = (right.second << 1) % MOD_2;
+
+        pair hashpair = {(left1 + right1 + node->val) % MOD_1,
+                         (left2 + right2 + node->val) % MOD_2};
+
+        if (needToAdd) memo.push_back(hashpair);
+
+        return hashpair;
+    }
+
+    // Vector to store hashed value of each node.
+    vector<pair<ull, ull>> memo;
+
+    bool isSubtree(TreeNode* root, TreeNode* subRoot) {
+        hashSubtreeAtNode(root, true);
+        pair<ull, ull> s = hashSubtreeAtNode(subRoot, false);
+
+        // Check if hash of subRoot is present in memo
+        return find(memo.begin(), memo.end(), s) != memo.end();
+    }
+};
+
+
 // Time: O(mn). m and n are number of nodes in the 2 trees.
 // Space: O(m + n)
 class Solution {
