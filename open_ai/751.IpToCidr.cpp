2d4196ef0f0102cc61c5fc79b1e6b8a3369fd2af
Tom Chen <txchen@gmail.com>
2026-01-15 22:33:52 +0800

    Update 751.IpToCidr.cpp

--- open_ai/751.IpToCidr.cpp
+++ open_ai/751.IpToCidr.cpp
@@ -1,51 +1,44 @@
 class Solution {
 public:
-    vector<string> ipToCIDR(string ip, int range) {
+    vector<string> ipToCIDR(string ip, int n) {
         vector<string> result;
-        int curIp = ipToInt(ip);
+        unsigned int curIp = converIpToInt(ip);
         int count; // number of IP addresses to include in each step.
 
-        while(range) {
-            if (curIp == 0) { 
+        while (n > 0) {
+            if (curIp == 0) {
                 count = 1;
-                while (count * 2 <= range) count *= 2;
+                while (count * 2 <= n) count *= 2;
+            } else {
+                count = curIp & -curIp; // get last 1-bit.
+                while (count > n) count /= 2;
             }
-            else { 
-                count = curIp & -curIp; // get the least significant 1-bit (LSB)
-                while (count > range) count /= 2; 
-            }
-
-            result.push_back(numberToIp(curIp, count));
-            range -= count;
+            result.push_back(getCidr(curIp, count));
             curIp += count;
+            n -= count;
         }
-
         return result;
     }
 
-    unsigned int ipToInt(string ip) {
-        unsigned int result = 0;
+    string getCidr(unsigned int curIp, int count) {
+        string ip = to_string((curIp >> 24) & 255) + "."
+                    + to_string((curIp >> 16) & 255) + "."
+                    + to_string((curIp >> 8) & 255) + "."
+                    + to_string(curIp & 255);
+        
+        int maskLen = 33;
+        for (; count > 0; count /= 2) maskLen --;
 
+        return ip + "/" + to_string(maskLen);
+    }
+
+    unsigned int converIpToInt(string& ip) {
         stringstream ss(ip);
         string out;
-        while (getline(ss, out, '.')) {
+        unsigned int result = 0;
+        while(getline(ss, out, '.')) {
             result = (result << 8) + stoi(out);
         }
-
         return result;
-    }
-
-    string numberToIp(unsigned int ip, int count) {
-        string ans = to_string(ip>>24) + "." +
-                     to_string(ip>>16 & 255) + "." +
-                     to_string(ip>>8 & 255)  + "." +
-                     to_string(ip & 255);
-
-        int maskLen = 33;
-        for (; count > 0; count /= 2) {
-            maskLen --;
-        }
-
-        return ans + "/" + to_string(maskLen);
     }
 };
