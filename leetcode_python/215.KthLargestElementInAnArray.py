e4191dd092c06c0b9b401a7e368ecbd83387b0f6
Tom Chen <txchen@gmail.com>
2026-01-25 18:41:29 -0800

    Update 215.KthLargestElementInAnArray.py

--- leetcode_python/215.KthLargestElementInAnArray.py
+++ leetcode_python/215.KthLargestElementInAnArray.py
@@ -1,3 +1,30 @@
+// More efficient.
+// From: https://leetcode.com/problems/kth-largest-element-in-an-array/editorial/
+class Solution2:
+    def findKthLargest(self, nums, k):
+        def quick_select(nums, k):
+            pivot = random.choice(nums)
+            left, mid, right = [], [], []
+
+            for num in nums:
+                if num > pivot:
+                    left.append(num)
+                elif num < pivot:
+                    right.append(num)
+                else:
+                    mid.append(num)
+            
+            if k <= len(left):
+                return quick_select(left, k)
+            
+            if len(left) + len(mid) < k:
+                return quick_select(right, k - len(left) - len(mid))
+            
+            return pivot
+        
+        return quick_select(nums, k)
+        
+
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
         def quickSelect(nums, left, right, k):
