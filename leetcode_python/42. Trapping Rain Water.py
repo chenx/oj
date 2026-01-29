9e3759d2615f02dff787383ae6391fbd8092c4c5
Tom Chen <txchen@gmail.com>
2026-01-29 13:50:29 -0800

    Create 42. Trapping Rain Water.py

---
+++ leetcode_python/42. Trapping Rain Water.py
@@ -0,0 +1,37 @@
+# if cur height < height at stack.top(): push to stack
+# else: pop from stack, add waters
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        n = len(height)
+        water = 0
+        stack = []
+        for i in range(n):
+            if len(stack) > 0 and height[i] >= height[stack[-1]]:
+                bottom = height[stack[-1]]
+                stack.pop()
+                while len(stack) > 0 and height[i] >= height[stack[-1]]:
+                    ht = height[stack[-1]] - bottom
+                    width = i - stack[-1] - 1
+                    water += width * ht
+
+                    bottom = height[stack[-1]]
+                    stack.pop()
+                if len(stack) > 0:
+                    ht = height[i] - bottom
+                    width = i - stack[-1] - 1
+                    water += width * ht
+            stack.append(i)
+        return water
+
+
+        
+
+/**
+42. Trapping Rain Water
+Solved
+Hard
+Topics
+conpanies iconCompanies
+
+Given n non-negative integers representing an elevation map where the width of each bar is 1, compute how much water it can trap after raining.
+ */
