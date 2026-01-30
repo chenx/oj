5e278ddf1e486baa0b14b9ede1db036b0b3089ca
Tom Chen <txchen@gmail.com>
2026-01-29 17:31:37 -0800

    Create 242. Valid Anagram.py

---
+++ leetcode_python/242. Valid Anagram.py
@@ -0,0 +1,19 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        def getCharCountMap(x):
+            map = {}
+            for i in range(len(x)):
+                map[x[i]] = map.get(x[i], 0) + 1
+            return map
+        
+        return getCharCountMap(s) == getCharCountMap(t)
+
+/**
+242. Valid Anagram
+Solved
+Easy
+Topics
+conpanies iconCompanies
+
+Given two strings s and t, return true if t is an of s, and false otherwise.
+ */
