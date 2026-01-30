5cea1afe7fe54f39786f7db53acc965a177fc265
Tom Chen <txchen@gmail.com>
2026-01-29 16:02:24 -0800

    Create 76. Minimum Window Substring.py

---
+++ leetcode_python/76. Minimum Window Substring.py
@@ -0,0 +1,49 @@
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        m, n = len(s), len(t)
+
+        toFind : Dict[str, int] = {}
+        found  : Dict[str, int] = {}
+
+        for ch in list(t):
+            toFind[ch] = toFind.get(ch, 0) + 1
+        
+        start = 0
+        total = 0
+        minWinLen = 0
+        minWinStart = -1
+        for i in range(m):
+            ch = s[i]
+            found[ch] = found.get(ch, 0) + 1
+            if ch in toFind and found[ch] <= toFind[ch]:
+                total += 1
+            
+            while total == n:
+                winLen = i - start + 1
+                if minWinLen == 0 or minWinLen > winLen:
+                    minWinLen = winLen
+                    minWinStart = start
+                
+                if s[start] not in toFind or found[s[start]] > toFind[s[start]]:
+                    found[s[start]] -= 1
+                    start += 1
+                else:
+                    break
+        
+        return "" if minWinStart == -1 else s[minWinStart : minWinStart + minWinLen]
+
+
+/**
+76. Minimum Window Substring
+Solved
+Hard
+Topics
+conpanies iconCompanies
+Hint
+
+Given two strings s and t of lengths m and n respectively, return the minimum window of s such that 
+every character in t (including duplicates) is included in the window. If there is no such substring, 
+return the empty string "".
+
+The testcases will be generated such that the answer is unique.
+ */
