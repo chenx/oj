bfa9eac57d12f2329a4cd3a37bf23b3f39e9b964
Tom Chen <txchen@gmail.com>
2026-02-24 23:22:22 -0800

    Create 416. Partition Equal Subset Sum.py

---
+++ leetcode_python/416. Partition Equal Subset Sum.py
@@ -0,0 +1,73 @@
+# Optimized DP.
+# Time: O(m*n), m is the subSetSum, and n is the number of array elements.
+# Space: O(m)
+class Solution:
+    def canPartition(self, nums: List[int]) -> bool:
+        # find sum of array elements
+        total_sum = sum(nums)
+
+        # if total_sum is odd, it cannot be partitioned into equal sum subsets
+        if total_sum % 2 != 0:
+            return False
+        subset_sum = total_sum // 2
+
+        # construct a dp table of size (subset_sum + 1)
+        dp = [False] * (subset_sum + 1)
+        dp[0] = True
+        for curr in nums:
+            for j in range(subset_sum, curr - 1, -1):
+                dp[j] = dp[j] or dp[j - curr]
+
+        return dp[subset_sum]
+
+
+# Bottom up DP
+# Time: O(m*n), m is the subSetSum, and n is the number of array elements.
+# Space: O(m*n) 
+class Solution:
+    def canPartition(self, nums: List[int]) -> bool:
+        # find sum of array elements
+        total_sum = sum(nums)
+
+        # if total_sum is odd, it cannot be partitioned into equal sum subsets
+        if total_sum % 2 != 0:
+            return False
+        subset_sum = total_sum // 2
+        n = len(nums)
+
+        # construct a dp table of size (n+1) x (subset_sum + 1)
+        dp = [[False] * (subset_sum + 1) for _ in range(n + 1)]
+        dp[0][0] = True
+        for i in range(1, n + 1):
+            curr = nums[i - 1]
+            for j in range(subset_sum + 1):
+                if j < curr:
+                    dp[i][j] = dp[i - 1][j]
+                else:
+                    dp[i][j] = dp[i - 1][j] or dp[i - 1][j - curr]
+        return dp[n][subset_sum]
+    
+/**
+416. Partition Equal Subset Sum
+Solved
+Medium
+Topics
+conpanies iconCompanies
+
+Given an integer array nums, return true if you can partition the array into two subsets such that the 
+sum of the elements in both subsets is equal or false otherwise.
+
+ 
+
+Example 1:
+
+Input: nums = [1,5,11,5]
+Output: true
+Explanation: The array can be partitioned as [1, 5, 5] and [11].
+
+Example 2:
+
+Input: nums = [1,2,3,5]
+Output: false
+Explanation: The array cannot be partitioned into equal sum subsets.
+ */
