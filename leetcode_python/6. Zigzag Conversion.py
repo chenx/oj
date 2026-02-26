42cbc5f395c7cd5bc7e0c87d5233d02767b83849
Tom Chen <txchen@gmail.com>
2026-02-25 18:24:38 -0800

    Update 6. Zigzag Conversion.py

--- leetcode_python/6. Zigzag Conversion.py
+++ leetcode_python/6. Zigzag Conversion.py
@@ -1,4 +1,4 @@
-class Solution:
+class Solution3:
     def convert(self, s: str, numRows: int) -> str:
         rows = ["" for _ in range(numRows)]
 
@@ -16,6 +16,49 @@ class Solution:
                 i += 1
 
         return "".join(rows)
+
+
+class Solution2:
+    def convert(self, s: str, numRows: int) -> str:
+        rows = [''] * numRows
+        k = 0
+        n = len(s)
+        while k < n:
+            j = 0
+            while j < numRows and k < n:
+                rows[j] += s[k]
+                k += 1
+                j += 1
+            
+            j = numRows - 2
+            while j >= 1 and k < n:
+                rows[j] += s[k]
+                k += 1
+                j -= 1
+
+        return "".join(rows)
+
+
+class Solution:
+    def convert(self, s: str, numRows: int) -> str:
+        rows = [''] * numRows
+        k = 0
+        n = len(s)
+        while k < n:
+            for j in range(numRows):
+                rows[j] += s[k]
+                k += 1
+                if k == n:
+                    break
+            if k == n:
+                break
+            for j in range(numRows - 2, 0, -1):
+                rows[j] += s[k]
+                k += 1
+                if k == n:
+                    break
+        return "".join(rows)
+
 
 /**
 6. Zigzag Conversion
@@ -36,4 +79,11 @@ And then read line by line: "PAHNAPLSIIGYIR"
 Write the code that will take a string and make this conversion given a number of rows:
 
 string convert(string s, int numRows);
+
+ 
+
+Example 1:
+
+Input: s = "PAYPALISHIRING", numRows = 3
+Output: "PAHNAPLSIIGYIR"
  */
