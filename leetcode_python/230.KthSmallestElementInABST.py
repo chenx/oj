47c78277ce43ef87efd099fb0de6a5a422bbafa4
Tom Chen <txchen@gmail.com>
2026-02-26 23:18:56 -0800

    Update 230.KthSmallestElementInABST.py

--- leetcode_python/230.KthSmallestElementInABST.py
+++ leetcode_python/230.KthSmallestElementInABST.py
@@ -1,9 +1,22 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+class Solution2:
+    def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+        def inorder(node):
+            nonlocal k, kthSmallestVal
+            if node == None:
+                return
+            
+            inorder(node.left)
+            k -= 1
+            if k == 0:
+                kthSmallestVal = node.val
+                return
+            inorder(node.right)
+
+        kthSmallestVal = 0  # it's ok without this
+        inorder(root)
+        return kthSmallestVal
+
+
 class Solution:
     def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
         def inorder(node):
