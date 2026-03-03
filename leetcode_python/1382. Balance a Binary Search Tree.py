a5be007f3d88c3e627e4832cc3af640fc1c96c33
Tom Chen <txchen@gmail.com>
2026-03-03 13:15:58 -0800

    Create 1382. Balance a Binary Search Tree.py

---
+++ leetcode_python/1382. Balance a Binary Search Tree.py
@@ -0,0 +1,36 @@
+# Time: O(n), n = number of nodes, for inorder traversal, and toBalancedTree
+# Space: O(n + logn) = O(n)
+class Solution:
+    def balanceBST(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        def inorder(orderedList, node):
+            if not node:
+                return
+            inorder(orderedList, node.left)
+            orderedList.append(node.val)
+            inorder(orderedList, node.right)
+        
+        def toBalancedTree(orderedList, left, right):
+            if left > right:
+                return None
+            mid = left + (right - left) // 2
+            root = TreeNode(orderedList[mid])
+            root.left = toBalancedTree(orderedList, left, mid - 1)
+            root.right = toBalancedTree(orderedList, mid + 1, right)
+            return root
+        
+        orderedList = []
+        inorder(orderedList, root)
+        return toBalancedTree(orderedList, 0, len(orderedList) - 1)
+
+/**
+1382. Balance a Binary Search Tree
+Medium
+Topics
+conpanies iconCompanies
+Hint
+
+Given the root of a binary search tree, return a balanced binary search tree with the same node values. 
+If there is more than one answer, return any of them.
+
+A binary search tree is balanced if the depth of the two subtrees of every node never differs by more than 1.
+ */
