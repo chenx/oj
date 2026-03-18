592de4e216301b87ef7321fb745ea0eda968d586
Tom Chen <txchen@gmail.com>
2026-03-17 18:30:53 -0700

    Update 66. Plus One.py

--- leetcode_python/66. Plus One.py
+++ leetcode_python/66. Plus One.py
@@ -1,8 +1,8 @@
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
-        carry = 0
+        carry = 1
         for i in range(len(digits) - 1, -1, -1):
-            digits[i] += 1
+            digits[i] += carry
             if digits[i] >= 10:
                 digits[i] -= 10
                 carry = 1
