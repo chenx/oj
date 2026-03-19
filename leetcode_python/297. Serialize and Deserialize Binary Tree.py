62a21bdff10d907e59c93b88eb22dae54a038317
Tom Chen <txchen@gmail.com>
2026-03-18 17:32:43 -0700

    Update 297. Serialize and Deserialize Binary Tree.py

--- leetcode_python/297. Serialize and Deserialize Binary Tree.py
+++ leetcode_python/297. Serialize and Deserialize Binary Tree.py
@@ -1,12 +1,176 @@
+# Level order traversal
+# [1,2,3,null,null,4,5] =>
+# serialize result: 1 2 3 null null 4 5
+from collections import deque
+class Codec4:
+
+    def serialize(self, root):
+        """Encodes a tree to a single string.
+        
+        :type root: TreeNode
+        :rtype: str
+        """
+        tokens = []
+        q = deque([root])
+
+        while q:
+            node = q.popleft()
+            if node:
+                tokens.append(str(node.val))
+                q.append(node.left)
+                q.append(node.right)
+            else:
+                tokens.append('null')
+            
+        while tokens and tokens[-1] == 'null':
+            tokens.pop()
+
+        result = ' '.join(tokens)
+        print(f"serialize result: {result}")
+        return result
+        
+
+    def deserialize(self, data):
+        """Decodes your encoded data to tree.
+        
+        :type data: str
+        :rtype: TreeNode
+        """
+        if data == '':
+            return None
+
+        tokens = data.split()
+        root = TreeNode(int(tokens[0]))
+        q = deque([root])
+        i, n = 1, len(tokens)
+        while q:
+            node = q.popleft()
+            if i < n and tokens[i] != 'null':
+                node.left = TreeNode(int(tokens[i]))
+                q.append(node.left)
+            i += 1
+            if i < n and tokens[i] != 'null':
+                node.right = TreeNode(int(tokens[i]))
+                q.append(node.right)
+            i += 1
+
+        return root
+
+
+# Level order traversal
+# [1,2,3,null,null,4,5] =>
+# serialize result: 1 2 3 null null 4 5 null null null null
+# However there are extra "null" at the end. To avoid this, see Codec4 above.
+from collections import deque
+class Codec3:
+
+    def serialize(self, root):
+        """Encodes a tree to a single string.
+        
+        :type root: TreeNode
+        :rtype: str
+        """
+        tokens = []
+        q = deque([root])
+
+        while q:
+            node = q.popleft()
+            if node:
+                tokens.append(str(node.val))
+                q.append(node.left)
+                q.append(node.right)
+            else:
+                tokens.append('null')
+
+        result = ' '.join(tokens)
+        print(f"serialize result: {result}")
+        return result
+        
+
+    def deserialize(self, data):
+        """Decodes your encoded data to tree.
+        
+        :type data: str
+        :rtype: TreeNode
+        """
+        tokens = data.split()
+        if tokens[0] == 'null':
+            return None
+
+        root = TreeNode(int(tokens[0]))
+        q = deque([root])
+        i = 1
+        while q:
+            node = q.popleft()
+            if tokens[i] != 'null':
+                node.left = TreeNode(int(tokens[i]))
+                q.append(node.left)
+            i += 1
+            if tokens[i] != 'null':
+                node.right = TreeNode(int(tokens[i]))
+                q.append(node.right)
+            i += 1
+
+        return root
+
+
+# DFS. Use tokens[] list instead of string.
+# Time: O(n)
+# Space: O(n)
+class Codec2:
+    def serialize(self, root):
+        """Encodes a tree to a single string.
+        
+        :type root: TreeNode
+        :rtype: str
+        """
+        def dfs(node):
+            if node is None:
+                tokens.append('null')
+                return
+            
+            tokens.append(str(node.val))
+            dfs(node.left)
+            dfs(node.right)
+
+        tokens = []
+        dfs(root)
+        result = ' '.join(tokens)
+        print(f"serialize result: {result}")
+        return result
+        
+
+    def deserialize(self, data):
+        """Decodes your encoded data to tree.
+        
+        :type data: str
+        :rtype: TreeNode
+        """
+        def dfs():
+            nonlocal pos
+            if tokens[pos] == 'null':
+                if pos < len(tokens):
+                    pos += 1
+                return None
+            
+            node = TreeNode(int(tokens[pos]))
+            pos += 1
+            node.left = dfs()
+            node.right = dfs()
+            return node
+
+        pos = 0
+        tokens = data.split()
+        return dfs()
+
+
 # Definition for a binary tree node.
 # class TreeNode(object):
 #     def __init__(self, x):
 #         self.val = x
 #         self.left = None
 #         self.right = None
-
 class Codec:
-
     def serialize(self, root):
         """Encodes a tree to a single string.
         
