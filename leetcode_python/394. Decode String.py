39a9272a322866f74b9e2fd61e3fd935fa8a4b0d
Tom Chen <txchen@gmail.com>
2026-03-31 15:45:38 -0700

    Update 394. Decode String.py

--- leetcode_python/394. Decode String.py
+++ leetcode_python/394. Decode String.py
@@ -1,3 +1,12 @@
+# Assume, n is the length of the string s.
+#
+# Time Complexity: O(maxK⋅n), where maxK is the maximum value of k and n is the length of a given string s. 
+# We traverse a string of size n and iterate k times to decode each pattern of form k[string]. This gives 
+# us worst case time complexity as O(maxK⋅n).
+#
+# Space Complexity: O(m+n), where m is the number of letters(a-z) and n is the number of digits(0-9) in 
+# string s. In worst case, the maximum size of stringStack and countStack could be m and n respectively.
+#
 class Solution:
     def decodeString(self, s: str) -> str:
         strStack = []
