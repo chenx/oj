530a9441c1deac18939c3ff67add41e0a3f2871f
Tom Chen <txchen@gmail.com>
2026-04-13 22:30:41 -0700

    Update CountCompleteTreeNodes.cpp

--- leetcode/CountCompleteTreeNodes.cpp
+++ leetcode/CountCompleteTreeNodes.cpp
@@ -1,3 +1,11 @@
+class Solution3 {
+public:
+    int countNodes(TreeNode* root) {
+        if (! root) return 0;
+        return 1 + countNodes(root->left) + countNodes(root->right);
+    }
+};
+
 // Works.
 class Solution2 {
 public:
