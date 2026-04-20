283ccbc5dfa06960ce97e5864ab362c8b694f694
Tom Chen <txchen@gmail.com>
2026-04-19 23:05:44 -0700

    Update 146.LRU_Cache.cpp

--- leetcode/146.LRU_Cache.cpp
+++ leetcode/146.LRU_Cache.cpp
@@ -9,6 +9,53 @@ When the cache reached its capacity, it should invalidate the least recently
 used item before inserting a new item. 
  */
 
+
+// Most recent item is at the front of DLL.
+class LRUCache11 {
+    struct Node {
+        int key;
+        int val;
+        Node(int k, int v) : key(k), val(v) {}
+    };
+    int capacity;
+    list<Node> dll;
+    unordered_map<int, list<Node>::iterator> cache;
+
+    void move_to_front(int key) {
+        Node node = *cache[key];
+        dll.erase(cache[key]);
+        dll.push_front(node);
+        cache[key] = dll.begin();
+    }
+
+public:
+    LRUCache(int capacity) : capacity(capacity) {}
+    
+    int get(int key) {
+        if (! cache.contains(key)) return -1;
+
+        move_to_front(key);
+        return cache[key]->val;
+    }
+    
+    void put(int key, int value) {
+        if (! cache.contains(key)) {
+            if (cache.size() == capacity) {
+                cache.erase(dll.back().key);
+                dll.pop_back();
+                // Or: dll.erase(-- dll.end());
+            }
+
+            dll.push_front(Node(key, value));
+            cache[key] = dll.begin();
+        } else {
+            move_to_front(key);
+            cache[key]->val = value;
+        }
+    }
+};
+
+
 // Most recent item is at the back of DLL.
 class LRUCache10 {
     struct Node {
