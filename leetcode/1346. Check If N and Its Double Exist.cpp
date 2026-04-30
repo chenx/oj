941aadc0eb5cd2bd6d03ea8739766193b83bd7c8
Tom Chen <txchen@gmail.com>
2026-04-29 23:17:34 -0700

    Create 1346. Check If N and Its Double Exist.cpp

---
+++ leetcode/1346. Check If N and Its Double Exist.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    bool checkIfExist(vector<int>& arr) {
+        unordered_set<int> numSet;
+        for (int i = 0; i < arr.size(); ++ i) {
+            if (numSet.contains(arr[i] * 2)) return true;
+            if (arr[i] % 2 == 0 && numSet.contains(arr[i] / 2)) return true;
+            numSet.insert(arr[i]);
+        }
+        return false;
+    }
+};
+
+/**
+1346. Check If N and Its Double Exist
+Solved
+Easy
+Topics
+conpanies iconCompanies
+Hint
+
+Given an array arr of integers, check if there exist two indices i and j such that :
+
+    i != j
+    0 <= i, j < arr.length
+    arr[i] == 2 * arr[j]
+
+ 
+
+Example 1:
+
+Input: arr = [10,2,5,3]
+Output: true
+Explanation: For i = 0 and j = 2, arr[i] == 10 == 2 * 5 == 2 * arr[j]
+
+Example 2:
+
+Input: arr = [3,1,7,11]
+Output: false
+Explanation: There is no i and j that satisfy the conditions.
+ */
