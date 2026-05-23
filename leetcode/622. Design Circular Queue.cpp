d33c081045492efb5904d7e2bf6f3df37ba593cc
Tom Chen <txchen@gmail.com>
2026-05-22 19:09:37 -0700

    Create 622. Design Circular Queue.cpp

---
+++ leetcode/622. Design Circular Queue.cpp
@@ -0,0 +1,94 @@
+class MyCircularQueue {
+    int capacity;
+    int size;
+    int front;
+    int back;
+    vector<int> buffer;
+
+public:
+    MyCircularQueue(int k) {
+        capacity = k;
+        buffer = vector<int>(k, 0);
+        front = back = 0;
+        size = 0;
+    }
+    
+    bool enQueue(int value) {
+        if (isFull()) return false;
+        size ++;
+
+        buffer[back ++] = value;
+        if (back == capacity) back = 0;
+        return true;
+    }
+    
+    bool deQueue() {
+        if (isEmpty()) return false;
+        size --;
+
+        buffer[front ++] = 0;
+        if (front == capacity) front = 0;
+
+        return true;
+    }
+    
+    int Front() {
+        if (isEmpty()) return -1;
+        return buffer[front];
+    }
+    
+    int Rear() {
+        if (isEmpty()) return -1;
+        int rear = back - 1;
+        if (rear < 0) rear += capacity;
+        return buffer[rear];        
+    }
+    
+    bool isEmpty() {
+        return size == 0;
+    }
+    
+    bool isFull() {
+        return size == capacity;
+    }
+};
+
+/**
+ * Your MyCircularQueue object will be instantiated and called as such:
+ * MyCircularQueue* obj = new MyCircularQueue(k);
+ * bool param_1 = obj->enQueue(value);
+ * bool param_2 = obj->deQueue();
+ * int param_3 = obj->Front();
+ * int param_4 = obj->Rear();
+ * bool param_5 = obj->isEmpty();
+ * bool param_6 = obj->isFull();
+ */
+
+
+/**
+622. Design Circular Queue
+Solved
+Medium
+Topics
+conpanies iconCompanies
+
+Design your implementation of the circular queue. The circular queue is a linear data structure in which 
+the operations are performed based on FIFO (First In First Out) principle, and the last position is 
+connected back to the first position to make a circle. It is also called "Ring Buffer".
+
+One of the benefits of the circular queue is that we can make use of the spaces in front of the queue. 
+In a normal queue, once the queue becomes full, we cannot insert the next element even if there is a 
+space in front of the queue. But using the circular queue, we can use the space to store new values.
+
+Implement the MyCircularQueue class:
+
+    MyCircularQueue(k) Initializes the object with the size of the queue to be k.
+    int Front() Gets the front item from the queue. If the queue is empty, return -1.
+    int Rear() Gets the last item from the queue. If the queue is empty, return -1.
+    boolean enQueue(int value) Inserts an element into the circular queue. Return true if the operation is successful.
+    boolean deQueue() Deletes an element from the circular queue. Return true if the operation is successful.
+    boolean isEmpty() Checks whether the circular queue is empty or not.
+    boolean isFull() Checks whether the circular queue is full or not.
+
+You must solve the problem without using the built-in queue data structure in your programming language. 
+ */
