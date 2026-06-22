3d0141b74905e371c3ae56ca919c1d23dee4f51b
Tom Chen <txchen@gmail.com>
2026-06-21 22:30:57 -0700

    Update and rename FindTheCelebrity.cpp to 277.FindTheCelebrity.cpp

--- leetcode/277.FindTheCelebrity.cpp
+++ leetcode/277.FindTheCelebrity.cpp
@@ -1,5 +1,21 @@
 // bool knows(int a, int b);
 
+class Solution2 {
+public:
+    int findCelebrity(int n) {
+        int c = 0;
+        for (int i = 1; i < n; ++ i) {
+            if (! knows(i, c)) c = i;
+        }
+
+        for (int i = 0; i < n; ++ i) {
+            if (i == c) continue;
+            if (! knows(i, c) || knows(c, i)) return -1;
+        }
+        return c;
+    }
+};
+
 // Works. Tested.
 // References: 
 // http://www.cnblogs.com/jcliBlogger/p/4787588.html
