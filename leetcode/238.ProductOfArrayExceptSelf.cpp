85be6451c5052bb00d3c2aa0c6769d13569a42c2
Tom Chen <txchen@gmail.com>
2026-07-02 18:16:47 -0700

    Update 238.ProductOfArrayExceptSelf.cpp

--- leetcode/238.ProductOfArrayExceptSelf.cpp
+++ leetcode/238.ProductOfArrayExceptSelf.cpp
@@ -11,13 +11,12 @@ public:
         for (int i = 1; i < n; ++ i) {
             result[i] = result[i-1] * nums[i-1];
         }
-
+        
         int prod = 1;
-        for (int i = n-1; i > 0; -- i) {
+        for (int i = n-1; i >= 0; -- i) {
             result[i] = result[i] * prod;
             prod *= nums[i];
         }
-        result[0] = prod;
 
         return result;
     }
