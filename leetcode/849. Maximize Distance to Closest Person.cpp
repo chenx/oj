f8d675af0bf5b17bcb5b469f6724b35fa387f478
Tom Chen <txchen@gmail.com>
2026-07-08 13:39:24 -0700

    Update 849. Maximize Distance to Closest Person.cpp

--- leetcode/849. Maximize Distance to Closest Person.cpp
+++ leetcode/849. Maximize Distance to Closest Person.cpp
@@ -1,3 +1,23 @@
+class Solution {
+public:
+    int maxDistToClosest(vector<int>& seats) {
+        int n = seats.size(), maxDist = 0, prev = -1, next = 0;
+
+        for (int i = 0; i < n; ++ i) {
+            if (seats[i] == 1) {
+                prev = i;
+            } else {
+                for (next = i+1; next < n && seats[next] == 0; ++ next) ;
+
+                int left = prev == -1 ? n : (i - prev);
+                int right = next == n ? n : (next - i);
+                maxDist = max(maxDist, min(left, right));
+            }
+        }
+        return maxDist;
+    }
+};
+
 // From: https://leetcode.com/problems/maximize-distance-to-closest-person/editorial/
 // Time: O(n)
 // Space: O(1)
